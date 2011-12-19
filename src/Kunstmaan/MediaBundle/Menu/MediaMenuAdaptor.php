596ef701d938f97b96233409df6ac9389c3d793e
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2011-12-19 10:51:07 +0100

    menu translation

--- src/Kunstmaan/MediaBundle/Menu/MediaMenuAdaptor.php
+++ src/Kunstmaan/MediaBundle/Menu/MediaMenuAdaptor.php
@@ -2,26 +2,28 @@
 // src/Acme/DemoBundle/Menu/Builder.php
 namespace Kunstmaan\MediaBundle\Menu;
 
+use Symfony\Component\Translation\Translator;
+
 use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Knp\Menu\ItemInterface as KnpMenu;
 
 class MediaMenuAdaptor implements \Kunstmaan\AdminBundle\Menu\MenuAdaptorInterface
 {
-    public function adaptMenu(KnpMenu $menu)
+    public function adaptMenu(KnpMenu $menu, Translator $translator)
     {
-        $menu->addChild('media.menu.media', array('route' => 'KunstmaanMediaBundle_media'));
-        $menu['media.menu.media']->setUri('#');
-        $menu['media.menu.media']->setLinkAttribute('class', 'dropdown-toggle');
-        $menu['media.menu.media']->setAttribute('class', 'dropdown');
-        $menu['media.menu.media']->setChildrenAttribute('class', 'dropdown-menu');
+        $menu->addChild($translator->trans('media.menu.media'), array('route' => 'KunstmaanMediaBundle_media'));
+        $menu[$translator->trans('media.menu.media')]->setUri('#');
+        $menu[$translator->trans('media.menu.media')]->setLinkAttribute('class', 'dropdown-toggle');
+        $menu[$translator->trans('media.menu.media')]->setAttribute('class', 'dropdown');
+        $menu[$translator->trans('media.menu.media')]->setChildrenAttribute('class', 'dropdown-menu');
 
-        $menu['media.menu.media']->moveToPosition(1);
+        $menu[$translator->trans('media.menu.media')]->moveToPosition(1);
 
-            $menu['media.menu.media']->addChild('media.menu.images', array('route' => 'KunstmaanMediaBundle_media_images'));
-            $menu['media.menu.media']->addChild('media.menu.videos', array('route' => 'KunstmaanMediaBundle_media_videos'));
-            $menu['media.menu.media']->addChild('media.menu.slides', array('route' => 'KunstmaanMediaBundle_media_slides'));
-            $menu['media.menu.media']->addChild('media.menu.les', array('route' => 'KunstmaanMediaBundle_media_files'));
+            $menu[$translator->trans('media.menu.media')]->addChild($translator->trans('media.menu.images'), array('route' => 'KunstmaanMediaBundle_media_images'));
+            $menu[$translator->trans('media.menu.media')]->addChild($translator->trans('media.menu.videos'), array('route' => 'KunstmaanMediaBundle_media_videos'));
+            $menu[$translator->trans('media.menu.media')]->addChild($translator->trans('media.menu.slides'), array('route' => 'KunstmaanMediaBundle_media_slides'));
+            $menu[$translator->trans('media.menu.media')]->addChild($translator->trans('media.menu.files'), array('route' => 'KunstmaanMediaBundle_media_files'));
     }
 
 }
