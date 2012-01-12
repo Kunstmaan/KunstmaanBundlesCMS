bf2460f8bf46cc0a66daf4c27a0bef8bf96c9e7c
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-01-12 17:54:42 +0100

    move nodes in tree script

--- src/Kunstmaan/AdminBundle/Resources/public/js/script.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/script.js
@@ -11,6 +11,81 @@ $(document).ready(function () {
 // JS-tree
 function init_tree() {
 	$('#tree').jstree({
+		"plugins" : [ "themes", "html_data", "types", "search" ] ,
+		"themes" : { 
+			"theme" : "OMNext",
+			"dots" : true,
+			"icons" : true		
+		},
+		"types" : {
+			"types" : {
+				//Page
+				"page" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -57px" 
+					}
+				},				
+				//Site
+				"site" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -38px" 
+					}
+				},
+				//Settings
+				"settings" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -37px" 
+					}
+				},
+				//Image
+				"image" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-20px -74px" 
+					}
+				},
+				//Video
+				"video" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -55px" 
+					}
+				},
+				//Slideshow
+				"slideshow" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-2px -72px" 
+					}
+				},
+				//Files
+				"files" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-38px -72px" 
+					}
+				}
+			}
+		
+		
+		},
+		// Configuring the search plugin
+		"search" : {
+			"ajax" : {
+			    "url" : "", //<-- Link naar php
+			    "data" : function (str) {
+			        return {
+			            "operation" : "search",
+			            "search_str" : str
+			        };
+			    }
+			}
+		}
+	});
+	$('#pages').jstree({
 		"plugins" : [ "themes", "html_data", "dnd", "crrm", "types", "search" ] ,
 		"themes" : { 
 			"theme" : "OMNext",
