c241d210a33198a17a7fa65a48b39f99c423943d
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-01-18 18:00:19 +0100

    get classname

--- src/Kunstmaan/AdminBundle/Modules/ClassLookup.php
+++ src/Kunstmaan/AdminBundle/Modules/ClassLookup.php
@@ -8,4 +8,10 @@ class ClassLookup
     {
         return ($object instanceof \Doctrine\ORM\Proxy\Proxy) ? get_parent_class($object) : get_class($object);
     }
+    
+    public static function getClassName($object)
+    {
+    	$classname = explode('\\', ClassLookup::getClass($object));
+    	return array_pop($classname);;
+    }
 }
