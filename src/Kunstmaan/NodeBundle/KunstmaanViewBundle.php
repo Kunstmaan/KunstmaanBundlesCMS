b37b058b0c06bf672cd794e61b365ab64bb2c891
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-01-26 17:06:04 +0100

    error page in viewbundle

--- src/Kunstmaan/NodeBundle/KunstmaanViewBundle.php
+++ src/Kunstmaan/NodeBundle/KunstmaanViewBundle.php
@@ -2,8 +2,13 @@
 
 namespace Kunstmaan\ViewBundle;
 
+use Symfony\Bundle\TwigBundle\TwigBundle;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KunstmaanViewBundle extends Bundle
 {
+	public function getParent(){
+		return "TwigBundle";
+	}
 }
