36d20f1e06c4a1128e3bcc29768739648848d46e
Kim Ausloos <kim.ausloos@kunstmaan.be>
2012-01-26 11:02:54 +0100

    Added highlights to search

---
+++ src/Kunstmaan/SearchBundle/Transformers/ElasticaTransformer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Kunstmaan\SearchBundle\Transformers;
+
+use FOQ\ElasticaBundle\Doctrine\ORM\ElasticaToModelTransformer;
+class ElasticaTransformer extends ElasticaToModelTransformer {
+
+    public function __construct($objectManager, $objectClass, array $options = array()) {
+        $this->objectManager = $objectManager;
+        $this->objectClass   = $objectClass;
+        $this->options       = array_merge($this->options, $options);
+    }
+
+    /**
+     * Transforms an array of elastica objects into an array of
+     * model objects fetched from the doctrine repository
+     *
+     * @param array of elastica ob
+     * @return array
+     **/
+    public function transform(array $elasticaObjects) {
+        $ids = array_map(function($elasticaObject) {
+            return $elasticaObject->getId();
+        }, $elasticaObjects);
+
+        $objects = $this->findByIdentifiers($this->objectClass, $this->options['identifier'], $ids, $this->options['hydrate']);
+        $identifierGetter = 'get'.ucfirst($this->options['identifier']);
+
+        // sort objects in the order of ids
+        $idPos = array_flip($ids);
+        usort($objects, function($a, $b) use ($idPos, $identifierGetter) {
+            return $idPos[$a->$identifierGetter()] > $idPos[$b->$identifierGetter()];
+        });
+
+        foreach($objects as &$object) {
+            foreach($elasticaObjects as $elasticaObject) {
+                $id = $elasticaObject->getParam('_id');
+                if($object->getId() == $id) {
+                    $object->highlight = $elasticaObject->getParam('highlight');
+                    break;
+                }
+            }
+        }
+
+        return $objects;
+    }
+}
