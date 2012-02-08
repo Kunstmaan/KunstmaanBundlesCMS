9850ac84a02d0c0029dbf636fa153d2cc92c6a61
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-02-08 13:58:35 +0100

    throw httpexception if folder not fount

--- src/Kunstmaan/MediaBundle/Repository/FolderRepository.php
+++ src/Kunstmaan/MediaBundle/Repository/FolderRepository.php
@@ -5,6 +5,7 @@ namespace Kunstmaan\MediaBundle\Repository;
 use Kunstmaan\MediaBundle\Entity\Folder;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * FolderRepository
@@ -70,7 +71,7 @@ class FolderRepository extends EntityRepository
     {
     	$folder = $em->getRepository('KunstmaanMediaBundle:Folder')->find($folder_id);
     	if (!$folder) {
-    		throw $this->createNotFoundException('Unable to find folder.');
+    		throw new NotFoundHttpException('The id given for the folder is not valid.');
     	}
     	return $folder;
     }
