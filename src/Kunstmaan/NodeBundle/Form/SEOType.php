3e6b810434aac2a9fc8d36d72d231db3728d3178
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-02-15 17:19:37 +0100

    seotype fix

--- src/Kunstmaan/NodeBundle/Form/SEOType.php
+++ src/Kunstmaan/NodeBundle/Form/SEOType.php
@@ -1,9 +1,7 @@
 <?php
-// src/Blogger/BlogBundle/Form/EnquiryType.php
+// Kunstmaan\AdminNodeBundle\Form\SEOType.php
 
 namespace Kunstmaan\AdminNodeBundle\Form;
-
-use Symfony\Component\DependencyInjection\Container;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilder;
