737d6a7c60a09a5248e992ce81ad1922d7592926
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-02-15 14:32:56 +0100

    fix nodegenerator

--- src/Kunstmaan/NodeBundle/Listener/NodeGenerator.php
+++ src/Kunstmaan/NodeBundle/Listener/NodeGenerator.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: kris
- * Date: 14/11/11
- * Time: 15:48
- * To change this template use File | Settings | File Templates.
- */
+* Created by JetBrains PhpStorm.
+* User: kris
+* Date: 14/11/11
+* Time: 15:48
+* To change this template use File | Settings | File Templates.
+*/
 
 namespace Kunstmaan\AdminNodeBundle\Listener;
 
@@ -30,50 +30,18 @@ class NodeGenerator {
         $em = $args->getEntityManager();
         $classname = ClassLookup::getClass($entity);
         if($entity instanceof HasNode){
-<<<<<<< HEAD
             $entityrepo = $em->getRepository($classname);
             $nodeVersion = $em->getRepository('KunstmaanAdminNodeBundle:NodeVersion')->getNodeVersionFor($entity);
             if($nodeVersion!=null){
-            	$nodeTranslation = $nodeVersion->getNodeTranslation();
-            	if($nodeTranslation->getPublicNodeVersion() && $nodeTranslation->getPublicNodeVersion()->getId() == $nodeVersion->getId()){
-            		$nodeTranslation->setTitle($entity->__toString());
-            		$nodeTranslation->setSlug(Slugifier::slugify($entity->__toString()));
-            		$nodeTranslation->setOnline($entity->isOnline());
-            		$em->persist($nodeTranslation);
-            		$em->flush();
-            	}
+	             $nodeTranslation = $nodeVersion->getNodeTranslation();
+	             if($nodeTranslation->getPublicNodeVersion() && $nodeTranslation->getPublicNodeVersion()->getId() == $nodeVersion->getId()){
+		             $nodeTranslation->setTitle($entity->__toString());
+		             $nodeTranslation->setSlug(Slugifier::slugify($entity->__toString()));
+		             $nodeTranslation->setOnline($entity->isOnline());
+		             $em->persist($nodeTranslation);
+		             $em->flush();
+	             }
             }
-=======
-        	$entityrepo = $em->getRepository($classname);
-            $node = $this->getNode($em, $entity->getId(), $classname);
-            if($node==null){
-                $node = new Node();
-                $node->setRefId($entity->getId());
-                $node->setRefEntityname($classname);
-            }
-            $parent = $entity->getParent();
-            if($parent){
-            	$parentNode = $em->getRepository('KunstmaanAdminNodeBundle:Node')->findOneBy(array('refId' => $parent->getId(), 'refEntityname' => ClassLookup::getClass($parent)));
-            	$node->setParent($parentNode);
-            }
-            $node->setTitle($entity->__toString());
-            $node->setSlug(strtolower(str_replace(" ", "-", $entity->__toString())));
-            $node->setOnline($entity->isOnline());
-            $node->setSequencenumber(1);
-            
-            if($node->getParent()==null){
-            	$node->setSequencenumber(1);
-            	var_dump("blub ". $node->getId());
-            }else{
-            	$parent = $node->getParent();
-            	$children = $em->getRepository('KunstmaanAdminNodeBundle:Node')->findBy(array('parent' => ClassLookup::getClass($node)));
-            	$node->setSequencenumber(sizeof($children));
-            	var_dump($node->getId());
-            }
-            
-            $em->persist($node);
-            $em->flush();
->>>>>>> 562825a6c9394743a800c27ccc6b8c67d4631370
         }
     }
 
@@ -82,36 +50,36 @@ class NodeGenerator {
     }
 
     public function preRemove(LifecycleEventArgs $args) {
-    	/*$entity = $args->getEntity();
-    	$em = $args->getEntityManager();
-    	$classname = ClassLookup::getClass($entity);
-    	if($entity instanceof HasNode){
-    		$entityrepo = $em->getRepository($classname);
-    		$node = $this->getNode($em, $entity->getId(), $classname);
-    		$em->remove($node);
-    	}*/
+	    /*$entity = $args->getEntity();
+		$em = $args->getEntityManager();
+		$classname = ClassLookup::getClass($entity);
+		if($entity instanceof HasNode){
+		$entityrepo = $em->getRepository($classname);
+		$node = $this->getNode($em, $entity->getId(), $classname);
+		$em->remove($node);
+		}*/
     }
 
     public function postLoad(LifecycleEventArgs $args) {
-    	$entity = $args->getEntity();
-    	$em = $args->getEntityManager();
-    	$classname = ClassLookup::getClass($entity);
-    	if($entity instanceof HasNode){
-    		$nodeVersion = $em->getRepository('KunstmaanAdminNodeBundle:NodeVersion')->findOneBy(array('refId' => $entity->getId(), 'refEntityname' => $classname));
-    		if($nodeVersion){
-    			$nodeTranslation = $nodeVersion->getNodeTranslation();
-    			$node = $nodeTranslation->getNode();
-    			$parentNode = $node->getParent();
-    			if($parentNode){
-    				$parentNodeTranslation = $parentNode->getNodeTranslation($nodeTranslation->getLang());
-    				if($parentNodeTranslation){
-    					$parentNodeVersion = $parentNodeTranslation->getPublicNodeVersion();
-    					$parent = $em->getRepository($parentNode->getRefEntityname())->find($parentNodeVersion->getRefId());
-    					$entity->setParent($parent);
-    				}
-    			}
-    		}
-    	}
+     	$entity = $args->getEntity();
+     	$em = $args->getEntityManager();
+     	$classname = ClassLookup::getClass($entity);
+     	if($entity instanceof HasNode){
+     		$nodeVersion = $em->getRepository('KunstmaanAdminNodeBundle:NodeVersion')->findOneBy(array('refId' => $entity->getId(), 'refEntityname' => $classname));
+     		if($nodeVersion){
+     			$nodeTranslation = $nodeVersion->getNodeTranslation();
+     			$node = $nodeTranslation->getNode();
+     			$parentNode = $node->getParent();
+     			if($parentNode){
+     				$parentNodeTranslation = $parentNode->getNodeTranslation($nodeTranslation->getLang());
+     				if($parentNodeTranslation){
+     					$parentNodeVersion = $parentNodeTranslation->getPublicNodeVersion();
+     					$parent = $em->getRepository($parentNode->getRefEntityname())->find($parentNodeVersion->getRefId());
+     					$entity->setParent($parent);
+     				}
+     			}
+     		}
+     	}
     }
 
 }
