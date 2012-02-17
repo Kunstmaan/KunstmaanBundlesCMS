ed89c11ea8ed3eb4fb801280d4de0c180d215b68
Kristof Van Cauwenbergh <kristof.van.cauwenbergh@kunstmaan.be>
2012-02-17 11:34:36 +0100

    exceptions

--- src/Kunstmaan/AdminBundle/Modules/LogHandler.php
+++ src/Kunstmaan/AdminBundle/Modules/LogHandler.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\Container;
 
 use Doctrine\ORM\EntityManager;
 
-use Kunstmaan\AdminBundle\Entity\LogItem;
+use Kunstmaan\AdminBundle\Entity\ErrorLogItem;
 use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 
@@ -29,38 +29,29 @@ class LogHandler extends AbstractProcessingHandler
             $this->initialize();
         }
 
-        	$this->container->get('monolog.logger.doctrine')->pushHandler(new NullHandler());
-	        
-	        /*$logitem = new ErrorLogItem();        
+        $this->container->get('monolog.logger.doctrine')->pushHandler(new NullHandler());
+	       	        
+	    try{
+	    	$logitem = new ErrorLogItem();
 	        $logitem->setChannel($record['channel']);
 	        $logitem->setLevel($record['level']);
 	        $logitem->setMessage($record['formatted']);
-	        $logitem->setCreatedAt($record['datetime']);
-	
+		    $em = $this->container->get('doctrine')->getEntityManager();
+			$em->persist($logitem);
+			$em->flush();
 	        /*$conn = $this->container->get('doctrine')->getEntityManager()->getConnection()->getWrappedConnection();
-	        $prep = $conn->prepare('INSERT INTO logitem (channel, level, message, createdat) VALUES (?, ?, ?, ?)');
-	        $prep->bindParam(1, $record['channel']); 
+	        $prep = $conn->prepare('INSERT INTO errorlogitem (channel, level, message) VALUES (?, ?, ?)');
+	        $prep->bindParam(1, $record['channel']);
 	        $prep->bindParam(2, $record['level']);
 	        $prep->bindParam(3, $record['formatted']);
-	        $datetime = $record['datetime']->format('U'); 
-	        $prep->bindParam(4, $datetime);
 	        $prep->execute();*/
-	        
-	        try{
-		       	/*$em = $this->container->get('doctrine')->getEntityManager();
-			    $em->persist($logitem);
-			    $em->flush();*/
-	        	$conn = $this->container->get('doctrine')->getEntityManager()->getConnection()->getWrappedConnection();
-	        	$prep = $conn->prepare('INSERT INTO errorlogitem (channel, level, message) VALUES (?, ?, ?)');
-	        	$prep->bindParam(1, $record['channel']);
-	        	$prep->bindParam(2, $record['level']);
-	        	$prep->bindParam(3, $record['formatted']);
-	        	$prep->execute();
-	        }catch(\PDOException $e){
-	        	
-	        }
+	     }catch(\PDOException $e){
+	     	// catching the exception during fullreload: errorlogitem table not found
+	     }catch(\ORMException $e){
+	     	// catching the exception during fullreload: EntityManager not set
+	     }
 		    
-		    $this->container->get('monolog.logger.doctrine')->popHandler();
+		 $this->container->get('monolog.logger.doctrine')->popHandler();
 	    
     }
 
