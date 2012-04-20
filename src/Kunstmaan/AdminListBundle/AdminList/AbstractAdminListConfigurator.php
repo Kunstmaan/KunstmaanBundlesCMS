c50a1f3a499ae36fd401e5ce4d00a550c8406db8
Kris Pypen <kris.pypen@kunstmaan.be>
2012-04-20 12:30:19 +0200

    feature/menu-rework

--- src/Kunstmaan/AdminListBundle/AdminList/AbstractAdminListConfigurator.php
+++ src/Kunstmaan/AdminListBundle/AdminList/AbstractAdminListConfigurator.php
@@ -1,93 +1,104 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: kris
- * Date: 15/11/11
- * Time: 22:32
- * To change this template use File | Settings | File Templates.
- */
 
 namespace Kunstmaan\AdminListBundle\AdminList;
+abstract class AbstractAdminListConfigurator
+{
 
-abstract class AbstractAdminListConfigurator {
-
-    public function buildFilters(AdminListFilter $builder){
+    public function buildFilters(AdminListFilter $builder)
+    {
 
     }
 
-	function getSortFields() {
+    function getSortFields()
+    {
         $array = array();
-        foreach($this->getFields() as $field){
-        	if($field->isSortable()) $array[] = $field->getFieldname();
+        foreach ($this->getFields() as $field) {
+            if ($field->isSortable())
+                $array[] = $field->getFieldname();
         }
         return $array;
     }
 
-    private $fields;
+    private $fields = array();
 
-    function getFields(){
-    	return $this->fields;
+    function getFields()
+    {
+        return $this->fields;
     }
 
     abstract function buildFields();
-
-    function addField($fieldname, $fieldheader, $sort){
-    	$this->fields[] = new Field($fieldname, $fieldheader, $sort);
+    
+    function addField($fieldname, $fieldheader, $sort)
+    {
+        $this->fields[] = new Field($fieldname, $fieldheader, $sort);
     }
 
-   function configureListFields(&$array){
-   		foreach($this->getFields() as $field){
-   			$array[$field->getFieldheader()] = $field->getFieldname();
-   		}
-   }
+    function configureListFields(&$array)
+    {
+        foreach ($this->getFields() as $field) {
+            $array[$field->getFieldheader()] = $field->getFieldname();
+        }
+    }
 
-    abstract function canEdit();
+    public function canEdit()
+    {
+        return true;
+    }
 
     abstract function getEditUrlFor($item);
 
-    abstract function canAdd();
+    public function canAdd()
+    {
+        return true;
+    }
 
-    abstract function getAddUrlFor($params=array());
+    abstract function getAddUrlFor($params = array());
 
-    abstract function canDelete($item);
+    public function canDelete($item)
+    {
+        return true;
+    }
 
     abstract function getRepositoryName();
 
-    function adaptQueryBuilder($querybuilder, $params=array()){
+    function adaptQueryBuilder($querybuilder, $params = array())
+    {
 
     }
 
-    function getValue($item, $columnName){
-        $methodName = "get".$columnName;
-        if(method_exists($item, $methodName)){
+    function getValue($item, $columnName)
+    {
+        $methodName = "get" . $columnName;
+        if (method_exists($item, $methodName)) {
             $result = $item->$methodName();
         } else {
-            $methodName = "is".$columnName;
-            if(method_exists($item, $methodName)){
+            $methodName = "is" . $columnName;
+            if (method_exists($item, $methodName)) {
                 $result = $item->$methodName();
             } else {
                 return "undefined function";
             }
         }
-        if($result instanceof \DateTime){
+        if ($result instanceof \DateTime) {
             return $result->format('Y-m-d H:i:s');
-        } else if($result instanceof \Doctrine\ORM\PersistentCollection) {
+        } else if ($result instanceof \Doctrine\ORM\PersistentCollection) {
             $results = "";
-            foreach($result as $entry) {
+            foreach ($result as $entry) {
                 $results[] = $entry->getName();
             }
             if (empty($results)) {
                 return "";
             }
             return implode(', ', $results);
-        } else if(is_array($result)) {
+        } else if (is_array($result)) {
             return implode(', ', $result);
         } else {
             return $result;
         }
     }
 
-    function getLimit(){
+    function getLimit()
+    {
         return 10;
     }
 }
