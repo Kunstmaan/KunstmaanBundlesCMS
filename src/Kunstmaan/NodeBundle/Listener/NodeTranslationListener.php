9a8cb972a6f561ae4a449365db13d2deda031665
Kim Ausloos <kim.ausloos@kunstmaan.be>
2012-04-26 08:27:36 +0200

    Added some comments & made it nicer

--- src/Kunstmaan/NodeBundle/Listener/NodeTranslationListener.php
+++ src/Kunstmaan/NodeBundle/Listener/NodeTranslationListener.php
@@ -5,30 +5,68 @@ namespace Kunstmaan\AdminNodeBundle\Listener;
 use Kunstmaan\AdminNodeBundle\Entity\NodeTranslation;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
+/**
+ * Listens to doctrine postPersist and postUpdate events and updates
+ * the urls if the entities are nodetranslations
+ */
 class NodeTranslationListener
 {
 
-    public function postUpdate(LifecycleEventArgs $args) {
+    /**
+     * Runs the postUpdate doctrine event and updates the nodetranslation urls if needed
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     */
+    public function postUpdate(LifecycleEventArgs $args)
+    {
         $entity = $args->getEntity();
-        $em = $args->getEntityManager();
 
         if ($entity instanceof NodeTranslation) {
-            $entity->setUrl($entity->getFullSlug());
+            $em     = $args->getEntityManager();
 
-            $children = $entity->getNode()->getChildren();
-            if (count($children) > 0) {
-                foreach($children as $child) {
-                    $translation = $child->getNodeTranslation($entity->getLang(), true);
-                    if ($translation) {
-                        $translation->setUrl($translation->getFullSlug());
-                        $em->persist($translation);
-                    }
+            $entity = $this->updateUrl($entity);
+            $this->updateNodeChildren($entity, $em);
+
+            $em->persist($entity);
+        }
+    }
+
+    /**
+     * Runs the postPersist doctrine event and updates the nodetranslation urls if needed
+     * @param \Doctrine\ORM\Event\LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $this->postUpdate($args);
+    }
+
+    /**
+     * Checks if a nodetranslation has children and update their url
+     * @param \Kunstmaan\AdminNodeBundle\Entity\NodeTranslation $node
+     * @param $em
+     */
+    private function updateNodeChildren(NodeTranslation $node, $em)
+    {
+        $children = $node->getNode()->getChildren();
+        if (count($children) > 0) {
+            foreach ($children as $child) {
+                $translation = $child->getNodeTranslation($node->getLang(), true);
+                if ($translation) {
+                    $translation->setUrl($translation->getFullSlug());
+                    $em->persist($translation);
                 }
             }
         }
     }
 
-    public function postPersist(LifecycleEventArgs $args) {
-        $this->postUpdate($args);
+    /**
+     * Update the url for a nodetranslation
+     * @param \Kunstmaan\AdminNodeBundle\Entity\NodeTranslation $node
+     *
+     * @return \Kunstmaan\AdminNodeBundle\Entity\NodeTranslation
+     */
+    private function updateUrl(NodeTranslation $node)
+    {
+        return $node->setUrl($node->getFullSlug());
     }
+
 }
