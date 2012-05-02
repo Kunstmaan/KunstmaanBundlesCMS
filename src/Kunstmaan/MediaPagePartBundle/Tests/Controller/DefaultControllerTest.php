0ad5cc5fa1726821e8b80709213c763add938105
Kris Pypen <kris.pypen@kunstmaan.be>
2012-05-02 20:02:27 +0200

    wrong namespace

--- src/Kunstmaan/MediaPagePartBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Kunstmaan/MediaPagePartBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kunstmaan\SearchBundle\Tests\Controller;
+namespace Kunstmaan\MediaPagePartBundle\Tests\Controller;
 
 class DefaultControllerTest extends \PHPUnit_Framework_TestCase
 {
