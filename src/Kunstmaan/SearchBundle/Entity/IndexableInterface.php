79122dabe5c1f4df0a22c092b1599a10d352a9a2
Kris Pypen <kris.pypen@kunstmaan.be>
2012-05-10 16:48:28 +0200

    abstractentities_and_cleanup

---
+++ src/Kunstmaan/SearchBundle/Entity/IndexableInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kunstmaan\SearchBundle\Entity;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * IndexableInterface
+ */
+interface IndexableInterface
+{
+
+    /**
+     * @param ContainerInterface $container The container
+     * @param object             $entity    The object to index
+     */
+    function getContentForIndexing(ContainerInterface $container, $entity);
+}
