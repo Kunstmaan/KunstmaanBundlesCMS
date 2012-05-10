4a598de27bb860750d2988c18b2f9f49f79c04b9
Kris Pypen <kris.pypen@kunstmaan.be>
2012-05-10 11:29:34 +0200

    there must be one test

---
+++ src/Kunstmaan/FormBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kunstmaan\SearchBundle\Tests\Controller;
+
+class DefaultControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIndex()
+    {
+        return true;
+    }
+}
