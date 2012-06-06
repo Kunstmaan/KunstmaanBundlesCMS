36c4501e1ce28060497c36cab8289bd94a93b650
Ibe Vanmeenen <ibe.vanmeenen@kunstmaan.be>
2012-06-06 17:58:42 +0200

    fix js tree

--- src/Kunstmaan/AdminBundle/Resources/public/js/script.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/script.js
@@ -10,9 +10,9 @@ $(document).ready(function () {
 });
 
 // JS-tree
-function init_tree() {
+function init_tree(movepagesnodeurl, movemedianodeurl) {
 	$('.tree').jstree({
-		"plugins" : [ "themes", "html_data", "types", "search" ],
+		"plugins" : [ "themes", "html_data", "types", "search" ] ,
 		"themes" : { 
 			"theme" : "OMNext",
 			"dots" : true,
@@ -23,55 +23,256 @@ function init_tree() {
 				//Page
 				"page" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-57px -57px" 
 					}
 				},
 				"page_offline" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-57px -74px" 
 					}
-				},			
+				},				
 				//Site
 				"site" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-75px -38px" 
 					}
 				},
 				//Settings
 				"settings" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-57px -37px" 
 					}
 				},
 				//Image
 				"image" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-20px -74px" 
 					}
 				},
 				//Video
 				"video" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-75px -55px" 
 					}
 				},
 				//Slideshow
 				"slideshow" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-2px -72px" 
 					}
 				},
 				//Files
 				"files" : {
 					"icon" : {
-						"image" : "js/libs/themes/OMNext/d.png",
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-38px -72px" 
+					}
+				}
+			}
+		},
+        // Configuring the search plugin
+        "search" : {
+            "show_only_matches" : true
+        }
+	});
+    $("#treeform").submit(function() {
+        $('.tree').jstree('search', $('#treeform #searchVal').val());
+        return false;
+    });
+    $('#treeform #searchVal').keyup(function() {
+        $('.tree').jstree('search', $('#treeform #searchVal').val());
+    })
+
+	$('.pagestree').jstree({
+		"plugins" : [ "themes", "html_data", "dnd", "crrm", "types", "search" ] ,
+		"themes" : { 
+			"theme" : "OMNext",
+			"dots" : true,
+			"icons" : true		
+		},
+		"types" : {
+			"types" : {
+				//Page
+				"page" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -57px" 
+					}
+				},
+				"page_offline" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -74px" 
+					}
+				},		
+				//Site
+				"site" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -38px" 
+					}
+				},
+				//Settings
+				"settings" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -37px" 
+					}
+				},
+				//Image
+				"image" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-20px -74px" 
+					}
+				},
+				//Video
+				"video" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -55px" 
+					}
+				},
+				//Slideshow
+				"slideshow" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-2px -72px" 
+					}
+				},
+				//Files
+				"files" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-38px -72px" 
+					}
+				}
+			}
+		},
+		"crrm" : { 
+			"move" : {
+				"check_move" : function (m) { 
+					var p = this._get_parent(m.o);
+					if(!p) return false;
+					p = p == -1 ? this.get_container() : p;
+					if(p === m.np) return true;
+					if(p[0] && m.np[0] && p[0] === m.np[0]) return true;
+					return false;
+				}
+			}
+		},
+		"dnd" : {
+			"drop_target" : false,
+			"drag_target" : false
+		},
+		// Configuring the search plugin
+		"search" : {
+            "show_only_matches" : true
+		}
+	})
+	.bind("move_node.jstree", function (e, data) {
+		console.log("parentid" + data.rslt.np.attr("id"));
+		console.log("fromposition" + data.rslt.o.attr("sequence"));
+		console.log("afterposition" + data.rslt.cp);
+		$.ajax({
+				async : false,
+				type: 'POST',
+				url: movepagesnodeurl,
+				data : { 
+					"parentid": data.rslt.np.attr("id"),
+					"fromposition": data.rslt.o.attr("sequence"),
+					"afterposition" : data.rslt.cp
+				},
+				success : function (r) {
+					if(!r.status) {
+						$.jstree.rollback(data.rlbk);
+					}
+					else {
+						$(data.rslt.oc).attr("id", "node_" + r.id);
+						if(data.rslt.cy && $(data.rslt.oc).children("ul").length) {
+							data.inst.refresh(data.inst._get_parent(data.rslt.oc));
+						}
+					}
+					$("#analyze").click();
+				}
+		});
+	});
+    $("#pagestreeform").submit(function() {
+        $('.pagestree').jstree('search', $('#pagestreeform #searchVal').val());
+        return false;
+    });
+    $('#pagestreeform #searchVal').keyup(function() {
+        $('.pagestree').jstree('search', $('#pagestreeform #searchVal').val());
+    })
+
+	$('.mediatree').jstree({
+		"plugins" : [ "themes", "html_data", "dnd", "crrm", "types", "search" ] ,
+		"themes" : { 
+			"theme" : "OMNext",
+			"dots" : true,
+			"icons" : true		
+		},
+		"types" : {
+			"types" : {
+				//Page
+				"page" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -57px" 
+					}
+				},
+				"page_offline" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -74px" 
+					}
+				},				
+				//Site
+				"site" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -38px" 
+					}
+				},
+				//Settings
+				"settings" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-57px -37px" 
+					}
+				},
+				//Image
+				"image" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-20px -74px" 
+					}
+				},
+				//Video
+				"video" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-75px -55px" 
+					}
+				},
+				//Slideshow
+				"slideshow" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
+						"position" : "-2px -72px" 
+					}
+				},
+				//Files
+				"files" : {
+					"icon" : {
+						"image" : $.jstree._themes + "OMNext/d.png",
 						"position" : "-38px -72px" 
 					}
 				}
@@ -79,24 +280,58 @@ function init_tree() {
 		
 		
 		},
-		// Configuring the search plugin
-		"search" : {
-			"ajax" : {
-			    "url" : "", //<-- Link naar php
-			    "data" : function (str) {
-			        return {
-			            "operation" : "search",
-			            "search_str" : str
-			        };
-			    }
+		"crrm" : { 
+			"move" : {
+				"check_move" : function (m) { 
+					var p = this._get_parent(m.o);
+					if(!p) return false;
+					p = p == -1 ? this.get_container() : p;
+					if(p === m.np) return true;
+					if(p[0] && m.np[0] && p[0] === m.np[0]) return true;
+					return false;
+				}
 			}
-		}
+		},
+		"dnd" : {
+			"drop_target" : false,
+			"drag_target" : false
+		},
+        // Configuring the search plugin
+        "search" : {
+            "show_only_matches" : true
+        }
+	})
+	.bind("move_node.jstree", function (e, data) {
+		$.ajax({
+				async : false,
+				type: 'POST',
+				url: movemedianodeurl,
+				data : { 
+					"parentid": data.rslt.np.attr("id"),
+					"fromposition": data.rslt.o.attr("sequence"),
+					"afterposition" : data.rslt.cp
+				},
+				success : function (r) {
+					if(!r.status) {
+						$.jstree.rollback(data.rlbk);
+					}
+					else {
+						$(data.rslt.oc).attr("id", "node_" + r.id);
+						if(data.rslt.cy && $(data.rslt.oc).children("ul").length) {
+							data.inst.refresh(data.inst._get_parent(data.rslt.oc));
+						}
+					}
+					$("#analyze").click();
+				}
+		});
 	});
-	$("#search").click(function() {
-		$(this).jstree("search", document.getElementById("searchVal").value);
-	});
-	
-	
+    $("#mediatreeform").submit(function() {
+        $('.mediatree').jstree('search', $('#mediatreeform #searchVal').val());
+        return false;
+    });
+    $('#mediatreeform #searchVal').keyup(function() {
+        $('.mediatree').jstree('search', $('#mediatreeform #searchVal').val());
+    })
 }
 
 // Drag and Drop
