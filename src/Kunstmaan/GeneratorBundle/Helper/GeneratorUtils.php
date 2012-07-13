faa1987984136ab9f45b87a5ee2e131c92c02e26
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2012-07-13 13:27:52 +0200

    Generator Utils helper

---
+++ src/Kunstmaan/GeneratorBundle/Helper/GeneratorUtils.php
@@ -0,0 +1,32 @@
+<?php
+namespace Kunstmaan\GeneratorBundle\Helper;
+
+use Doctrine\ORM\Mapping\ClassMetadata;
+
+class GeneratorUtils
+{
+    /**
+     * Returns an array of fields. Fields can be both column fields and
+     * association fields.
+     *
+     * @param ClassMetadataInfo $metadata
+     * @return array $fields
+     */
+    public static function getFieldsFromMetadata(ClassMetadata $metadata)
+    {
+        $fields = (array) $metadata->fieldNames;
+    
+        // Remove the primary key field if it's not managed manually
+        if (!$metadata->isIdentifierNatural()) {
+            $fields = array_diff($fields, $metadata->identifier);
+        }
+    
+        foreach ($metadata->associationMappings as $fieldName => $relation) {
+            if ($relation['type'] !== ClassMetadata::ONE_TO_MANY) {
+                $fields[] = $fieldName;
+            }
+        }
+    
+        return $fields;
+    }
+}
