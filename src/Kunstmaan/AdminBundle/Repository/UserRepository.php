4fdad33f0fe0fb1b4aa0a03adb3ca1eb5ded0e95
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-07-18 16:27:02 +0200

    added getUsersByRole

--- src/Kunstmaan/AdminBundle/Repository/UserRepository.php
+++ src/Kunstmaan/AdminBundle/Repository/UserRepository.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Kunstmaan\AdminBundle\Repository;
-
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Doctrine\ORM\EntityRepository;
@@ -9,13 +8,41 @@ use Doctrine\ORM\EntityRepository;
 class UserRepository extends EntityRepository
 {
 
-	public function getUser($user_id, EntityManager $em)
-	{
-		$user = $em->getRepository('KunstmaanAdminBundle:User')->find($user_id);
-		if (!$user) {
-			throw new NotFoundHttpException('The id given for the user is not valid.');
-		}
-		return $user;
-	}
-	
+    public function getUser($user_id, EntityManager $em)
+    {
+        $user = $em->getRepository('KunstmaanAdminBundle:User')->find($user_id);
+        if (!$user) {
+            throw new NotFoundHttpException('The id given for the user is not valid.');
+        }
+        return $user;
+    }
+
+    /**
+     *
+     * @param string|array $role The role(s) for which you want to retrieve the users
+     *
+     * @return array
+     */
+    public function getUsersByRole($role)
+    {
+        if (is_array($role)) {
+            $roles = $role;
+        } else {
+            $roles = array($role);
+        }
+        
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb->select('u')
+            ->from('KunstmaanAdminBundle:User', 'u')
+            ->innerJoin('u.groups', 'g')
+            ->innerJoin('g.roles', 'r')
+            ->where('u.enabled=1')
+            ->andWhere('u.locked=0')
+            ->andWhere('u.expired=0')
+            ->andWhere('r.role IN (:roles)')
+            ->setParameter('roles', $roles);
+        
+        return $qb->getQuery()->getResult();
+    }
+    
 }
