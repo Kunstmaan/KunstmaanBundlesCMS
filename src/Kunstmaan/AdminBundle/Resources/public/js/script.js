e4e038c7622e09e02bfa8359580e2b137450fc33
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-07-19 16:06:53 +0200

    fix for adminlist filters

--- src/Kunstmaan/AdminBundle/Resources/public/js/script.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/script.js
@@ -472,16 +472,6 @@ function createFilter(el, hide, options){
     newitem.find(".uniquefilterid").val(uniqueid);
     newitem.find(".filterdummy").val(line.find(".filterselect").val());
     updateOptions(newitem.find(".filterdummy"), options);
-    newitem.find("input, select").each(function(){
-        $(this).attr("name", "filter_" + $(this).attr("name"));
-    });
-    
-    newitem.find(".filteroptions").find("input:not(.uniquefilterid), select").each(function(){
-        $(this).attr("name", $(this).attr("name") + "_" + uniqueid);
-		if($(this).hasClass("datepick")){
-			$(this).datePicker(options);				
-		}
-    });
     
     if(hide == true){
         newitem.removeClass("hidden");
@@ -533,8 +523,18 @@ function calculateUniqueFilterId(){
 function updateOptions(el, options){
     var val = $(el).val();
     val = val.replace('.',  '_');
+    var uniqueid = $(el).parent(".filterline").find(".uniquefilterid").val();
     $(el).parent(".filterline").find(".filteroptions").html($('#filterdummyoptions_'+ val).html());
-   	$(el).parent(".filterline").find(".datepick").datePicker(options);
+    $(el).parent(".filterline").find("input, select").each(function(){
+    	var fieldName = $(this).attr("name");
+    	if (fieldName.substr(0, 7) != "filter_") {
+    		$(this).attr("name", "filter_" + $(this).attr("name"));
+    	}
+    });    
+    $(el).parent(".filterline").find(".filteroptions").find("input:not(.uniquefilterid), select").each(function(){
+        $(this).attr("name", $(this).attr("name") + "_" + uniqueid);
+		if($(this).hasClass("datepick")){
+			$(this).datePicker(options);				
+		}
+    });
 }
-
-
