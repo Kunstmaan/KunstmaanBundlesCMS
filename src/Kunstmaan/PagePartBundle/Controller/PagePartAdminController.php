e796de2c214ef5963d58b638e067cf413b62fc86
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2012-08-20 10:24:25 +0200

    Cleanup the PagePartAdminController
    Remove duplicate code, typecasting, docs, formatting

--- src/Kunstmaan/PagePartBundle/Controller/PagePartAdminController.php
+++ src/Kunstmaan/PagePartBundle/Controller/PagePartAdminController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace Kunstmaan\PagePartBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Kunstmaan\PagePartBundle\Entity\PagePartRef;
+use Kunstmaan\PagePartBundle\Repository\PagePartRefRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Kunstmaan\AdminNodeBundle\Modules\NodeMenu;
@@ -10,61 +11,70 @@ use Kunstmaan\AdminNodeBundle\Modules\NodeMenu;
 class PagePartAdminController extends Controller
 {
 
-    public function indexAction() {
+    public function indexAction()
+    {
         return $this->render('KunstmaanPagePartBundle:PagePartAdmin:index.html.twig');
     }
 
-    public function moveUpAction($id) {
-        $em = $this->getDoctrine()->getEntityManager();
+    /**
+     * Moves a PagePartRef in a certain direction.
+     *
+     * @param $id       the id of the pagepartref
+     * @param $steps    amount of steps to move, 1 for one up, -1 for one down
+     */
+    private function movePagePartRef($id, $steps)
+    {
+        $em = $this->getDoctrine()->getManager();
+        /** @var PagePartRefRepository $repo  */
         $repo = $em->getRepository('KunstmaanPagePartBundle:PagePartRef');
+        /** @var PagePartRef $pagepartref  */
         $pagepartref = $repo->find($id);
-        $pagepartrefs = $repo->findBy(array('context' => $pagepartref->getContext(), 'pageId' => $pagepartref->getPageId(), 'pageEntityname' => $pagepartref->getPageEntityName()));
-        foreach ($pagepartrefs as &$ppref){
-            if($ppref->getSequenceNumber()+1==$pagepartref->getSequenceNumber()){
+        /** @var PagePartRef[] $pagepartrefs  */
+        $pagepartrefs = $repo->findBy(array('context' => $pagepartref->getContext(),
+                                            'pageId' => $pagepartref->getPageId(),
+                                            'pageEntityname' => $pagepartref->getPageEntityName()));
+        foreach ($pagepartrefs as &$ppref) {
+            if ($ppref->getSequenceNumber() + $steps == $pagepartref->getSequenceNumber()) {
                 $ppref->setSequenceNumber($pagepartref->getSequenceNumber());
                 $em->persist($ppref);
             }
         }
-        if($pagepartref->getSequenceNumber()>1){
-            $pagepartref->setSequenceNumber($pagepartref->getSequenceNumber()-1);
-            $em->persist($pagepartref);
-        }
-        $em->flush();
-    }
-
-    public function moveDownAction($id) {
-        $em = $this->getDoctrine()->getEntityManager();
-        $repo = $em->getRepository('KunstmaanPagePartBundle:PagePartRef');
-        $pagepartref = $repo->find($id);
-        $pagepartrefs = $repo->findBy(array('context' => $pagepartref->getContext(), 'pageId' => $pagepartref->getPageId(), 'pageEntityname' => $pagepartref->getPageEntityName()));
-        foreach ($pagepartrefs as &$ppref){
-            if($ppref->getSequenceNumber()-1==$pagepartref->getSequenceNumber()){
-                $ppref->setSequenceNumber($pagepartref->getSequenceNumber());
-                $em->persist($ppref);
-            }
-        }
-        if($pagepartref->getSequenceNumber()<count($pagepartrefs)){
-            $pagepartref->setSequenceNumber($pagepartref->getSequenceNumber()+1);
+        if ($pagepartref->getSequenceNumber() > 1) {
+            $pagepartref->setSequenceNumber($pagepartref->getSequenceNumber() - 1);
             $em->persist($pagepartref);
         }
         $em->flush();
     }
 
     /**
-     * @Route("/pageparts/selecturl", name="KunstmaanPagePartBundle_selecturl")
+     * @param int $id
+     */
+    public function moveUpAction($id)
+    {
+        $this->movePagePartRef($id, 1);
+    }
+
+    /**
+     * @param int $id
+     */
+    public function moveDownAction($id)
+    {
+       $this->movePagePartRef($id, -1);
+    }
+
+    /**
+     * @Route   ("/pageparts/selecturl", name="KunstmaanPagePartBundle_selecturl")
      * @Template()
      */
-    public function selectlinkAction(){
-        $em = $this->getDoctrine()->getEntityManager();
+    public function selectlinkAction()
+    {
+        $em = $this->getDoctrine()->getManager();
         $request = $this->getRequest();
         $locale = $request->getLocale();
         $user = $this->container->get('security.context')->getToken()->getUser();
-        $topnodes = $em->getRepository('KunstmaanAdminNodeBundle:Node')->getTopNodes($locale, $user, 'read', TRUE);
-        $nodeMenu = new NodeMenu($this->container, $locale, null, 'read', FALSE, TRUE);
+        $topnodes = $em->getRepository('KunstmaanAdminNodeBundle:Node')->getTopNodes($locale, $user, 'read', true);
+        $nodeMenu = new NodeMenu($this->container, $locale, null, 'read', false, true);
 
-        return array(
-                'topnodes'      => $topnodes,
-                'nodemenu' 	    => $nodeMenu,
-        );
+        return array('topnodes' => $topnodes, 'nodemenu' => $nodeMenu,);
     }
 }
