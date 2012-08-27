f507037b554064ff8da4634598a5b2e252e3f691
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-08-27 17:41:59 +0200

    composer.json modified for unit tests

---
+++ src/Kunstmaan/AdminBundle/Tests/Component/Security/Acl/Permission/MaskBuilderTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: wim
+ * Date: 27/08/12
+ * Time: 17:06
+ * To change this template use File | Settings | File Templates.
+ */
+class MaskBuilderTest
+{
+
+}
