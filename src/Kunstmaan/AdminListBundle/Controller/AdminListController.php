9efaf5c53fd2e19b3a1f720d9abbbc9aa4544f0b
Kris Pypen <kris.pypen@kunstmaan.be>
2012-09-10 13:52:42 +0200

    cleanup the delete action

--- src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
+++ src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
@@ -142,26 +142,18 @@ abstract class AdminListController extends Controller
      */
     protected function doDeleteAction(AbstractAdminListConfigurator $configurator, $entityid)
     {
-        $em = $this->getDoctrine()->getEntityManager();
+        $em = $this->getDoctrine()->getManager();
 
         $request = $this->getRequest();
         $helper = $em->getRepository($configurator->getRepositoryName())->findOneById($entityid);
         if ($helper == null) {
             throw new NotFoundHttpException("Entity not found.");
         }
-        $form = $this->createFormBuilder($helper)->add('id', "hidden")->getForm();
-
+        $indexUrl = $configurator->getIndexUrlFor();
         if ('POST' == $request->getMethod()) {
-            $form->bind($request);
-            if ($form->isValid()) {
-                $em->remove($helper);
-                $em->flush();
-                $indexUrl = $configurator->getIndexUrlFor();
-
-                return new RedirectResponse($this->generateUrl($indexUrl['path'], isset($indexUrl['params']) ? $indexUrl['params'] : array()));
-            }
+            $em->remove($helper);
+            $em->flush();
         }
-
-        return new Response($this->renderView($configurator->getDeleteTemplate(), array('form' => $form->createView(), 'entity' => $helper, 'adminlistconfigurator' => $configurator)));
+        return new RedirectResponse($this->generateUrl($indexUrl['path'], isset($indexUrl['params']) ? $indexUrl['params'] : array()));
     }
 }
