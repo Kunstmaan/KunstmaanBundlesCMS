ac478833772d3792f2e884a162beae551ab31cc8
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-09-17 14:20:37 +0200

    fix twig extension

--- src/Kunstmaan/PagePartBundle/Twig/Extension/PagePartAdminTwigExtension.php
+++ src/Kunstmaan/PagePartBundle/Twig/Extension/PagePartAdminTwigExtension.php
@@ -3,6 +3,7 @@
 namespace Kunstmaan\PagePartBundle\Twig\Extension;
 
 use Symfony\Component\Form\FormView;
+use Kunstmaan\PagePartBundle\PagePartAdmin\PagePartAdmin;
 
 class PagePartAdminTwigExtension extends \Twig_Extension
 {
@@ -34,30 +35,30 @@ class PagePartAdminTwigExtension extends \Twig_Extension
     }
 
     /**
-     * Renders the HTML for a given view
+     * Renders the HTML for a given pagepart
      *
      * Example usage in Twig:
      *
-     *     {{ form_widget(view) }}
+     *     {{ pagepartadmin_widget(ppAdmin) }}
      *
      * You can pass options during the call:
      *
-     *     {{ form_widget(view, {'attr': {'class': 'foo'}}) }}
+     *     {{ pagepartadmin_widget(ppAdmin, {'attr': {'class': 'foo'}}) }}
      *
-     *     {{ form_widget(view, {'separator': '+++++'}) }}
+     *     {{ pagepartadmin_widget(ppAdmin, {'separator': '+++++'}) }}
      *
-     * @param \Symfony\Component\Form\FormView $view       The view to render
-     * @param null                             $form
-     * @param array                            $parameters Additional variables passed to the template
+     * @param PagePartAdmin $ppAdmin    The pagepart admin to render
+     * @param null          $form
+     * @param array         $parameters Additional variables passed to the template
      *
      * @return string The html markup
      */
-    public function renderWidget(FormView $view , $form = null , array $parameters = array())
+    public function renderWidget(PagePartAdmin $ppAdmin , $form = null , array $parameters = array())
     {
         $template = $this->environment->loadTemplate("KunstmaanPagePartBundle:PagePartAdminTwigExtension:widget.html.twig");
 
         return $template->render(array_merge($parameters, array(
-            'pagepartadmin' => $view,
+            'pagepartadmin' => $ppAdmin,
             'form' => $form
         )));
     }
