37b9305197f0a4eeeefaec6045a3a631f2786630
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2012-09-28 12:04:27 +0200

    Fix to allow previewing of homepages (pages with empty slug)

--- src/Kunstmaan/NodeBundle/Controller/SlugController.php
+++ src/Kunstmaan/NodeBundle/Controller/SlugController.php
@@ -28,6 +28,8 @@ class SlugController extends Controller
      * @param bool   $draft   Show the draft version or not
      *
      * @Route("/")
+     * @Route("/draft", defaults={"preview" = true, "draft" = true})
+     * @Route("/preview", defaults={"preview" = true})
      * @Route("/draft/{url}", requirements={"url" = ".+"}, defaults={"preview" = true, "draft" = true}, name="_slug_draft")
      * @Route("/preview/{url}", requirements={"url" = ".+"}, defaults={"preview" = true}, name="_slug_preview")
      * @Route("/{url}", requirements={"url" = ".+"}, name="_slug")
