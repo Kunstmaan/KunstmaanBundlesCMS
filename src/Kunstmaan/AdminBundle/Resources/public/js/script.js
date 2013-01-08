5969cf16588f73e5c33f96c720e7302143c3ded1
Sam Bellen <sam.bellen@kunstmaan.be>
2013-01-08 17:33:18 +0100

    updated to bootstrap 2.2.2, updated js libs

--- src/Kunstmaan/AdminBundle/Resources/public/js/script.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/script.js
@@ -1,13 +1,18 @@
-//Author: Indri & Ibe
+// Author: Indri & Ibe
 
-//Init functions needed on every page
+// Init functions needed on every page
 $(document).ready(function () {
+	init_tree();
 	init_main_functions();
 	initTop();
+	initCancel();
 	initCustomSelect();
 	initDel();
 	initFilter();
-    initDatePicker();
+	initTimePicker();
+	initDatePicker();
+	initDropdownButton();
+	// initStickyColumns();
 });
 
 //JS-tree
@@ -251,8 +256,8 @@ function init_tree() {
 					}
 				}
 			}
-		
-		
+
+
 		},
 		"crrm" : { 
 			"move" : {
@@ -284,29 +289,30 @@ function init_tree() {
     })
 }
 
-//Drag and Drop
+
+// Drag and Drop
 function init_DragDrop() {
 	$('#parts').sortable({  
-	    handle: '.prop_bar',  
-	    cursor: 'move',  
-	    placeholder: 'placeholder',  
-	    forcePlaceholderSize: true, 
+		handle: '.prop_bar',  
+		cursor: 'move',  
+		placeholder: 'placeholder',  
+		forcePlaceholderSize: true, 
 		revert: 100,
 	    //opacity: 0.4
 	    opacity: 1,
-		start: function(e, ui) {
-			$('.draggable').css('opacity', ".4");
-			$('.ui-sortable-helper .new_pagepart').slideUp("fast");
-		},
-		stop: function(e, ui) {
-			$('.draggable').css('opacity', "1");
-		}
+	    start: function(e, ui) {
+	    	$('.draggable').css('opacity', ".4");
+	    	$('.ui-sortable-helper .new_pagepart').slideUp("fast");
+	    },
+	    stop: function(e, ui) {
+	    	$('.draggable').css('opacity', "1");
+	    }
 	});
 }
 
- 
 
-//Drop down main_actions
+
+// Drop down main_actions
 function init_main_functions() {
 	$(window).scroll(
 		function() {
@@ -319,10 +325,11 @@ function init_main_functions() {
 				$('#main_actions_top').removeClass('slideDown');
 			}		
 		}
-	);
+		);
 }
 
-//Toplink
+
+// Toplink
 function initTop() {
 	$(".up").click(function(e) {
 		e.preventDefault();
@@ -330,22 +337,47 @@ function initTop() {
 	});
 }
 
-//Thumbnails Helper
+// Media-Grid Helper
 function initDel() {
-	$('.thumbnails .del').live("mouseover mouseout", function(e) {
+	$('.media-grid .del').live("mouseover mouseout", function(e) {
 		if (e.type == "mouseover") {
-			$(this).parent().find('.thumbnail').addClass('warning');
+			$(this).parent().find('.item').addClass('warning');
 			$(this).parent().find('.helper').html('Delete').addClass('warning');
 		} else {
-			$(this).parent().find('.thumbnail').removeClass('warning');
+			$(this).parent().find('.item').removeClass('warning');
 			$(this).parent().find('.helper').html('Click to edit').removeClass('warning');
 		}
 	});
 }
 
-//Datepicker
+function initCancel() {
+	$('.close_modal').on('click', function (e) {
+		e.preventDefault();
+		$(this).parents('.modal').modal('hide');
+	});
+}
+
+// Datepicker
 function init_datePicker() {
 	$('.date-pick').datePicker();
+}
+
+// Toggle Properties
+function init_toggleProp() {
+	$("#toggle-properties").click(function(e){
+		e.preventDefault();
+		if ($(this).hasClass('big')) {
+			$("#prop_wrp").slideUp("slow").animate({opacity: 0},{queue: false, duration: "slow"}).addClass('small_prop').removeClass('big_prop');
+			$(this).removeClass('big').addClass('small').html('Show Properties');
+			
+			
+					
+		}
+		else {
+			$("#prop_wrp").slideDown("slow").animate({opacity: 1},{queue: false, duration: "slow"}).addClass('small_prop').removeClass('big_prop');
+			$(this).removeClass('small').addClass('big').html('Hide Properties');
+		}
+	});
 }
 
 //Twipsy
@@ -353,14 +385,12 @@ function init_twipsy() {
 	$("a[rel=twipsy]").twipsy({live:true});
 };
 
-//Custom Select
+// Custom Select
 function initCustomSelect() {
-	$('select.chzn-select').chosen();
+	$('.chzn-select').chosen();
 }
 
-
-
-////FILTERS
+//Filter
 function initFilter() {
 	var checked = $("#filter_on_off").attr("checked");
 	
@@ -387,111 +417,99 @@ function initFilter() {
 			} else {
 				$(".all").addClass("active");
 				$(".filters_wrp").removeClass("active");
-				resetFilters();
 			}
-		
 		}
+	});
+}
+
+function initDatePicker() {
+	// http://www.eyecon.ro/bootstrap-datepicker/
+	if($('.form_datepicker').length > 0) {
+		$(".form_datepicker").datepicker({
+			'format': 'dd/mm/yyyy'
+		});
+	}
+}
+
+
+function initTimePicker() {
+	// http://jdewit.github.com/bootstrap-timepicker/
+	if($('.form_timepicker').length > 0) {
+		$(".form_timepicker").timepicker({
+			'showMeridian': false,
+			'minuteStep': 1
+		});
+	}
+}
+
+function initDropdownButton() {
+	var $el = $('.main_actions .btn.dropdown-toggle');
+
+	if($el.is(':nth-child(2)')) {
+		$el.css({
+			'-webkit-border-top-right-radius': 0,
+			'-webkit-border-bottom-right-radius': 0,
+			'-moz-border-radius-topright': 0,
+			'-moz-border-radius-bottomright': 0,
+			'border-top-right-radius': 0,
+			'border-bottom-right-radius': 0
+		})
+	}
+
+	$el.on('click', function() {
+		var offset = $el.offset().left - $('.main_actions').offset().left - $('.main_actions .dropdown-menu').outerWidth() + $(this).outerWidth();
+		$el.next('.dropdown-menu').css('left', offset);
 	});
 }
 
 
 
 
-
-
-function createFilter(el, hide, options){
-    console.log("Create filter init");
-    
-    var line = $(el).parent("li");
-    
-    if(hide == true){
-        line.addClass("hidden");
-    }
-  
-    var uniqueid = calculateUniqueFilterId();
-    var newitem = $('<li>').attr('class', 'filterline').append($("#filterdummyline").html());
-    
-    if(hide == true){
-    	line.after(newitem);
-    } else {
-    	line.before(newitem);
-    }
-    
-    
-    
-    newitem.find(".uniquefilterid").val(uniqueid);
-    newitem.find(".filterdummy").val(line.find(".filterselect").val());
-    updateOptions(newitem.find(".filterdummy"), options);
-    
-    if(hide == true){
-        newitem.removeClass("hidden");
-        line.find("select").val("");
-    } else {
-        newitem.slideDown();
-    }
-
-    return false;
-}
-
-
-
-function resetFilters(){
-    $(".filterline").remove();
-    $(".apply_filter").click();
-    return false;
-}
-
-
-
-function removeFilter(el){
-    if($("#filtermap").find(".filterline").length==2){
-        $(el).parent(".filterline").remove();
-        $("#addline").removeClass("hidden");
-    } else {
-        $(el).parent(".filterline").slideUp(function(){
-            $(this).remove();
-        });
-    }
-    return false;
-}
-
-
-
-function calculateUniqueFilterId(){
-    var result = 1;
-    $("input.uniquefilterid").each(function(){
-        var value = parseInt( jQuery(this).val() );
-        if(result <= value){
-            result = value+1;
+// UI SORTABLE
+$(function() {
+    var sortableClicked = false;
+    $('.sortable').mousedown(
+        function() {
+            sortableClicked = true;
+            var scope = $(this).data('scope');
+            $('.sortable[data-scope~=' + scope + ']')
+                .addClass('connectedSortable')
+                .sortable('option', 'connectWith', '.connectedSortable');
+            $('.sortable:not([data-scope~=' + scope + '])')
+                .sortable('disable')
+                .sortable('option', 'connectWith', false)
+                .parent().addClass('region-disabled');
+            $('.template-block-content').not('.sortable')
+                .parent().addClass('region-disabled');
         }
-    });
-    return result;
-}
-
-
-
-function updateOptions(el, options){
-    var val = $(el).val();
-    val = val.replace('.',  '_');
-    var uniqueid = $(el).parent(".filterline").find(".uniquefilterid").val();
-    $(el).parent(".filterline").find(".filteroptions").html($('#filterdummyoptions_'+ val).html());
-    $(el).parent(".filterline").find("input, select").each(function(){
-    	var fieldName = $(this).attr("name");
-    	if (fieldName.substr(0, 7) != "filter_") {
-    		$(this).attr("name", "filter_" + $(this).attr("name"));
-    	}
-    });    
-    $(el).parent(".filterline").find(".filteroptions").find("input:not(.uniquefilterid), select").each(function(){
-        $(this).attr("name", $(this).attr("name") + "_" + uniqueid);
-		if($(this).hasClass("datepick")){
-			$(this).datePicker(options);				
-		}
-    });
-}
-
-function initDatePicker() {
-    // http://www.eyecon.ro/bootstrap-datepicker/
-    if($('.form_datepicker').length > 0) {
-        $(".form_datepicker").datepicker();
-    }
-}
+    );
+    $('body').mouseup(
+        function() {
+            if (sortableClicked) {
+                // Enable all sortable regions again
+                $('.sortable')
+                    .sortable('enable')
+                    .sortable('option', 'connectWith', false)
+                    .parent().removeClass('region-disabled');
+                $('.template-block-content').not('.sortable')
+                    .parent().removeClass('region-disabled');
+                sortableClicked = false;
+            }
+        }
+    );
+    $( ".sortable" ).sortable({
+        connectWith: ".connectedSortable",
+        placeholder: "sortable-placeholder",
+        tolerance:"pointer",
+        revert: 300
+/*
+        stop: function(e, ui) {
+            // Enable all
+            $('.sortable')
+                .sortable('enable')
+                .sortable('option', 'connectWith', false)
+                .parent().removeClass('region-disabled');
+        }
+*/
+    }).disableSelection();
+});
