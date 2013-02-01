680c6fcbf8f38e3296baedbeb69cefbfc44816b7
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-02-01 16:24:39 +0100

    basic unit tests for email field

---
+++ src/Kunstmaan/FormBundle/Tests/Entity/FormSubmissionFieldTypes/EmailFormSubmissionFieldTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Kunstmaan\FormBundle\Tests\Entity\FormSubmissionFieldTypes;
+
+use Kunstmaan\FormBundle\Entity\FormSubmissionFieldTypes\EmailFormSubmissionField;
+use Kunstmaan\FormBundle\Form\EmailFormSubmissionType;
+
+/**
+ * Tests for EmailFormSubmissionField
+ */
+class EmailFormSubmissionFieldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EmailFormSubmissionField
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new EmailFormSubmissionField;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\FormSubmissionFieldTypes\EmailFormSubmissionField::getValue
+     * @covers Kunstmaan\FormBundle\Entity\FormSubmissionFieldTypes\EmailFormSubmissionField::setValue
+     */
+    public function testSetGetValue()
+    {
+        $object = $this->object;
+        $value = 'test@test.be';
+        $object->setValue($value);
+        $this->assertEquals($value, $object->getValue());
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\FormSubmissionFieldTypes\EmailFormSubmissionField::getDefaultAdminType
+     */
+    public function testGetDefaultAdminType()
+    {
+        $adminType = $this->object->getDefaultAdminType();
+        $this->assertNotNull($adminType);
+        $this->assertTrue($adminType instanceof EmailFormSubmissionType);
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\FormSubmissionFieldTypes\EmailFormSubmissionField::__toString
+     */
+    public function test__toString()
+    {
+        $stringValue = $this->object->__toString();
+        $this->assertNotNull($stringValue);
+        $this->assertTrue(is_string($stringValue));
+    }
+}
---
+++ src/Kunstmaan/FormBundle/Tests/Entity/PageParts/EmailPagePartTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Kunstmaan\FormBundle\Tests\Entity\PageParts;
+
+use ArrayObject;
+
+use Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart;
+use Kunstmaan\FormBundle\Form\EmailPagePartAdminType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Tests for EmailPagePart
+ */
+class EmailPagePartTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EmailPagePart
+     */
+    protected $object;
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->object = new EmailPagePart;
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::setErrorMessageRequired
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::getErrorMessageRequired
+     */
+    public function testSetErrorMessageRequired()
+    {
+        $object = $this->object;
+        $object->setErrorMessageRequired('');
+        $this->assertEquals('This value should not be blank.', $object->getErrorMessageRequired());
+
+        $message = "Some example error message";
+        $object->setErrorMessageRequired($message);
+        $this->assertEquals($message, $object->getErrorMessageRequired());
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::setErrorMessageInvalid
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::getErrorMessageInvalid
+     */
+    public function testSetErrorMessageInvalid()
+    {
+        $object = $this->object;
+        $object->setErrorMessageInvalid('');
+        $this->assertEquals('This value is not a valid email address.', $object->getErrorMessageInvalid());
+
+        $message = "Some example error message";
+        $object->setErrorMessageInvalid($message);
+        $this->assertEquals($message, $object->getErrorMessageInvalid());
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::getDefaultView
+     */
+
+    public function testGetDefaultView()
+    {
+        $stringValue = $this->object->getDefaultView();
+        $this->assertNotNull($stringValue);
+        $this->assertTrue(is_string($stringValue));
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::adaptForm
+     */
+    public function testAdaptForm()
+    {
+        $object = $this->object;
+        $object->setRequired(true);
+
+        $formBuilder = $this->getMockBuilder('Symfony\Component\Form\FormBuilder')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $formBuilder->expects($this->any())
+            ->method('getData')
+            ->will($this->returnValue(array()));
+
+        $fields = new ArrayObject();
+
+        $this->assertTrue(sizeof($fields) == 0);
+        /* @var $formBuilder FormBuilderInterface */
+        $object->adaptForm($formBuilder, $fields);
+        $this->assertTrue(sizeof($fields) > 0);
+    }
+
+    /**
+     * @covers Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart::getDefaultAdminType
+     */
+    public function testGetDefaultAdminType()
+    {
+        $adminType = $this->object->getDefaultAdminType();
+        $this->assertNotNull($adminType);
+        $this->assertTrue($adminType instanceof EmailPagePartAdminType);
+    }
+}
