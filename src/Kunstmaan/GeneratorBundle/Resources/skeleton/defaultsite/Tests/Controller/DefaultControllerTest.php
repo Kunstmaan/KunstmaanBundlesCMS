3f69b8e32d69f4390591dc4753fc8ab1fc79de3e
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-03-01 14:36:18 +0100

    move unit test to default site generation

--- src/Kunstmaan/GeneratorBundle/Resources/skeleton/defaultsite/Tests/Controller/DefaultControllerTest.php
+++ src/Kunstmaan/GeneratorBundle/Resources/skeleton/defaultsite/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Setup complete!")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Welcome")')->count() > 0);
     }
 }
