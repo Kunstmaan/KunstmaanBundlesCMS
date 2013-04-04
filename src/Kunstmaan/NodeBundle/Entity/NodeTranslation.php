d875f101f2820710ce51df89f7b070cd927a1f1a
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-04 09:47:43 +0200

    quickfix to fetch correct node version

--- src/Kunstmaan/NodeBundle/Entity/NodeTranslation.php
+++ src/Kunstmaan/NodeBundle/Entity/NodeTranslation.php
@@ -303,8 +303,12 @@ class NodeTranslation extends AbstractEntity
     public function getNodeVersion($type)
     {
         $nodeVersions = $this->getNodeVersions();
-        /* @var NodeVersion $nodeVersion */
-        foreach ($nodeVersions as $nodeVersion) {
+
+        $max = count($nodeVersions);
+        for ($i = $max-1; $i >= 0; $i--) {
+            /* @var NodeVersion $nodeVersion */
+            $nodeVersion = $nodeVersions[$i];
+
             if ($type == $nodeVersion->getType()) {
                 return $nodeVersion;
             }
