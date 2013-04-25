89c66ea3d6775e2e150237a637a828791ebee74c
Kris Pypen <kris.pypen@kunstmaan.be>
2013-04-25 12:58:23 +0300

    Update script.js

--- src/Kunstmaan/AdminBundle/Resources/public/js/script.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/script.js
@@ -668,14 +668,6 @@ function initSaveKeyListener() {
 					return false;
 				}
 			}
-			// checken voor enter key
-			var evt = (evt) ? evt : ((event) ? event : null);
-			var node = (evt.target) ? evt.target
-					: ((evt.srcElement) ? evt.srcElement : null);
-			if ((evt.keyCode == 13) && (node.type == "text")) {
-				performSaveAction();
-				return false;
-			}
 		};
 	}
 }
