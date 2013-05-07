f711e0079654e4e4a03a721985a94fc502a260de
Vincent De Snerck <vincent@supervillain.be>
2013-05-07 14:40:30 +0200

    Add a default index action to the DefaultController in the Bundle generator.
    Seems a little strange not to do this when there is a 'Setup complete!' template included.

--- src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/bundle/DefaultController.php
+++ src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/bundle/DefaultController.php
@@ -4,7 +4,17 @@ namespace {{ namespace }}\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 class DefaultController extends Controller
 {
-
+    /**
+     * @Route("/", name="default_index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
 }
