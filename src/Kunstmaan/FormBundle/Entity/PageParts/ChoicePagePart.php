f6f1a03b224e944cbd18c08cc9f0f05fc4888c80
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-06-03 10:02:36 +0200

    fix choice field : required

--- src/Kunstmaan/FormBundle/Entity/PageParts/ChoicePagePart.php
+++ src/Kunstmaan/FormBundle/Entity/PageParts/ChoicePagePart.php
@@ -100,7 +100,7 @@ class ChoicePagePart extends AbstractFormPagePart
         $data = $formBuilder->getData();
         $data['formwidget_' . $this->getUniqueId()] = $cfsf;
         $label = $this->getLabel();
-        $formBuilder->add('formwidget_' . $this->getUniqueId(), new ChoiceFormSubmissionType($label, $this->getExpanded(), $this->getMultiple(), $choices, $this->getEmptyValue()));
+        $formBuilder->add('formwidget_' . $this->getUniqueId(), new ChoiceFormSubmissionType($label, $this->required, $this->getExpanded(), $this->getMultiple(), $choices, $this->getEmptyValue()));
         $formBuilder->setData($data);
         if ($this->getRequired()) {
             $thiss = $this;
