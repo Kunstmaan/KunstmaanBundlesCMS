0a45063feb973ea9fcb6b531d846faa9b24ae14f
Daan Poron <daan@poron.be>
2013-06-04 10:25:22 +0200

    Add permission creator service

---
+++ src/Kunstmaan/AdminBundle/Helper/Creators/ACLPermissionCreator.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Kunstmaan\AdminBundle\Helper\Creators;
+
+use Kunstmaan\AdminBundle\Helper\Security\Acl\Permission;
+use Kunstmaan\AdminBundle\Helper\Security\Acl\Permission\MaskBuilder;
+
+use Symfony\Component\Security\Acl\Exception\AclNotFoundException;
+use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
+use Symfony\Component\Security\Acl\Model\EntryInterface;
+use Symfony\Component\Security\Acl\Model\MutableAclProviderInterface;
+use Symfony\Component\Security\Acl\Model\ObjectIdentityRetrievalStrategyInterface;
+
+/**
+ * Service to add the correct permissions to objects.
+ */
+class ACLPermissionCreator
+{
+    /**
+     * @var MutableAclProviderInterface
+     */
+    private $aclProvider;
+
+    /**
+     * @var ObjectIdentityRetrievalStrategyInterface
+     */
+    private $objectIdentityRetrievalStrategy;
+
+    /**
+     * @param MutableAclProviderInterface              $aclProvider
+     * @param ObjectIdentityRetrievalStrategyInterface $objectIdentityRetrievalStrategy
+     */
+    public function __construct(MutableAclProviderInterface $aclProvider, ObjectIdentityRetrievalStrategyInterface $objectIdentityRetrievalStrategy)
+    {
+        $this->aclProvider = $aclProvider;
+        $this->objectIdentityRetrievalStrategy = $objectIdentityRetrievalStrategy;
+    }
+
+    /**
+     * @param mixed $object
+     * @param array $aces
+     */
+    private function init($object, $aces)
+    {
+        $aclProvider = $this->aclProvider;
+        $strategy = $this->objectIdentityRetrievalStrategy;
+
+        $objectIdentity = $strategy->getObjectIdentity($object);
+        try {
+            $aclProvider->deleteAcl($objectIdentity);
+        } catch (AclNotFoundException $e) {
+            // Do nothing
+        }
+
+        $acl = $aclProvider->createAcl($objectIdentity);
+
+        foreach ($aces as $ace) {
+            $acl->insertObjectAce($ace['identity'], $ace['mask']);
+        }
+
+        $aclProvider->updateAcl($acl);
+    }
+
+    /**
+     * @param mixed $object
+     * @param mixed $example
+     */
+    public function initByExample($object, $example)
+    {
+        $aclProvider = $this->aclProvider;
+        $strategy = $this->objectIdentityRetrievalStrategy;
+
+        $exampleIdentity = $strategy->getObjectIdentity($example);
+        $exampleAcl = $aclProvider->findAcl($exampleIdentity);
+
+        $aces = array();
+        /* @var EntryInterface $ace */
+        foreach ($exampleAcl->getObjectAces() as $ace) {
+            $securityIdentity = $ace->getSecurityIdentity();
+            if ($securityIdentity instanceof RoleSecurityIdentity) {
+                $aces[] = array(
+                    'identity' => $securityIdentity,
+                    'mask' => $ace->getMask()
+                );
+            }
+        }
+        $this->init($object, $aces);
+    }
+
+    /**
+     * @param mixed $object
+     * @param array $map
+     *        with as key the name of the role you want to set the permissions for
+     *        and as value the mask you want to use
+     *        for example array('ROLE_GUEST' => MaskBuilder::MASK_EDIT | MaskBuilder::MASK_PUBLISH)
+     */
+    public function initByMap($object, $map)
+    {
+        $aces = array();
+        foreach ($map as $key => $value) {
+            $aces[] = array(
+                'identity' => new RoleSecurityIdentity($key),
+                'mask' => $value
+            );
+        }
+
+        $this->init($object, $aces);
+    }
+
+}
