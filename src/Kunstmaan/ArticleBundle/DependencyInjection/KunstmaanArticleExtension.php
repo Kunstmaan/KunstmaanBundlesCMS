2e05f53544b656478e1b9125861079acd9774c0f
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-06-10 15:48:09 +0200

    fix no services.yml anymore

--- src/Kunstmaan/ArticleBundle/DependencyInjection/KunstmaanArticleExtension.php
+++ src/Kunstmaan/ArticleBundle/DependencyInjection/KunstmaanArticleExtension.php
@@ -23,6 +23,5 @@ class KunstmaanArticleExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
     }
 }
