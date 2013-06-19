09072743c6070e854292d27b9c3191844230a2fc
Joeri Verdeyen <joeri.verdeyen@kunstmaan.be>
2013-06-19 11:12:45 +0200

    Removing the need to import config.yml into you app/config/config.yml

--- src/Kunstmaan/NodeBundle/DependencyInjection/KunstmaanNodeExtension.php
+++ src/Kunstmaan/NodeBundle/DependencyInjection/KunstmaanNodeExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class KunstmaanNodeExtension extends Extension
+class KunstmaanNodeExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -30,5 +31,20 @@ class KunstmaanNodeExtension extends Extension
         ));
 
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        // set twig global params
+        $twigConfig['globals']['nodebundleisactive'] = true;
+        $container->prependExtensionConfig('twig', $twigConfig);
+
+        $cmfRoutingExtraConfig['chain']['routers_by_id']['router.default'] = 100;
+        $cmfRoutingExtraConfig['chain']['replace_symfony_router'] = true;
+        $container->prependExtensionConfig('symfony_cmf_routing_extra', $cmfRoutingExtraConfig);
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
     }
 }
