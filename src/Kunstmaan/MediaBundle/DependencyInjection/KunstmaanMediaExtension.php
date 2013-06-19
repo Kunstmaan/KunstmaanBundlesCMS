245c9e7c7ddea270f40d3c56aaf16e0c6803582b
Joeri Verdeyen <joeri.verdeyen@kunstmaan.be>
2013-06-19 14:57:17 +0200

    Removing the need to import config.yml into you app/config/config.yml

--- src/Kunstmaan/MediaBundle/DependencyInjection/KunstmaanMediaExtension.php
+++ src/Kunstmaan/MediaBundle/DependencyInjection/KunstmaanMediaExtension.php
@@ -7,13 +7,15 @@ use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class KunstmaanMediaExtension extends Extension
+class KunstmaanMediaExtension extends Extension implements PrependExtensionInterface
 {
 
     /**
@@ -29,6 +31,10 @@ class KunstmaanMediaExtension extends Extension
 
         $config = $processor->processConfiguration($configuration, $configs);
 
+        if(!$container->hasParameter('kunstmaan_media.upload_dir')) {
+            $container->setParameter('kunstmaan_media.upload_dir', '/uploads/media/');
+        }
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
         $container->setParameter('twig.form.resources', array_merge(
@@ -38,6 +44,20 @@ class KunstmaanMediaExtension extends Extension
 
         $loader->load('services.yml');
         $loader->load('handlers.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+        $twigConfig['globals']['upload_dir'] = $container->getParameter('kunstmaan_media.upload_dir');
+        $twigConfig['globals']['mediabundleisactive'] = true;
+        $twigConfig['globals']['mediamanager'] = "@kunstmaan_media.media_manager";
+        $container->prependExtensionConfig('twig', $twigConfig);
+
+        $liipConfig = Yaml::parse(file_get_contents(__DIR__ . '/../Resources/config/liip_imagine.yml'));
+        $container->prependExtensionConfig('liip_imagine', $liipConfig['liip_imagine']);
+
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
     }
 
     /**
