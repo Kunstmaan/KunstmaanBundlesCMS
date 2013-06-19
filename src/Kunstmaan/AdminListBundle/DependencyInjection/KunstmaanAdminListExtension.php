5fea2281715d0ce475f3e351d5c20ca3629b13c6
Joeri Verdeyen <joeri.verdeyen@kunstmaan.be>
2013-06-19 10:32:55 +0200

    Removing the need to import config.yml into you app/config/config.yml

--- src/Kunstmaan/AdminListBundle/DependencyInjection/KunstmaanAdminListExtension.php
+++ src/Kunstmaan/AdminListBundle/DependencyInjection/KunstmaanAdminListExtension.php
@@ -6,13 +6,14 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * This is the class that loads and manages your bundle configuration
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class KunstmaanAdminListExtension extends Extension
+class KunstmaanAdminListExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritDoc}
@@ -24,5 +25,23 @@ class KunstmaanAdminListExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend(ContainerBuilder $container)
+    {
+
+        $parameterName = 'datePicker_startDate';
+
+        $config = array();
+        $config['globals'][$parameterName] = '01/01/1970';
+
+        if($container->hasParameter($parameterName)) {
+            $config['globals'][$parameterName] = $container->getParameter($parameterName);
+        }
+
+        $container->prependExtensionConfig('twig', $config);
+        $configs = $container->getExtensionConfig($this->getAlias());
+        $config = $this->processConfiguration(new Configuration(), $configs);
+
     }
 }
