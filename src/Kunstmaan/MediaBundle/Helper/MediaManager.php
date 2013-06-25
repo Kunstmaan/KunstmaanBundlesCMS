c247509c963fd58478604bdfe9b7b8e46d89aa5b
Vincent De Snerck <vincent@supervillain.be>
2013-06-25 17:47:20 +0200

    Use correct FileHandler class in MediaManager

--- src/Kunstmaan/MediaBundle/Helper/MediaManager.php
+++ src/Kunstmaan/MediaBundle/Helper/MediaManager.php
@@ -2,6 +2,7 @@
 
 namespace Kunstmaan\MediaBundle\Helper;
 
+use Kunstmaan\MediaBundle\Helper\File\FileHandler;
 use Kunstmaan\MediaBundle\Helper\Media\AbstractMediaHandler;
 
 use Kunstmaan\MediaBundle\Entity\Media;
