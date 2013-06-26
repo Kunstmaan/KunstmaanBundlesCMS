ae876bb8f0dbfdd2a9354df3f0beb52a3594ae22
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-06-26 13:10:36 +0200

    fix imports

--- src/Kunstmaan/MediaBundle/Helper/MediaManager.php
+++ src/Kunstmaan/MediaBundle/Helper/MediaManager.php
@@ -3,10 +3,8 @@
 namespace Kunstmaan\MediaBundle\Helper;
 
 use Kunstmaan\MediaBundle\Helper\File\FileHandler;
-use Kunstmaan\MediaBundle\Helper\Media\AbstractMediaHandler;
-
 use Kunstmaan\MediaBundle\Entity\Media;
-use Kunstmaan\MediaBundle\Helper\File\FileHandler;
+use Kunstmaan\MediaBundle\Helper\Media\AbstractMediaHandler;
 
 /**
  * MediaManager
