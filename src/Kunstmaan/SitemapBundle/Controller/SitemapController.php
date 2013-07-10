c9a2de8be01cd8c9bc1ccc040264b35f897624d6
Kenny Debrauwer <kenny.debrauwer@kunstmaan.be>
2013-07-10 12:47:58 +0200

    don't load offline pages

--- src/Kunstmaan/SitemapBundle/Controller/SitemapController.php
+++ src/Kunstmaan/SitemapBundle/Controller/SitemapController.php
@@ -16,8 +16,6 @@ class SitemapController extends Controller
      * @Route("/sitemap.{_format}", name="KunstmaanSitemapBundle_sitemap", requirements={"_format" = "xml"})
      * @Template("KunstmaanSitemapBundle:Sitemap:view.xml.twig")
      *
-     * @param $mode
-     *
      * @return array
      */
     public function sitemapAction()
@@ -26,7 +24,7 @@ class SitemapController extends Controller
         $locale = $this->getRequest()->getLocale();
         $securityContext = $this->container->get('security.context');
         $aclHelper = $this->container->get('kunstmaan_admin.acl.helper');
-        $nodeMenu = new NodeMenu($em, $securityContext, $aclHelper, $locale, null, PermissionMap::PERMISSION_VIEW, true, true);
+        $nodeMenu = new NodeMenu($em, $securityContext, $aclHelper, $locale, null, PermissionMap::PERMISSION_VIEW, false, true);
 
         return array(
             'nodemenu' => $nodeMenu,
