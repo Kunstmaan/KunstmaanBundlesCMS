2bc828db80f908f6b303d9b7b0492810adef1bfa
Joeri Verdeyen <joeri.verdeyen@kunstmaan.be>
2013-07-11 15:17:53 +0200

    Changes in controller

--- src/Kunstmaan/TranslatorBundle/Controller/TranslatorController.php
+++ src/Kunstmaan/TranslatorBundle/Controller/TranslatorController.php
@@ -8,17 +8,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 use Kunstmaan\TranslatorBundle\Model\Import\ImportCommand;
+use Kunstmaan\TranslatorBundle\Exception\InvalidDomainException;
 
 class TranslatorController extends Controller
 {
     /**
      * @Route("/show/{domain}", requirements={"domain"}, name="KunstmaanTranslatorBundle_translations_show")
-     * @Template()
+     * @Template("KunstmaanTranslatorBundle:Translator:index.html.twig")
      *
      * @return array
      */
-    public function indexAction($domain = false)
+    public function showAction($domain = false)
     {
+        $this->validateDomain($domain);
+
         $translationGroups = $this->container->get('kunstmaan_translator.service.manager')->getTranslationGroupsByDomain($domain);
         $managedLocales = $this->container->getParameter('kuma_translator.managed_locales');
         $domains = $this->container->get('kunstmaan_translator.service.manager')->getAllDomains();
@@ -36,18 +39,49 @@ class TranslatorController extends Controller
                 );
     }
 
+
     /**
-     * @Route("/", name="KunstmaanTranslatorBundle_translations_save")
+     * @Route("/", requirements={"domain"}, name="KunstmaanTranslatorBundle_translations_index")
+     * @Template()
+     *
+     * @return array
+     */
+    public function indexAction()
+    {
+        $domains = $this->container->get('kunstmaan_translator.service.manager')->getAllDomains();
+        $cacheFresh = $this->container->get('kunstmaan_translator.service.translator.cache_validator')->isCacheFresh();
+
+        if ($this->container->getParameter('kernel.debug') === false && $cacheFresh === false) {
+            $this->get('session')->getFlashBag()->add('warning', 'Rebuild cache to update to latest translations.');
+        }
+
+        return array(
+                'translationGroups' => null,
+                'managedLocales' => null,
+                'domain' => null,
+                'domains' => $domains
+                );
+    }
+
+    /**
+     * @Route("/save", name="KunstmaanTranslatorBundle_translations_save")
      * @Template()
      *
      */
     public function saveAction()
     {
         $post = $this->getRequest()->request->all();
-        $this->container->get('kunstmaan_translator.service.manager')->updateTranslationsFromArray($post['domain'], $post['translation']);
-        $this->get('session')->getFlashBag()->add('success', 'Translations succesful saved!');
 
-        return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $post['domain'])));
+        try {
+
+            $this->container->get('kunstmaan_translator.service.manager')->updateTranslationsFromArray($post['domain'], $post['translation']);
+            $this->get('session')->getFlashBag()->add('success', 'Translations succesful saved!');
+
+            return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $post['domain'])));
+
+        } catch (\Exception $e) {
+            throw $e;
+        }
     }
 
     /**
@@ -60,17 +94,27 @@ class TranslatorController extends Controller
     {
         $domain = $this->container->get('kunstmaan_translator.service.manager')->getFirstDefaultDomainName();
 
-        $importCommand = new ImportCommand();
-        $importCommand
-            ->setForce(false)
-            ->setLocales(false)
-            ->setGlobals(false)
-            ->setBundle($this->container->getParameter('kuma_translator.default_bundle'));
+        try {
+            $importCommand = new ImportCommand();
+            $importCommand
+                ->setForce(false)
+                ->setLocales(false)
+                ->setGlobals(false)
+                ->setBundle($this->container->getParameter('kuma_translator.default_bundle'));
 
-        $nbOfImports = $this->container->get('kunstmaan_translator.service.importer.command_handler')->executeImportCommand($importCommand);
-        $this->get('session')->getFlashBag()->add('success', sprintf('%s translations imported', $nbOfImports));
+            $nbOfImports = $this->container->get('kunstmaan_translator.service.importer.command_handler')->executeImportCommand($importCommand);
 
-        return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $domain)));
+            if($nbOfImports <= 0) {
+                $this->get('session')->getFlashBag()->add('warning', sprintf('No translations imported, because no new translation were found.', $nbOfImports));
+            } else {
+                $this->get('session')->getFlashBag()->add('success', sprintf('%s translations imported', $nbOfImports));
+            }
+
+            return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $domain)));
+        } catch (\Exception $e) {
+            throw $e; // FIXME: do somehting useful
+        }
+
     }
 
     /**
@@ -87,5 +131,19 @@ class TranslatorController extends Controller
         $this->get('session')->getFlashBag()->add('success', 'Translation cache flushed');
 
         return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $domain)));
+    }
+
+    private function validateDomain($domainName)
+    {
+
+        if (trim($domainName) == '') {
+            return $this->redirect($this->generateUrl('KunstmaanTranslatorBundle_translations_show', array('domain' => $domain)));
+        }
+
+        $domain = $this->container->get('kunstmaan_translator.repository.translation_domain')->findOneByName($domainName);
+
+        if (!$domain instanceOf \Kunstmaan\TranslatorBundle\Model\Translation\TranslationDomain ) {
+            throw new InvalidDomainException(sprintf('"%s" isn\'t a valid domain name.', $domainName));
+        }
     }
 }
---
+++ src/Kunstmaan/TranslatorBundle/Exception/InvalidDomainException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kunstmaan\TranslatorBundle\Exception;
+
+class InvalidDomainException extends \RuntimeException
+{
+
+}
