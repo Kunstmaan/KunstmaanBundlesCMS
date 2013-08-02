055fd671f7a8162c2b6e7c4e236b8436b01d759d
Vincent De Snerck <vincent@supervillain.be>
2013-08-02 13:42:08 +0200

    Refactor PagePartCreatorService + doc + conform to CodingStandards

--- src/Kunstmaan/PagePartBundle/Helper/Services/PagePartCreatorService.php
+++ src/Kunstmaan/PagePartBundle/Helper/Services/PagePartCreatorService.php
@@ -3,21 +3,24 @@
 namespace Kunstmaan\PagePartBundle\Helper\Services;
 
 use Doctrine\ORM\EntityManager;
-use Kunstmaan\NodeBundle\Entity\Node,
-    Kunstmaan\NodeBundle\Repository\NodeRepository,
-    Kunstmaan\NodeBundle\Repository\NodeTranslationRepository;
+
+use Kunstmaan\NodeBundle\Repository\NodeRepository,
+    Kunstmaan\NodeBundle\Repository\NodeTranslationRepository,
+    Kunstmaan\NodeBundle\Entity\NodeTranslation;
+
 use Kunstmaan\PagePartBundle\Helper\PagePartInterface,
     Kunstmaan\PagePartBundle\Repository\PagePartRefRepository;
 
 /**
- * A class to facilitate the adding of pageparts to existing pages.
+ * A class to facilitate the adding of PageParts to existing pages.
  *
  * NOTE: There is a similar implementation for adding pages. See the NodeBundle for more on this.
  *
  * Class PagePartCreatorService
  * @package Kunstmaan\PagePartBundle\Helper\Services
  */
-class PagePartCreatorService {
+class PagePartCreatorService
+{
 
     /** @var EntityManager */
     protected $em;
@@ -28,18 +31,27 @@ class PagePartCreatorService {
     /** @var NodeRepository */
     protected $nodeRepo;
 
-    public function setEntityManager(EntityManager $em) {
+    /**
+     * Sets the EntityManager dependency.
+     *
+     * @param EntityManager $em
+     */
+    public function setEntityManager(EntityManager $em)
+    {
         $this->em = $em;
+        // Because these repositories are shared between the different functions it's
+        // easier to make them available in the class.
         $this->pagePartRepo = $em->getRepository('KunstmaanPagePartBundle:PagePartRef');
         $this->translationRepo = $em->getRepository('KunstmaanNodeBundle:NodeTranslation');
         $this->nodeRepo = $em->getRepository('KunstmaanNodeBundle:Node');
     }
-    public function getEntityManager() {
-        return $this->em;
-    }
 
-    public function __construct(EntityManager $em) {
-        $this->setEntityManager($em);
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->em;
     }
 
 
@@ -47,28 +59,34 @@ class PagePartCreatorService {
     /**
      * Add a single pagepart to an existing page for a specific language, in an optional position.
      *
-     * @param Node $node A Node instance or the internal name.
-     *                   When the internal name is passed we'll get the node instance.
-     *                   Based on the language we'll locate the correct Page instance.
-     * @param PagePartInterface $pagePart A completely configured pagepart for this language.
-     * @param $language The languagecode. nl|fr|en|.. . Just one.
-     * @param string $context Where you want the pagepart to be.
-     * @paral integer $position Leave null if you want to append at the end.
-     *                          Otherwise set a position you would like and it'll inject
-     *                          the pagepart in that position.
-     *                          It won't override pageparts but it will rather inject itself in
-     *                          that position and push the other pageparts down.
+     * @param mixed(Node|string) $nodeOrInternalName
+     *      A Node instance or the internal name.
+     *      When the internal name is passed we'll get the node instance.
+     *      Based on the language we'll locate the correct Page instance.
+     * @param PagePartInterface $pagePart
+     *      A completely configured pagepart for this language.
+     * @param string $language
+     *      The languagecode. nl|fr|en|.. . Just one.
+     * @param string $context
+     *      Where you want the pagepart to be.
+     * @param mixed(integer\NULL) $position
+     *      Leave null if you want to append at the end.
+     *      Otherwise set a position you would like and it'll inject the pagepart in that position.
+     *      It won't override pageparts but it will rather inject itself in that position and
+     *      push the other pageparts down.
      */
-    public function addPagePartToPage($nodeOrInternalName, PagePartInterface $pagePart, $language, $context='main', $position=null) {
+    public function addPagePartToPage($nodeOrInternalName, PagePartInterface $pagePart, $language, $context='main', $position=null)
+    {
         // Find the correct page instance.
         $node = $this->getNode($nodeOrInternalName);
+        /** @var $translation NodeTranslation */
         $translation = $node->getNodeTranslation($language);
         $page = $translation->getRef($this->em);
 
         // Find latest position.
         if (is_null($position)) {
-            $pageparts = $this->pagePartRepo->getPagePartRefs($page, $context);
-            $position = count($pageparts) + 1;
+            $pageParts = $this->pagePartRepo->getPagePartRefs($page, $context);
+            $position = count($pageParts) + 1;
         }
 
         $this->em->persist($pagePart);
@@ -77,37 +95,42 @@ class PagePartCreatorService {
         $this->pagePartRepo->addPagePart($page, $pagePart, $position, $context);
     }
 
+
     /**
      * A helper function to more easily append multiple pageparts in different manners.
      *
-     *
-     * $structure is something like:
-     *
-     * array('main' => array(function() { return new DummyPagePart('A') }, function() { return new DummyPagePart('B') }),
-     *       'banners' => array($awesomeNanner));
-     *
-     * So it's an array containing the pageparts per region. Each pagepart is returned by a function.
-     * This is clean because we don't have to bother with variablenames which we have to remember to pass
-     * to the pagecreatorservice at the right time. With this method it's impossible to assign a wrong pagepart to a page.
-     * Unless you provide the incorrect page oviously ... .
-     *
-     * You can also include variables in the pagepart arrays if you want.
-     *
-     * Or optionally you can use the results of the getCreatorArgumentsForPagePartAndProperties function instead of an anonymous function.
-     *
-     * @param Node $node
+     * @param mixed(Node|string) $nodeOrInternalName
+     *      The node that you'd like to append the pageparts to. It's also possible to provide an internalname.
      * @param array $structure
-     * @param $language
+     *      The structure array is something like this:
+     *
+     *      array('main' => array(
+     *          function() { return new DummyPagePart('A') }, function() { return new DummyPagePart('B') }
+     *       ), 'banners' => array($awesomeNanner));
+     *
+     *      So it's an array containing the pageparts per region. Each pagepart is returned by a function.
+     *      This is clean because we don't have to bother with variablenames which we have to remember to pass
+     *      to the pagecreatorservice at the right time. With this method it's impossible to assign a wrong pagepart to a page.
+     *      Unless you provide the incorrect page oviously ... .
+     *
+     *      You can also include variables in the pagepart arrays if you want.
+     *
+     *      Or optionally you can use the results of the getCreatorArgumentsForPagePartAndProperties function instead of an anonymous function.
+     * @param string $language
+     *      The language of the translation you want to append to.
+     *
+     * @throws \LogicException
      */
-    public function addPagePartsToPage($nodeOrInternalName, array $structure, $language) {
+    public function addPagePartsToPage($nodeOrInternalName, array $structure, $language)
+    {
         $node = $this->getNode($nodeOrInternalName);
 
-        // First instantiate all pageparts. This way no pageparts will be saved if there is an issue instantiating some of them.
-        $instantiatedPageparts = array();
-        foreach ($structure as $context => $pageparts) {
-            $instantiatedPageparts[$context] = array();
+        // First instantiate all PageParts. This way no PageParts will be saved if there is an issue instantiating some of them.
+        $instantiatedPageParts = array();
+        foreach ($structure as $context => $pageParts) {
+            $instantiatedPageParts[$context] = array();
 
-            foreach ($pageparts as $pagePartOrFunction) {
+            foreach ($pageParts as $pagePartOrFunction) {
                 if (is_callable($pagePartOrFunction)) {
                     $pagePartOrFunction = $pagePartOrFunction();
 
@@ -119,28 +142,18 @@ class PagePartCreatorService {
                     throw new \LogicException('Detected a supposed pagepart that did not implement the PagePartInterface.');
                 }
 
-                $instantiatedPageparts[$context][] = $pagePartOrFunction;
+                $instantiatedPageParts[$context][] = $pagePartOrFunction;
             }
         }
 
         // All good. We can start saving.
-        foreach ($instantiatedPageparts as $context => $pageparts) {
-            foreach ($pageparts as $pagepart) {
-                $this->addPagePartToPage($node, $pagepart, $language, $context);
+        foreach ($instantiatedPageParts as $context => $pageParts) {
+            foreach ($pageParts as $pagePart) {
+                $this->addPagePartToPage($node, $pagePart, $language, $context);
             }
         }
     }
 
-    /**
-     * @param mixed(string|Node) $nodeOrInternalName
-     * @return object
-     */
-    private function getNode($nodeOrInternalName) {
-        if (is_string($nodeOrInternalName)) {
-            return $this->nodeRepo->findOneBy(array('internalName' => $nodeOrInternalName));
-        }
-        return $nodeOrInternalName;
-    }
 
     /**
      * A helper function to express what pagepart you want.
@@ -149,10 +162,13 @@ class PagePartCreatorService {
      *
      * It'll return an anonymous function which instantiates the pagepart.
      *
-     * @param $pagePartClassName The full class name of the pagepart you want to instantiate.
+     * @param string $pagePartClassName The full class name of the pagepart you want to instantiate.
      * @param array $setters An array of setternames and their values. array('setName' => 'Kim', 'isDeveloper' => true)
+     *
+     * @return callable The function that will instantiate a pagepart.
      */
-    public function getCreatorArgumentsForPagePartAndProperties($pagePartClassName, array $setters=null) {
+    public function getCreatorArgumentsForPagePartAndProperties($pagePartClassName, array $setters=null)
+    {
         return function() use ($pagePartClassName, $setters) {
             $pp = new $pagePartClassName;
 
@@ -164,5 +180,20 @@ class PagePartCreatorService {
 
             return $pp;
         };
+    }
+
+
+    /**
+     * @param mixed(string|Node) $nodeOrInternalName
+     *
+     * @return object
+     */
+    private function getNode($nodeOrInternalName)
+    {
+        if (is_string($nodeOrInternalName)) {
+            return $this->nodeRepo->findOneBy(array('internalName' => $nodeOrInternalName));
+        }
+
+        return $nodeOrInternalName;
     }
 }
---
+++ src/Kunstmaan/PagePartBundle/Resources/doc/PagePartBundle.md
@@ -0,0 +1,90 @@
+# PagePartBundle Documentation
+
+## Creating PageParts in Your Code
+
+We've provided a ```PagePartCreatorService``` that simplifies the creation of pageparts for your pages.
+
+This works nicely in conjunction with the ```PageCreatorService``` that's present in the NodeBundle.
+Another handy service is the ```MediaCreatorService``` that can be found in the MediaBundle.
+It's used to easily upload files to the Media part of the backend.
+
+The PagePartCreatorService supports several different syntax styles that can be interchanged with one another.
+
+There are 2 functions available. The first one is the basic function ```addPagePartToPage``` which can only
+add a single PagePart to a single page at once. It can however be added in a position of your own choosing.
+If the position parameter is left as null it'll just be appended at the end.
+
+It simply expects a Node instance or more conveniently an internal name of a node, a fully instantiated PagePart,
+the language for which translation to append it to and finally the position.
+
+
+The second function is far more useful and can be used to append multiple PageParts for multiple regions
+on a single page for a single language. The function's interface is quite flexible. It expects a named array with
+the name of the region as the key and an array containing the PageParts as the value.
+
+The pageparts don't have to be instantiated but can be callables instead.
+You can also mix & match instantiated PageParts and callables.
+No PageParts are saved before each callable has returned an instantiated PagePart.
+
+
+There are multiple of ways you can provide PagePart information.
+
+* The most obvious one is a PagePart object that you manually instantiated yourself.
+
+* The second way is an anonymous function that in itself instantiates a PagePart.
+This is a pretty useful way to do more complex things. You could for example attempt to
+upload assets using the ```MediaCreatorService```.
+```PHP
+function() {
+    $pp = new HeaderPagePart();
+    $pp->setTitle("General Conditions");
+    $pp->setNiv(1);
+    return $pp;
+}
+```
+
+* The last one is a convenience-method provided by the PagePartCreatorService.
+It basically expects the full class name as a string and a named array with
+the keys being the names of the functions to call and the values what will be provided. Perfect for calling setters etc.
+```PHP
+$ppCreatorService->getCreatorArgumentsForPagePartAndProperties('Kunstmaan\PagePartBundle\Entity\HeaderPagePart',
+    array('setNiv' => 1, 'setTitle' => 'General Conditions')
+)
+```
+
+It's also important to note that the singular function ```addPagePartToPage``` does NOT support callables.
+
+
+Below is an example of what you can do with this with all the styles interchanged.
+
+```PHP
+    $ppCreatorService = new PagePartCreatorService($this->container->get('doctrine.orm.entity_manager'));
+
+    $logo = new Logo();
+    $logo.setUrl('http://kunstmaan.be');
+
+    $pageparts = array(
+        'banners' => array(function() {
+                $pp = new Satellite();
+                $pp->setType('sputnik');
+                return $pp;
+            }, $logo
+        ),
+        'main' => array(
+                $ppCreatorService->getCreatorArgumentsForPagePartAndProperties('Kunstmaan\PagePartBundle\Entity\HeaderPagePart',
+                    array('setNiv' => 2, 'setTitle' => 'Some Title')
+                ),
+                $ppCreatorService->getCreatorArgumentsForPagePartAndProperties('Kunstmaan\PagePartBundle\Entity\TextPagePart',
+                    array('setContent' => '<p>A bunch of interesting content.</p>')
+                ),
+                $ppCreatorService->getCreatorArgumentsForPagePartAndProperties('Kunstmaan\PagePartBundle\Entity\LinePagePart'),
+                function() {
+                                $pp = new InfoButtonPagePart();
+                                $pp->setTitle('Show me more!');
+                                return $pp; // Don't forget to return the PagePart ;)
+                },
+            )
+        );
+
+        $ppCreatorService->addPagePartsToPage('homepage', $pageparts, 'en');
+```
