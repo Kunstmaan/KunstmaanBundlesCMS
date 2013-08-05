897c537c1358ced7a94b43817371d87a64159e64
Kim Ausloos <kim.ausloos@kunstmaan.be>
2013-08-05 16:25:13 +0200

    Add import

--- src/Kunstmaan/AdminBundle/Entity/Role.php
+++ src/Kunstmaan/AdminBundle/Entity/Role.php
@@ -5,6 +5,7 @@ namespace Kunstmaan\AdminBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\Role\RoleInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Role Entity
