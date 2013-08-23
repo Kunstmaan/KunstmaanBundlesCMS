a568c684e213bc3fe81d0fbfe80b6e2f3d36a38e
Stéphane Bakelants <stephane.bakelants@kunstmaan.be>
2013-08-23 14:34:13 +0200

    removed double entity annotation

--- src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/article/Entity/OverviewPage.php
+++ src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/article/Entity/OverviewPage.php
@@ -15,7 +15,6 @@ use Symfony\Component\HttpFoundation\Request;
  * The article overview page which shows its articles
  *
  * @ORM\Entity(repositoryClass="{{ namespace }}\Repository\{{ entity_class }}\{{ entity_class }}OverviewPageRepository")
- * @ORM\Entity()
  * @ORM\Table(name="{{ prefix }}{{ entity_class|lower }}overviewpages")
  */
 class {{ entity_class }}OverviewPage extends AbstractArticleOverviewPage
