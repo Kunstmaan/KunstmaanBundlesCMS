232f355cdd8b1f1bea7eb654d0da92056fc4813d
Joakim Löfgren <joakim.lofgren@klarna.com>
2014-03-10 14:43:36 +0100

    Update fixtures to include translation ids (issue #27)

--- src/Kunstmaan/TranslatorBundle/DataFixtures/ORM/TranslationFixtures.php
+++ src/Kunstmaan/TranslatorBundle/DataFixtures/ORM/TranslationFixtures.php
@@ -2,7 +2,8 @@
 
 namespace Kunstmaan\TranslatorBundle\DataFixtures\ORM;
 
-use Kunstmaan\TranslatorBundle\Entity\Translation;
+use Kunstmaan\TranslatorBundle\Model\Translation as Model;
+use Kunstmaan\TranslatorBundle\Entity\Translation as Entity;
 
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -14,51 +15,60 @@ use Doctrine\Common\Persistence\ObjectManager;
 class TranslationFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
     /**
+     * @var Kunstmaan\TranslatorBundle\Repository\TranslationRepository
+     */
+    protected $repo;
+
+    /**
      * Load data fixtures with the passed EntityManager
      *
      * @param ObjectManager $manager
      */
     public function load(ObjectManager $manager)
     {
-        $helloWorld = new Translation;
+        $this->repo = $manager->getRepository('Kunstmaan\TranslatorBundle\Entity\Translation');
+
+        $helloWorld = new Model;
         $helloWorld->setKeyword('heading.hello_world');
         $helloWorld->setDomain('messages');
-        $helloWorld->setCreatedAt(new \DateTime());
-        $helloWorld->setFlag(Translation::FLAG_NEW);
+
+        $translations = array(
+            'en' => 'Hello World!',
+            'fr' => 'Bonjour tout le monde',
+            'nl' => 'Hallo wereld!'
+        );
 
         $needForFlush = false;
+        foreach ($translations as $language => $text) {
+            if ($this->hasFixtureInstalled('messages', 'heading.hello_world', $language)) {
+                continue;
+            }
 
-        if (!$this->hasFixtureInstalled($manager, 'messages', 'heading.hello_world', 'en')) {
-            $helloWorld->setLocale('en');
-            $helloWorld->setText('Hello World!');
-            $manager->persist($helloWorld);
+            $helloWorld->addText($language, $text);
             $needForFlush = true;
         }
 
-        if (!$this->hasFixtureInstalled($manager, 'messages', 'heading.hello_world', 'fr')) {
-            $bonjour = clone $helloWorld;
-            $bonjour->setText('Bonjour tout le monde');
-            $bonjour->setLocale('fr');
-            $manager->persist($bonjour);
-            $needForFlush = true;
-        }
-
-        if (!$this->hasFixtureInstalled($manager, 'messages', 'heading.hello_world', 'nl')) {
-            $hallo = clone $helloWorld;
-            $hallo->setText('Hallo wereld!');
-            $hallo->setLocale('nl');
-            $manager->persist($hallo);
-            $needForFlush = true;
-        }
+        $this->repo->createTranslations($helloWorld);
 
         if ($needForFlush === true) {
             $manager->flush();
         }
     }
 
-    public function hasFixtureInstalled(ObjectManager $manager, $domain, $keyword, $locale)
+    /**
+     * Checks if the specified translation is installed.
+     *
+     * @param string $domain
+     * @param string $keyword
+     * @param string $locale
+     *
+     * @return bool
+     */
+    public function hasFixtureInstalled($domain, $keyword, $locale)
     {
-        return $manager->getRepository('Kunstmaan\TranslatorBundle\Entity\Translation')->findOneBy(array('domain' => $domain, 'keyword' => $keyword, 'locale' => $locale)) instanceof \Kunstmaan\TranslatorBundle\Entity\Translation;
+        $criteria = array('domain' => $domain, 'keyword' => $keyword, 'locale' => $locale);
+
+        return $this->repo->findOneBy($criteria) instanceof Entity;
     }
 
     /**
