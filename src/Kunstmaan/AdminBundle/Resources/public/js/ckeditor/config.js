3d6725374a097d49341455d56b8bd40cd30be3df
Indri Kenens <indri.kenens@kunstmaan.be>
2014-03-13 15:33:42 +0100

    new ckeditor

--- src/Kunstmaan/AdminBundle/Resources/public/js/ckeditor/config.js
+++ src/Kunstmaan/AdminBundle/Resources/public/js/ckeditor/config.js
@@ -1,43 +1,9 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
 
-/*CKEDITOR.editorConfig = function( config )
-{
-    config.skin = 'kCK';
-    config.startupFocus = false;
-    config.height = 500;
-    //config.filebrowserBrowseUrl = '/en/admin/pages/ckselecturl'
-    //config.filebrowserImageBrowseUrl = '/en/admin/media/ckeditor';
-    //config.filebrowserImageBrowseLinkUrl = '/en/admin/media/ckeditor';
-    config.filebrowserImageUploadUrl = '';
-    config.toolbar_Full =
-            [
-                { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike', '-', 'Subscript','Superscript' ] },
-                { name: 'lists', items : [ 'NumberedList','BulletedList' ] },
-                { name: 'dents', items : [ 'Outdent','Indent' ] },
-                { name: 'links', items : [ 'Link','Unlink' ] },
-                { name: 'insert', items : [ 'Image', 'SpecialChar' ] },
-                { name: 'format', items : [ 'Format' ] },
-                { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-                { name: 'editing', items : [ 'Find','Replace','-','SelectAll', 'RemoveFormat' ] },
-                { name: 'document', items : [ 'Source' ] }
-
-                 /*{ name: 'document',    items : [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
-                 { name: 'clipboard',   items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
-                 { name: 'editing',     items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
-                 { name: 'forms',       items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
-                 '/',
-                 { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-                 { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
-                 { name: 'links',       items : [ 'Link','Unlink','Anchor' ] },
-                 { name: 'insert',      items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak' ] },
-                 '/',
-                 { name: 'styles',      items : [ 'Styles','Format','Font','FontSize' ] },
-                 { name: 'colors',      items : [ 'TextColor','BGColor' ] },
-                 { name: 'tools',       items : [ 'Maximize', 'ShowBlocks','-','About' ] }*/
-           // ];
+//CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
