59bbc1907678a05e9b44566e74f3f01050575df2
Brent Roose <brent.roose@kunstmaan.be>
2014-03-25 16:52:48 +0100

    Added per-user language support

--- src/Kunstmaan/AdminBundle/EventListener/AdminLocaleListener.php
+++ src/Kunstmaan/AdminBundle/EventListener/AdminLocaleListener.php
@@ -1,36 +1,42 @@
 <?php
 
-namespace Kunstmaan\AdminBundle\EventListener
+namespace Kunstmaan\AdminBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Translation\TranslatorInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\SecurityContext;
+
 
 
 class AdminLocaleListener implements EventSubscriberInterface
 {
-    private $defaultLocale;
-
     private $translator;
+    private $context;
 
 
-    public function __construct(TranslatorInterface $translator, $defaultLocale = 'en')
+    public function __construct(SecurityContext $context, TranslatorInterface $translator)
     {
-        $this->defaultLocale    = $defaultLocale;
-        $this->translator       = $translator;
+        $this->translator = $translator;
+        $this->context = $context;
     }
 
-
-    public function onKernelRequest(GetResponseEvent $event)
+    public function onKernelView(GetResponseEvent $event)
     {
         $url = $event->getRequest()->getRequestUri();
 
-        preg_match("/^\/(app_(.*)\.php\/)?[a-z][a-z]\/admin\/(.*)/", $url, $match);
+        if ($this->context->getToken()) {
+            $locale = $this->context->getToken()->getUser()->getLocaleAdmin();
 
-        if (count($match)) {
-            $this->translator->setLocale($this->defaultLocale);
+            preg_match("/^\/(app_(.*)\.php\/)?[a-z][a-z]\/admin\/(.*)/", $url, $match);
+
+            if (count($match) && $locale) {
+                $this->translator->setLocale($locale);
+            }
         }
+
     }
 
 
