f0395a7bc53b8dd7f508f4e3bf4ccf344748c664
Brent Roose <brent.roose@kunstmaan.be>
2014-05-06 16:17:23 +0200

    added some css

--- src/Kunstmaan/DashboardBundle/Resources/public/js/setup/setup.js
+++ src/Kunstmaan/DashboardBundle/Resources/public/js/setup/setup.js
@@ -1,14 +1,13 @@
 $(function () {
 
-    $('#properties').css('display', 'none');
-    $('#profiles').css('display', 'none');
-    // setAccountData();
+    // $('#properties').css('display', 'none');
+    // $('#profiles').css('display', 'none');
 
     /* =============================== ACCOUNTS =============================== */
 
         $("#accounts").change(function() {
-            $('#properties').fadeOut();
-            $('#profiles').fadeOut();
+            $('#properties').removeClass('setup-item__shown');
+            $('#profiles').removeClass('setup-item__shown');
             $("#accounts option:selected").each(function() {
                 var id = $(this).attr('data-id');
                 var url = $('#path_account_save').attr('data-url');
@@ -23,25 +22,7 @@ $(function () {
             });
         });
 
-
-
     /* =============================== PROPERTIES =============================== */
-
-        $("#properties").change(function() {
-            $('#profiles').fadeOut();
-            $("#properties option:selected").each(function() {
-                var id = $(this).attr('data-id');
-                var url = $('#path_property_save').attr('data-url');
-                $.ajax({
-                    type: 'get',
-                    url: url,
-                    data: {'id' : id},
-                    success: function (data) {
-                        setProfileData();
-                    }
-                });
-            });
-        });
 
         function setPropertyData() {
             var url = $('#path_properties').attr('data-url');
@@ -59,28 +40,29 @@ $(function () {
                     for(var i = 0; i < data.length; i++) {
                         var option = $('<option>', { 'data-id': data[i].propertyId, text: data[i].propertyName});
                         $('#properties').append(option);
-                        $('#properties').fadeIn();
+                        $('#properties').addClass('setup-item__shown');
                     }
                 }
             });
         }
 
-    /* =============================== PROFILE =============================== */
-
-        $("#profiles").change(function() {
-            $("#profiles option:selected").each(function() {
+        $("#properties").change(function() {
+            $('#profiles').removeClass('setup-item__shown');
+            $("#properties option:selected").each(function() {
                 var id = $(this).attr('data-id');
-                var url = $('#path_profile_save').attr('data-url');
+                var url = $('#path_property_save').attr('data-url');
                 $.ajax({
                     type: 'get',
                     url: url,
                     data: {'id' : id},
                     success: function (data) {
-                        $('#submit_save').removeAttr('disabled');
+                        setProfileData();
                     }
                 });
             });
         });
+
+    /* =============================== PROFILE =============================== */
 
         function setProfileData() {
             var url = $('#path_profiles').attr('data-url');
@@ -98,10 +80,25 @@ $(function () {
                     for(var i = 0; i < data.length; i++) {
                         var option = $('<option>', { 'data-id': data[i].profileId, text: data[i].profileName});
                         $('#profiles').append(option);
-                        $('#profiles').fadeIn();
+                        $('#profiles').addClass('setup-item__shown');
                     }
                 }
             });
         }
+
+        $("#profiles").change(function() {
+            $("#profiles option:selected").each(function() {
+                var id = $(this).attr('data-id');
+                var url = $('#path_profile_save').attr('data-url');
+                $.ajax({
+                    type: 'get',
+                    url: url,
+                    data: {'id' : id},
+                    success: function (data) {
+                        $('#submit_save').removeAttr('disabled');
+                    }
+                });
+            });
+        });
 
 });
