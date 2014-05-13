3671e43de2a9577a8188a35b46a42b5bde2f3154
Brent Roose <brent.roose@kunstmaan.be>
2014-05-13 10:42:39 +0200

    fixed inaccurate data

--- src/Kunstmaan/DashboardBundle/Command/Helper/Analytics/GoalCommandHelper.php
+++ src/Kunstmaan/DashboardBundle/Command/Helper/Analytics/GoalCommandHelper.php
@@ -185,13 +185,16 @@ class GoalCommandHelper extends AbstractAnalyticsCommandHelper
 
             $chartData = array();
             $totalVisits = 0;
+            $goalVisits = 0;
             $i = 0;
             // Fill the chartdata array
             foreach ($goalEntry['visits'] as $timestamp => $visits) {
                 $totalVisits += $visits;
+                $goalVisits += $visits;
                 if ($timespan <= 7 && $timespan > 1) {
                     if ($i%5 == 0) {
-                        $chartData[] = array('timestamp' => $timestamp, 'conversions' => $visits);
+                        $chartData[] = array('timestamp' => $timestamp, 'conversions' => $goalVisits);
+                        $goalVisits = 0;
                     }
                 } else {
                     $chartData[] = array('timestamp' => $timestamp, 'conversions' => $visits);
