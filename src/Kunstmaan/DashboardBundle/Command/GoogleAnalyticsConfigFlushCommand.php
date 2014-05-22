8b8dfcb6e882663505e6320cb466cc3352b77a31
Brent Roose <brent.roose@kunstmaan.be>
2014-05-22 11:29:29 +0200

    added config flush command

---
+++ src/Kunstmaan/DashboardBundle/Command/GoogleAnalyticsConfigFlushCommand.php
@@ -0,0 +1,49 @@
+<?php
+namespace Kunstmaan\DashboardBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+class GoogleAnalyticsConfigFlushCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('kuma:dashboard:widget:googleanalytics:config:flush')
+            ->setDescription('Flush configs')
+            ->addOption(
+                'config',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Specify to only flush one config',
+                false
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $configRepository = $em->getRepository('KunstmaanDashboardBundle:AnalyticsConfig');
+        $configId  = $input->getOption('config');
+        $configs = array();
+
+        try {
+            if ($configId) {
+                $configs[] = $configRepository->find($configId);
+            } else {
+                $configs = $configRepository->findAll();
+            }
+
+            foreach ($configs as $config) {
+                $em->remove($config);
+            }
+            $em->flush();
+            $output->writeln('<fg=green>Config flushed</fg=green>');
+        } catch (\Exception $e) {
+            $output->writeln('<fg=red>'.$e->getMessage().'</fg=red>');
+        }
+    }
+}
