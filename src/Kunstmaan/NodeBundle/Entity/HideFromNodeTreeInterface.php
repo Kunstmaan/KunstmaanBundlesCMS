a5f4156cf2e2bf3deb4ba1bf7e0b5a1e4a8946d7
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2014-07-30 14:19:15 +0200

    support hidden tree nodes

---
+++ src/Kunstmaan/NodeBundle/Entity/HideFromNodeTreeInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by Kunstmaan.
+ * Date: 30/07/14
+ * Time: 13:37
+ */
+
+namespace Kunstmaan\NodeBundle\Entity;
+
+interface HideFromNodeTreeInterface
+{
+}
--- src/Kunstmaan/NodeBundle/Helper/Menu/PageMenuAdaptor.php
+++ src/Kunstmaan/NodeBundle/Helper/Menu/PageMenuAdaptor.php
@@ -9,6 +9,8 @@ use Kunstmaan\AdminBundle\Helper\Menu\MenuBuilder;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuItem;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuAdaptorInterface;
 use Kunstmaan\AdminBundle\Helper\Menu\TopMenuItem;
+use Kunstmaan\NodeBundle\Entity\HideFromNodeTreeInterface;
+use Kunstmaan\NodeBundle\Entity\Node;
 use Kunstmaan\NodeBundle\Helper\NodeMenuItem;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -52,14 +54,19 @@ class PageMenuAdaptor implements MenuAdaptorInterface
             }
             $children[] = $menuItem;
         } else {
-            $treeNodes = $this->getTreeNodes($request->getLocale(), PermissionMap::PERMISSION_EDIT, $this->aclNativeHelper, true);
+            $treeNodes     = $this->getTreeNodes(
+                $request->getLocale(),
+                PermissionMap::PERMISSION_EDIT,
+                $this->aclNativeHelper,
+                true
+            );
             $activeNodeIds = $this->getActiveNodeIds($request);
 
-            if ('KunstmaanNodeBundle_nodes' == $parent->getRoute()  && isset($treeNodes[0])) {
+            if ('KunstmaanNodeBundle_nodes' == $parent->getRoute() && isset($treeNodes[0])) {
                 $this->processNodes($menu, $children, $treeNodes[0], $parent, $activeNodeIds);
             } elseif ('KunstmaanNodeBundle_nodes_edit' == $parent->getRoute()) {
                 $parentRouteParams = $parent->getRouteparams();
-                $parent_id = $parentRouteParams['id'];
+                $parent_id         = $parentRouteParams['id'];
                 if (array_key_exists($parent_id, $treeNodes)) {
                     $this->processNodes($menu, $children, $treeNodes[$parent_id], $parent, $activeNodeIds);
                 }
@@ -70,21 +77,26 @@ class PageMenuAdaptor implements MenuAdaptorInterface
     /**
      * Get the list of nodes that is used in the admin menu.
      *
-     * @param string $lang
-     * @param string $permission
+     * @param string          $lang
+     * @param string          $permission
      * @param AclNativeHelper $aclNativeHelper
-     * @param bool $includeHiddenFromNav
+     * @param bool            $includeHiddenFromNav
+     *
      * @return array
      */
     private function getTreeNodes($lang, $permission, AclNativeHelper $aclNativeHelper, $includeHiddenFromNav)
     {
         if (is_null($this->treeNodes)) {
-            $repo = $this->em->getRepository('KunstmaanNodeBundle:Node');
+            $repo            = $this->em->getRepository('KunstmaanNodeBundle:Node');
             $this->treeNodes = array();
 
             // Get all nodes that should be shown in the menu
             $allNodes = $repo->getAllMenuNodes($lang, $permission, $aclNativeHelper, $includeHiddenFromNav);
-            foreach($allNodes as $nodeInfo) {
+            /** @var Node $nodeInfo */
+            foreach ($allNodes as $nodeInfo) {
+                if ($this->isHiddenFromNodeTree($nodeInfo['ref_entity_name'])) {
+                    continue;
+                }
                 $parent_id = is_null($nodeInfo['parent']) ? 0 : $nodeInfo['parent'];
                 unset($nodeInfo['parent']);
                 $this->treeNodes[$parent_id][] = $nodeInfo;
@@ -96,9 +108,29 @@ class PageMenuAdaptor implements MenuAdaptorInterface
     }
 
     /**
-     * Get an arrey with the id's off all nodes in the tree that should be expanded.
+     * Determine if current node should be hidden from the node tree
+     *
+     * @param string $refEntityName
+     *
+     * @return bool
+     */
+    private function isHiddenFromNodeTree($refEntityName)
+    {
+        $isHidden = false;
+        if (class_exists($refEntityName)) {
+            $page = new $refEntityName();
+            $isHidden = ($page instanceof HideFromNodeTreeInterface);
+            unset($page);
+        }
+
+        return $isHidden;
+    }
+
+    /**
+     * Get an array with the id's off all nodes in the tree that should be expanded.
      *
      * @param $request
+     *
      * @return array
      */
     private function getActiveNodeIds($request)
@@ -107,8 +139,8 @@ class PageMenuAdaptor implements MenuAdaptorInterface
             if (stripos($request->attributes->get('_route'), 'KunstmaanNodeBundle_nodes_edit') === 0) {
                 $repo = $this->em->getRepository('KunstmaanNodeBundle:Node');
 
-                $currentNode = $repo->findOneById($request->attributes->get('id'));
-                $parentNodes = $repo->getAllParents($currentNode);
+                $currentNode         = $repo->findOneById($request->attributes->get('id'));
+                $parentNodes         = $repo->getAllParents($currentNode);
                 $this->activeNodeIds = array();
                 foreach ($parentNodes as $parentNode) {
                     $this->activeNodeIds[] = $parentNode->getId();
@@ -126,8 +158,13 @@ class PageMenuAdaptor implements MenuAdaptorInterface
      * @param MenuItem       $parent        The parent menu item
      * @param array          $activeNodeIds List with id's of all nodes that should be expanded in the tree
      */
-    private function processNodes(MenuBuilder $menu, array &$children, array $nodes, MenuItem $parent = null, array $activeNodeIds)
-    {
+    private function processNodes(
+        MenuBuilder $menu,
+        array &$children,
+        array $nodes,
+        MenuItem $parent = null,
+        array $activeNodeIds
+    ) {
         foreach ($nodes as $child) {
             $menuItem = new MenuItem($menu);
             $menuItem->setRoute('KunstmaanNodeBundle_nodes_edit');
