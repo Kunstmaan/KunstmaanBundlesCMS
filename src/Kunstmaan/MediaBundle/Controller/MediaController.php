b8b11cd5130af9d579fc5a2d71aeaf0f1544b889
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-01-06 09:01:58 +0100

    exit() and die() functions should be avoided

--- src/Kunstmaan/MediaBundle/Controller/MediaController.php
+++ src/Kunstmaan/MediaBundle/Controller/MediaController.php
@@ -171,7 +171,9 @@ class MediaController extends Controller
         // Remove old temp files
         if ($cleanupTargetDir) {
             if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
-                die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+		$response = new Response('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
             }
 
             while (($file = readdir($dir)) !== false) {
@@ -192,21 +194,29 @@ class MediaController extends Controller
 
         // Open temp file
         if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
-            die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+	    $response = new Response('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+	    $response->headers->set('Content-Type', 'application/json');
+	    return $response;
         }
 
         if (!empty($_FILES)) {
             if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
-                die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+		$response = new Response('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
             }
 
             // Read binary input stream and append it to temp file
             if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
-                die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+		$response = new Response('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
             }
         } else {
             if (!$in = @fopen("php://input", "rb")) {
-                die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+		$response = new Response('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
             }
         }
 
