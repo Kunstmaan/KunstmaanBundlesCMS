f88ecd95679ceee9eb0409524451b49b74196e28
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-01-06 13:29:31 +0100

    Set the defaults correctly

--- src/Kunstmaan/VotingBundle/DependencyInjection/KunstmaanVotingExtension.php
+++ src/Kunstmaan/VotingBundle/DependencyInjection/KunstmaanVotingExtension.php
@@ -56,7 +56,7 @@ class KunstmaanVotingExtension extends Extension
 
         // When no values are defined, initialize with defaults
         foreach($possibleActions as $action) {
-            if (!is_array($config['actions'][$action])) {
+            if (!isset($config['actions'][$action]) && !is_array($config['actions'][$action])) {
                 $config['actions'][$action]['default_value'] = ( $action == 'down_vote' ? -1 * $votingDefaultValue : $votingDefaultValue );
             }
         }
