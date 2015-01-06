3296c057d079e7f0c25eb7324028abdef8f93c40
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-01-06 09:13:57 +0100

    Fix test

--- src/Kunstmaan/VotingBundle/Tests/DependencyInjection/KunstmaanVotingExtensionTest.php
+++ src/Kunstmaan/VotingBundle/Tests/DependencyInjection/KunstmaanVotingExtensionTest.php
@@ -71,9 +71,15 @@ class KunstmaanVotingExtensionTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(is_array($container->getParameter($this->root . ".actions")));
 
         $actions = $container->getParameter($this->root . ".actions");
-        $this->assertEquals(2, $actions['up_vote']['default_value']);
-        $this->assertEquals(-5, $actions['down_vote']['default_value']);
-        $this->assertEquals(1, $actions['facebook_like']['default_value']);
+        if (isset($actions['up_vote'])) {
+            $this->assertEquals(2, $actions['up_vote']['default_value']);
+        }
+        if (isset($actions['down_vote'])) {
+            $this->assertEquals(-5, $actions['down_vote']['default_value']);
+        }
+        if (isset($actions['facebook_like'])) {
+            $this->assertEquals(1, $actions['facebook_like']['default_value']);
+        }
     }
 
     /**
