54825811bbe70857505a28c1667580c6473a7f48
Indri Kenens <indri.kenens@kunstmaan.be>
2015-01-29 16:42:42 +0100

    override jshintrc file in root

--- src/Kunstmaan/GeneratorBundle/Generator/LayoutGenerator.php
+++ src/Kunstmaan/GeneratorBundle/Generator/LayoutGenerator.php
@@ -62,7 +62,7 @@ class LayoutGenerator extends KunstmaanGenerator
      */
     private function generateJshintrcFile()
     {
-        $this->copyFiles($this->skeletonDir.'/jshint/.jshintrc', $this->rootDir, true);
+        $this->renderSingleFile($this->skeletonDir.'/jshint/.jshintrc', $this->rootDir, '.jshintrc', array('bundle' => $this->bundle), true);
         $this->assistant->writeLine('Generating jshint configuration : <info>OK</info>');
     }
 
