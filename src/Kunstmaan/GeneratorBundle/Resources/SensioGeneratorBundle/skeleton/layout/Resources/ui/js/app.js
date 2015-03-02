86386fb06ddf03f8bd6bd580b3be3a96ab3bcb05
Indri Kenens <indri.kenens@kunstmaan.be>
2015-03-02 16:07:02 +0100

    forms

--- src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/layout/Resources/ui/js/app.js
+++ src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/layout/Resources/ui/js/app.js
@@ -7,6 +7,20 @@ projectname = (function($, window, undefined) {
     init = function() {
         cargobay.scrollToTop.init();
         cargobay.toggle.init();
+
+        initForms();
+    };
+
+    initForms = function() {
+        if( $('.form--default').length ) {
+            $('.form-control').bind('blur', function () {
+                if( $(this):filled ) {
+                    $(this).addClass('form-control--filled');
+                } else {
+                    $(this).removeClass('form-control--filled');
+                }
+            });
+        }
     };
 
     return {
