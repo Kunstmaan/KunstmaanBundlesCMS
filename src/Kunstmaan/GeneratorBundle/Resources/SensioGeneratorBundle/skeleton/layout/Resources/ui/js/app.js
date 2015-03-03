68bc641344b4726c4361e43ccba01839b9f377a1
Indri Kenens <indri.kenens@kunstmaan.be>
2015-03-03 14:35:21 +0100

    forms

--- src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/layout/Resources/ui/js/app.js
+++ src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/layout/Resources/ui/js/app.js
@@ -13,11 +13,26 @@ projectname = (function($, window, undefined) {
 
     initForms = function() {
         if( $('.form--default').length ) {
+
+            $('.form-control').bind('focus', function () {
+                $(this).addClass('form-control--filled');
+            });
             $('.form-control').bind('blur', function () {
                 if( $(this).val() ) {
                     $(this).addClass('form-control--filled');
                 } else {
                     $(this).removeClass('form-control--filled');
+                }
+            });
+
+            $('.js-form-control-choice').bind('focus', function () {
+                $(this).parent().parent().find('.form-widget--choices').addClass('form-widget--choices--filled');
+            });
+            $('.js-form-control-choice').bind('blur', function () {
+                if( $(this).val() ) {
+                    $(this).parent().parent().find('.form-widget--choices').addClass('form-widget--choices--filled');
+                } else {
+                    $(this).parent().parent().find('.form-widget--choices').removeClass('form-widget--choices--filled');
                 }
             });
         }
