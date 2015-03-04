a2540587e4b3fcdcd529d9205f803e778f8c2929
Ibe Vanmeenen <ibe.vanmeenen@kunstmaan.be>
2015-03-04 12:27:05 +0100

    start check dnd

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_app-sidebar-tree.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_app-sidebar-tree.js
@@ -9,105 +9,145 @@ kunstmaanbundles.sidebartree = (function($, window, undefined) {
         $searchField = $('#app__sidebar__search');
 
     init = function() {
-        buildTree();
-        searchTree();
+        if($sidebarNavContainer !== 'undefined' && $sidebarNavContainer !== null) {
+            buildTree();
+            searchTree();
+        }
     };
 
     buildTree = function() {
-        if($sidebarNavContainer !== 'undefined' && $sidebarNavContainer !== null) {
 
-            // Show when ready
-            $sidebarNavContainer.on('ready.jstree', function() {
-                // http://www.jstree.com/api/#/?q=.jstree%20Event&f=ready.jstree
+        // Show when ready
+        $sidebarNavContainer.on('ready.jstree', function() {
+            // http://www.jstree.com/api/#/?q=.jstree%20Event&f=ready.jstree
 
-                $sidebar.addClass('app__sidebar--tree-ready');
+            $sidebar.addClass('app__sidebar--tree-ready');
+        });
+
+
+        // Go to url
+        $sidebarNavContainer.on('changed.jstree', function(e, data) {
+            // http://www.jstree.com/api/#/?q=.jstree%20Event&f=changed.jstree
+
+            var href = data.event.currentTarget.href;
+
+            document.location.href = href;
+        });
+
+
+        // Drag and drop callback
+        $sidebarNavContainer.on('move_node.jstree', function(e, data) {
+            // http://www.jstree.com/api/#/?q=.jstree%20Event&f=move_node.jstree
+
+            // Vars
+            var $container = $(this),
+                parentNode = data.parent,
+                reorderUrl = $container.data('reorder-url'),
+                params = {
+                    nodes : []
+                };
+
+            // Recalc id's
+            $('#' + parentNode).find('> ul > li').each(function() {
+                var newId = $(this).attr('id').replace(/node-/,'');
+
+                console.log('reset id: ' + newId);
+
+                params.nodes.push(newId);
             });
 
+            console.log('parentNode: ' + parentNode);
+            console.log('results:');
+            console.log(params);
 
-            // Go to url
-            $sidebarNavContainer.on('changed.jstree', function(e, data) {
-                // http://www.jstree.com/api/#/?q=.jstree%20Event&f=changed.jstree
-
-                var href = data.event.currentTarget.href;
-
-                document.location.href = href;
-            });
-
-
-            // Drag and drop callback
-            $sidebarNavContainer.on('move_node.jstree', function(e, data) {
-                // http://www.jstree.com/api/#/?q=.jstree%20Event&f=move_node.jstree
-
-                // Vars
-                var $container = $(this),
-                    parentNode = data.parent,
-                    reorderUrl = $container.data('reorder-url'),
-                    params = {
-                        nodes : []
-                    };
-
-                // Reset id's
-                $('#' + parentNode).find(' > ul > li').each(function() {
-                    var id = $(this).attr('id').replace(/node-/,'');
-
-                    params.nodes.push(id);
-                });
-
-                // Save
-                $.post(
-                    reorderUrl,
-                    params,
-                    function(result){
-                        console.log('move_node saved');
-                    }
-                );
-            });
-
-
-            // Create
-            $sidebarNavContainer.jstree({
-                'core': {
-                    'check_callback' : true
-                },
-                'plugins': [
-                    'types',
-                    'search',
-                    'dnd'
-                ],
-                'types': {
-                    '#': {
-                        'icon': 'fa fa-home'
-                    },
-                    'default': {
-                        'icon' : 'fa fa-file-o'
-                    },
-                    'offline': {
-                        'icon': 'fa fa-chain-broken'
-                    },
-                    'folder': {
-                        'icon': 'fa fa-folder-o'
-                    },
-                    'image': {
-                        'icon': 'fa fa-picture-o'
-                    },
-                    'files': {
-                        'icon': 'fa fa-files-o'
-                    },
-                    'slideshow': {
-                        'icon': 'fa fa-desktop'
-                    },
-                    'video': {
-                        'icon': 'fa fa-film'
-                    },
-                    'media': {
-                        'icon': 'fa fa-folder-o'
-                    }
-                },
-                'search' : {
-                    'show_only_matches': true
+            //; Save
+            $.post(
+                reorderUrl,
+                params,
+                function(result){
+                    console.log('move_node saved');
+                    console.log(result);
                 }
-            });
-        }
+            );
+        });
+
+
+        // Create
+        $sidebarNavContainer.jstree({
+            'core': {
+                'check_callback': function (operation, node, node_parent, node_position, more) {
+                    // operation can be 'create_node', 'rename_node', 'delete_node', 'move_node' or 'copy_node'
+                    // in case of 'rename_node' node_position is filled with the new node name
+
+                    if(operation === 'move_node') {
+                        console.log('-- move_node ---------------');
+
+                        // Has it got a parent (no dnd outsite home)
+                        console.log('node_parent.id: ' + node_parent.id);
+
+                        if(!node_parent || node_parent.id === '#') {
+                            return false;
+                        }
+
+                        // Go ahead
+                        return true;
+
+
+                        // OLD
+                        // "check_move" : function (m) {
+                        //     var p = this._get_parent(m.o);
+                        //     if(!p) return false;
+                        //     p = p == -1 ? this.get_container() : p;
+                        //     if(p === m.np) return true;
+                        //     if(p[0] && m.np[0] && p[0] === m.np[0]) return true;
+                        //     return false;
+                        // }
+
+                        console.log('-----------------');
+
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            'plugins': [
+                'types',
+                'search',
+                'dnd'
+            ],
+            'types': {
+                '#': {
+                    'icon': 'fa fa-home'
+                },
+                'default': {
+                    'icon' : 'fa fa-file-o'
+                },
+                'offline': {
+                    'icon': 'fa fa-chain-broken'
+                },
+                'folder': {
+                    'icon': 'fa fa-folder-o'
+                },
+                'image': {
+                    'icon': 'fa fa-picture-o'
+                },
+                'files': {
+                    'icon': 'fa fa-files-o'
+                },
+                'slideshow': {
+                    'icon': 'fa fa-desktop'
+                },
+                'video': {
+                    'icon': 'fa fa-film'
+                },
+                'media': {
+                    'icon': 'fa fa-folder-o'
+                }
+            },
+            'search' : {
+                'show_only_matches': true
+            }
+        });
     };
 
     searchTree = function() {
