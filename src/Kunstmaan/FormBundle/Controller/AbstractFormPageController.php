2405ccc90a5d3bebd806fd38fbee99c81b49e571
Kristof Jochmans <kristof.jochmans@kunstmaan.be>
2015-04-16 14:51:21 +0200

    fix the formpage submission logic

--- src/Kunstmaan/FormBundle/Controller/AbstractFormPageController.php
+++ src/Kunstmaan/FormBundle/Controller/AbstractFormPageController.php
@@ -2,45 +2,38 @@
 
 namespace Kunstmaan\FormBundle\Controller;
 
-
 use Kunstmaan\NodeBundle\Helper\RenderContext;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Class AbstractFormPageController
- * @package Kunstmaan\FormBundle\Controller
- */
-class AbstractFormPageController extends Controller{
-
-
+class AbstractFormPageController extends Controller
+{
     /**
      * @param Request $request
      * @return null
      */
-    public function serviceAction(Request $request){
-
-        $context = array();
+    public function serviceAction(Request $request)
+    {
         $thanksParam = $request->get('thanks');
-        if (!empty($thanksParam)) {
-            $context["thanks"] = true;
-
-            return null;
-        }
         $entity = $request->attributes->get('_entity');
-
-        $renderContext = new RenderContext(
-            array(
-                'nodetranslation' => $request->attributes->get('_nodeTranslation'),
-                'slug'            => $request->attributes->get('url'),
-                'page'            => $entity,
-                'resource'        => $entity,
-                'nodemenu'        => $request->attributes->get('_nodeMenu'),
-            )
+        $context = array(
+            'nodetranslation' => $request->attributes->get('_nodeTranslation'),
+            'slug'            => $request->attributes->get('url'),
+            'page'            => $entity,
+            'resource'        => $entity,
+            'nodemenu'        => $request->attributes->get('_nodeMenu'),
         );
 
-        $this->container->get('kunstmaan_form.form_handler')->handleForm($entity, $request, $renderContext);
+        if (!empty($thanksParam)) {
+            $context['thanks'] = true;
+        }
+
+        $renderContext = new RenderContext($context);
+        $result = $this->container->get('kunstmaan_form.form_handler')->handleForm($entity, $request, $renderContext);
+        if ($result instanceof Response) {
+            return $result;
+        }
 
         $request->attributes->set('_renderContext', $renderContext->getArrayCopy());
     }
