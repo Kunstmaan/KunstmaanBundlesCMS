4fc90b99d076d034c78f9c92a88755a727b977af
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2015-05-28 17:56:35 +0200

    make sure og image can be removed (#434)

--- src/Kunstmaan/SeoBundle/Entity/Seo.php
+++ src/Kunstmaan/SeoBundle/Entity/Seo.php
@@ -350,7 +350,7 @@ class Seo extends AbstractEntity
      *
      * @return Seo
      */
-    public function setOgImage(Media $ogImage)
+    public function setOgImage(Media $ogImage = null)
     {
         $this->ogImage = $ogImage;
 
