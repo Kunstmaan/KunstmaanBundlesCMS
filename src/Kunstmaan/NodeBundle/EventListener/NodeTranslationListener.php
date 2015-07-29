cf98ba97211ec5053755f4957c8a290109b9448e
Tarjei Huse <tarjei.huse@gmail.com>
2015-07-29 12:24:35 +0200

    Allow overriding of the Session variable
    When I try to create Nodes in my fixtures, I get errors due to the NodeTranslationListener emitting flash messages even though the work happens in the console.
    
    I'm not sure if there is a better way, but allowing the console to inject a MockSession is the simplest way to get around this problem.
    
    The code I end up using is:
    ```php MyCommand.php
    $listener = $this->container->get('kunstmaan_node.nodetranslation.listener');
    $session = new Session(new MockArraySessionStorage(), new AttributeBag(), new FlashBag());
    $listener->setSession($session);
    ```

--- src/Kunstmaan/NodeBundle/EventListener/NodeTranslationListener.php
+++ src/Kunstmaan/NodeBundle/EventListener/NodeTranslationListener.php
@@ -303,4 +303,17 @@ class NodeTranslationListener
             return $string . $append . '1';
         }
     }
+    
+    /**
+     * @param \Symfony\Component\HttpFoundation\Session\Session $session
+     *
+     * @return $this
+     */
+    public function setSession($session)
+    {
+        $this->session = $session;
+
+        return $this;
+    }
+    
 }
