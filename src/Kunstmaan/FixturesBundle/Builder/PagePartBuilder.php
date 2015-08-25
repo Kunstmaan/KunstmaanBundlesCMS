779b187fb9cf1f4ca47c5d35388de1b301e91fe1
Wesley Lancel <wesley@yappa.be>
2015-08-25 21:26:32 +0200

    Issue #638: make sure pagepart translations are saved in fixtures

--- src/Kunstmaan/FixturesBundle/Builder/PagePartBuilder.php
+++ src/Kunstmaan/FixturesBundle/Builder/PagePartBuilder.php
@@ -74,6 +74,7 @@ class PagePartBuilder implements BuilderInterface
             $page = $additionalEntities[$key];
             $this->populator->populate($pp, $data);
             $this->em->persist($pp);
+            $this->em->flush();
 
             // Find latest position.
             $position = array_key_exists('position', $params) ? $params['position'] : null;
