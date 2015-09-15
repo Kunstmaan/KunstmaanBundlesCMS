f6190bd6c8cf7f81d2a6ca64605872eb7849c6c5
Wim Vandersmissen <wim.vandersmissen@gmail.com>
2015-09-15 16:09:20 +0200

    performance fixes

--- src/Kunstmaan/NodeBundle/Entity/Node.php
+++ src/Kunstmaan/NodeBundle/Entity/Node.php
@@ -14,7 +14,14 @@ use Gedmo\Tree\Node as GedmoNode;
  * Node
  *
  * @ORM\Entity(repositoryClass="Kunstmaan\NodeBundle\Repository\NodeRepository")
- * @ORM\Table(name="kuma_nodes", indexes={@ORM\Index(name="idx_node_internal_name", columns={"internal_name"}), @ORM\Index(name="idx_node_ref_entity_name", columns={"ref_entity_name"})})
+ * @ORM\Table(
+ *      name="kuma_nodes",
+ *      indexes={
+ *          @ORM\Index(name="idx_node_internal_name", columns={"internal_name"}),
+ *          @ORM\Index(name="idx_node_ref_entity_name", columns={"ref_entity_name"}),
+ *          @ORM\Index(name="idx_node_tree", columns={"deleted", "hidden_from_nav", "lft", "rgt"})
+ *      }
+ * )
  * @ORM\HasLifecycleCallbacks()
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  * @Gedmo\Tree(type="nested")
