14b1901386a535d70f271bb03b43aad77acce906
Bart Van den Brande <bart.van.den.brande@kunstmaan.be>
2015-11-18 09:27:43 +0100

    fix parent menu item in menu edit form

--- src/Kunstmaan/MenuBundle/Entity/MenuItem.php
+++ src/Kunstmaan/MenuBundle/Entity/MenuItem.php
@@ -2,6 +2,7 @@
 
 namespace Kunstmaan\MenuBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -14,5 +15,63 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class MenuItem extends BaseMenuItem
 {
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(targetEntity="MenuItem", mappedBy="parent")
+     * @ORM\OrderBy({"lft" = "ASC"})
+     */
+    protected $children;
 
+    /**
+     * @var MenuItem
+     *
+     * @Gedmo\TreeParent
+     * @ORM\ManyToOne(targetEntity="MenuItem", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    protected $parent;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * @param ArrayCollection $children
+     */
+    public function setChildren($children)
+    {
+        $this->children = $children;
+    }
+    /**
+     * @return MenuItem
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param MenuItem $parent
+     *
+     * @return MenuItem
+     */
+    public function setParent($parent)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
 }
