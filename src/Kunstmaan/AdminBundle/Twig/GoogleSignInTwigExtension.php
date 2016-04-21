ee8d93d00eec2bbdcd08c66b35361b35940cc500
Numkil <kevin.jossart@hotmail.com>
2016-04-21 14:43:31 +0200

    [AdminBundle] Auto Google SignIn with Guard (#1119)
    * auto admin signin with google
    
    add check for verification
    
    catch direct check url when loggedin
    
    * Complete login by OAuth functionality
    [AdminBundle]
    Only make a new user when email adress hasn't been used, otherwise extend existing account
    Allow configuring multiple domains with multiple access_levels
    Write documentation.
    Use custom user class defined by fos_user.class parameter.
    Show Error message whenever something goes wrong with authentication
    
    Only load javascript api when functionality is enabled
    
    extract logic for creating users and it's dependencies from the authenticator and put
    them in a service

---
+++ src/Kunstmaan/AdminBundle/Twig/GoogleSignInTwigExtension.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Kunstmaan\AdminBundle\Twig;
+
+use Twig_Environment;
+use Twig_Extension;
+
+/**
+ * Class GoogleSignInTwigExtension
+ * @package Kunstmaan\AdminBundle\Twig
+ */
+class GoogleSignInTwigExtension extends Twig_Extension
+{
+    private $enabled;
+    private $clientId;
+
+    public function __construct($enabled, $clientId)
+    {
+        $this->enabled = $enabled;
+        $this->clientId = $clientId;
+    }
+
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('google_signin_enabled', array($this, 'isGoogleSignInEnabled')),
+            new \Twig_SimpleFunction('google_signin_client_id', array($this, 'getClientId')),
+        );
+    }
+
+    public function isGoogleSignInEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClientId()
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'google_signin_twig_extension';
+    }
+
+}
