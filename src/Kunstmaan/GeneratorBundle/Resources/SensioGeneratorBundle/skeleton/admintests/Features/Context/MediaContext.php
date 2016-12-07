c6130b86f93dfcb19a5fe4762c871840ec35865f
Sander Goossens <sander@kodeus.be>
2016-12-07 08:02:52 +0100

    [GeneratorBundle]: fix media admin test

--- src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/admintests/Features/Context/MediaContext.php
+++ src/Kunstmaan/GeneratorBundle/Resources/SensioGeneratorBundle/skeleton/admintests/Features/Context/MediaContext.php
@@ -228,6 +228,22 @@ class MediaContext extends BehatContext
     private function performDelete()
     {
         $page = $this->getMainContext()->getSession()->getPage();
-        $this->getMainContext()->findAndClickButton($page, 'xpath', "//a[contains(@class, 'del')]");
+        $this->getMainContext()->findAndClickButton($page, 'xpath', "//button[contains(@class, 'del')]");
+
+        $modals = $page->findAll('xpath', "//div[contains(@id, 'sure-modal')]");
+
+        //Wait 1 second for the modal to be visible
+        //Else we can get a error when running the tests.
+        $this->getMainContext()->iWaitSeconds(1);
+
+        // Find the visible modal.
+        // Couldn't do this via xpath using : [contains(@class, 'modal') and contains(@class, 'in')]
+        foreach ($modals as $modal) {
+            if ($modal->hasClass('in')) {
+                $this->getMainContext()->findAndClickButton($page, 'xpath', "//a[contains(@href, 'del')]");
+
+                return;
+            }
+        }
     }
 }
