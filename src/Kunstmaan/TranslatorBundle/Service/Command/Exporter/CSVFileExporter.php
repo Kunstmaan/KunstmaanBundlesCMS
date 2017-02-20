1a71e44d883c89379d4ce5bc9b88e21600bc1414
Sander Goossens <sander@kodeus.be>
2017-02-20 15:01:26 +0100

    [TranslatorBundle]: add csv export for translations
    Make csv file exporter class variable

---
+++ src/Kunstmaan/TranslatorBundle/Service/Command/Exporter/CSVFileExporter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Kunstmaan\TranslatorBundle\Service\Command\Exporter;
+
+use Kunstmaan\TranslatorBundle\Entity\Translation;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Export into csv format
+ */
+class CSVFileExporter implements FileExporterInterface
+{
+    /** @var array */
+    private $locales;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function export(array $translations)
+    {
+        $handle = fopen('php://temp', 'r+');
+
+        // Add the header of the CSV file
+        $headers = ['Keyword'];
+
+        // Get all possible locales and add them as header.
+        $headers = array_merge($headers, $this->locales);
+
+        fputcsv($handle, $headers);
+
+        // Add all translation to the CSV.
+        foreach ($translations as $key => $translation) {
+            $row = [$key];
+
+            /** @var Translation $item */
+            foreach ($this->locales as $locale) {
+                $locale = preg_replace_callback('/\_([a-z]+)/', function ($match) {
+                    return '_' . strtoupper($match[1]);
+                }, $locale);
+
+                if (isset($translation[$locale])) {
+                    $item = $translation[$locale];
+                    $row[] = utf8_decode($item->getText());
+                } else {
+                    $row[] = '';
+                }
+            }
+
+            fputcsv($handle, $row);
+        }
+
+        rewind($handle);
+
+        $response = new Response(stream_get_contents($handle));
+        fclose($handle);
+        $response->setStatusCode(200);
+        $response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export_translations.csv"');
+
+        return $response;
+    }
+
+    public function setLocales($locales)
+    {
+        $this->locales = $locales;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($format)
+    {
+        return 'csv' === $format;
+    }
+}
