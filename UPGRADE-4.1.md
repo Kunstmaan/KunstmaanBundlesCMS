1cdb981403203ff49b5692b995e9337d080175b4
Sander Goossens <sander@kodeus.be>
2017-03-30 16:58:36 +0200

    [RedirectBundle]: add note to redirect
    Add upgrade 4.1

---
+++ UPGRADE-4.1.md
@@ -0,0 +1,9 @@
+# UPGRADE FROM 4.0 to 4.1
+
+## RedirectBundle
+
+Added note to the RedirectBundle:
+
+https://github.com/Kunstmaan/KunstmaanBundlesCMS/pull/1490
+
+Please be sure to execute database migrations after upgrading.
--- src/Kunstmaan/RedirectBundle/AdminList/RedirectAdminListConfigurator.php
+++ src/Kunstmaan/RedirectBundle/AdminList/RedirectAdminListConfigurator.php
@@ -41,6 +41,7 @@ class RedirectAdminListConfigurator extends AbstractDoctrineORMAdminListConfigur
         $this->addField('origin', 'redirect.adminlist.header.origin', true);
         $this->addField('target', 'redirect.adminlist.header.target', true);
         $this->addField('permanent', 'redirect.adminlist.header.permanent', true);
+        $this->addField('note', 'redirect.adminlist.header.note', true);
     }
 
     /**
@@ -57,6 +58,7 @@ class RedirectAdminListConfigurator extends AbstractDoctrineORMAdminListConfigur
         $this->addFilter('origin', new ORM\StringFilterType('origin'), 'redirect.adminlist.filter.origin');
         $this->addFilter('target', new ORM\StringFilterType('target'), 'redirect.adminlist.filter.target');
         $this->addFilter('permanent', new ORM\BooleanFilterType('permanent'), 'redirect.adminlist.filter.permanent');
+        $this->addFilter('note', new ORM\StringFilterType('note'), 'redirect.adminlist.filter.note');
     }
 
     /**
