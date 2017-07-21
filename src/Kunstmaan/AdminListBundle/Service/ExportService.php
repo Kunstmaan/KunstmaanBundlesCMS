a606810a87ee70805530e2bbb179834000786780
Kevin Jossart <kevin.jossart@kunstmaan.be>
2017-07-21 23:29:15 +0200

    [AdminListBundle] refactor and modernize exportservice Replace legacy method of returning array with availabel outputs Remove template for rendering with csvs if box/spout can do it 1 method 1 way for each output refactoring legacy style coding
    workaround no fa icon for ods

--- src/Kunstmaan/AdminListBundle/Service/ExportService.php
+++ src/Kunstmaan/AdminListBundle/Service/ExportService.php
@@ -6,13 +6,32 @@ use Box\Spout\Common\Type;
 use Box\Spout\Writer\WriterFactory;
 use Box\Spout\Writer\WriterInterface;
 use Kunstmaan\AdminListBundle\AdminList\ExportableInterface;
+use Kunstmaan\AdminListBundle\AdminList\Field;
+use Kunstmaan\AdminListBundle\EventSubscriber\ExportException;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Translation\Translator;
 
+/**
+ * class ExportService
+ */
 class ExportService
 {
+    const SUPPORTED_EXTENSIONS = [
+        'Csv' => Type::CSV,
+        'Ods' => Type::ODS,
+        'Excel' => Type::XLSX
+    ];
+
+    /**
+     * @return array
+     */
+    public static function getSupportedExtensions()
+    {
+        return self::SUPPORTED_EXTENSIONS;
+    }
+
     /**
      * @var EngineInterface
      */
@@ -23,101 +42,45 @@ class ExportService
      */
     private $translator;
 
-    const EXT_CSV   = 'csv';
-    const EXT_EXCEL = 'xlsx';
-
-    /**
-     * @return array
-     */
-    public static function getSupportedExtensions()
-    {
-        $rfl  = new \ReflectionClass(new self());
-        $data = $rfl->getConstants();
-
-        $extensions = array();
-        foreach ($data as $name => $ext) {
-            if (strpos($name, 'EXT_') !== false) {
-                $key              = ucfirst(strtolower(str_replace('EXT_', '', $name)));
-                $extensions[$key] = $ext;
-            }
-        }
-
-        return $extensions;
-    }
-
     /**
      * @param ExportableInterface $adminList
-     * @param string              $_format
-     * @param string|null         $template
+     * @param string $format
      *
-     * @return Response|StreamedResponse
+     * @return StreamedResponse
      *
-     * @throws \Exception
+     * @throws ExportException
      */
-    public function getDownloadableResponse(ExportableInterface $adminList, $_format, $template = null)
+    public function getDownloadableResponse(ExportableInterface $adminList, $format)
     {
-        switch ($_format) {
-            case self::EXT_EXCEL:
-                $response = $this->streamExcelSheet($adminList);
-                break;
-            default:
-                $content  = $this->createFromTemplate($adminList, $_format, $template);
-                $response = $this->createResponse($content, $_format);
-                $filename = sprintf('entries.%s', $_format);
-                $response->headers->set('Content-Disposition', sprintf('attachment; filename=%s', $filename));
-                break;
-        }
+        $response = $this->streamOutput($adminList, $format);
 
         return $response;
     }
 
     /**
      * @param ExportableInterface $adminList
-     * @param string              $_format
-     * @param string|null         $template
+     * @param string $format
      *
-     * @return string
+     * @return StreamedResponse
+     *
+     * @throws ExportException
      */
-    public function createFromTemplate(ExportableInterface $adminList, $_format, $template = null)
-    {
-        if ($template === null) {
-            $template = sprintf("KunstmaanAdminListBundle:Default:export.%s.twig", $_format);
-        }
-
-        $iterator = $adminList->getIterator();
-
-        return $this->renderer->render(
-            $template,
-            array(
-                'iterator'    => $iterator,
-                'adminlist'   => $adminList,
-                'queryparams' => array()
-            )
-        );
-    }
-
-    /**
-     * @param ExportableInterface $adminList
-     *
-     * @return Response
-     *
-     * @throws \Exception
-     */
-    public function streamExcelSheet(ExportableInterface $adminList)
+    protected function streamOutput(ExportableInterface $adminList, $format)
     {
         $response = new StreamedResponse();
-        $response->setCallback(function () use ($adminList) {
-            $writer = WriterFactory::create(Type::XLSX);
-            $writer->openToBrowser("export.xlsx");
+        $response->setCallback(function () use ($adminList, $format) {
+            $writer = WriterFactory::create($format);
+            $writer->openToBrowser("export.".$format);
 
-            $row = array();
+            $row = [];
+            /** @var Field $column */
             foreach ($adminList->getExportColumns() as $column) {
                 $row[] = $this->translator->trans($column->getHeader());
             }
             $writer->addRow($row);
 
             $iterator = $adminList->getIterator();
-            $rows = array();
+            $rows = [];
             foreach ($iterator as $item) {
                 if (array_key_exists(0, $item)) {
                     $itemObject = $item[0];
@@ -125,7 +88,8 @@ class ExportService
                     $itemObject = $item;
                 }
 
-                $row = array();
+                $row = [];
+                /** @var Field $column */
                 foreach ($adminList->getExportColumns() as $column) {
                     $columnName = $column->getName();
                     $itemHelper = $itemObject;
@@ -136,10 +100,10 @@ class ExportService
                     $data = $adminList->getStringValue($itemHelper, $columnName);
                     if (null !== $column->getTemplate()) {
                         if (!$this->renderer->exists($column->getTemplate())) {
-                            throw new \Exception('No export template defined for ' . get_class($data));
+                            throw new ExportException('No export template defined for ' . get_class($data), $data);
                         }
 
-                        $data = $this->renderer->render($column->getTemplate(), array('object' => $data));
+                        $data = $this->renderer->render($column->getTemplate(), ['object' => $data]);
                     }
 
                     $row[] = $data;
@@ -150,21 +114,6 @@ class ExportService
             $writer->addRows($rows);
             $writer->close();
         });
-
-        return $response;
-    }
-
-    /**
-     * @param string $content
-     * @param string $_format
-     *
-     * @return Response
-     */
-    public function createResponse($content, $_format)
-    {
-        $response = new Response();
-        $response->headers->set('Content-Type', sprintf('text/%s', $_format));
-        $response->setContent($content);
 
         return $response;
     }
