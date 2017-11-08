c80490bb3dcd3315383d8de9c83a8157df60c94a
Kajetan <cv65kr@gmail.com>
2017-11-09 00:23:23 +0100

    Namespace fix

--- src/Kunstmaan/AdminBundle/EventListener/ExceptionSubscriber.php
+++ src/Kunstmaan/AdminBundle/EventListener/ExceptionSubscriber.php
@@ -7,6 +7,7 @@ use Kunstmaan\AdminBundle\Entity\Exception;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionSubscriber implements EventSubscriberInterface
 {
