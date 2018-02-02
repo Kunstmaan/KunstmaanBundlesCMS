93c65066624b20c7ebc11a9926990cb1f96acaf6
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-02 15:12:41 +0100

    [FormBundle]  SingleLineTextPagePartAdminTypeTest

---
+++ tests/FormBundle/Form/SingleLineTextPagePartAdminTypeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Kunstmaan\FormBundle\Entity;
+
+use Kunstmaan\FormBundle\Entity\PageParts\SingleLineTextPagePart;
+use Kunstmaan\FormBundle\Form\SingleLineTextPagePartAdminType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+/**
+ * Class SingleLineTextPagePartAdminTypeTest
+ */
+class SingleLineTextPagePartAdminTypeTest extends TypeTestCase
+{
+    public function testFormType()
+    {
+        $formData = [
+            'required' => false,
+            'label' => 'type in this box!',
+            'errormessage_required' => 'fill in the form',
+            'regex' => '#\w+#',
+            'errormessage_regex' => 'oops',
+        ];
+
+        $form = $this->factory->create(SingleLineTextPagePartAdminType::class);
+
+        $object = new SingleLineTextPagePart();
+        $object->setRequired(false);
+        $object->setErrorMessageRequired('fill in the form');
+        $object->setLabel('type in this box!');
+        $object->setRegex('#\w+#');
+        $object->setErrorMessageRegex('oops');
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isValid());
+
+        $this->assertEquals($object, $form->getData());
+    }
+}
