11a449e69ba98022cd382c02b8ecf2727d693e47
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-02 12:44:11 +0100

    [FormBundle] EmailPagePartAdminTypeTest

---
+++ tests/FormBundle/Form/EmailPagePartAdminTypeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Kunstmaan\FormBundle\Entity;
+
+use Kunstmaan\FormBundle\Entity\PageParts\ChoicePagePart;
+use Kunstmaan\FormBundle\Entity\PageParts\EmailPagePart;
+use Kunstmaan\FormBundle\Form\ChoicePagePartAdminType;
+use Kunstmaan\FormBundle\Form\EmailPagePartAdminType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+/**
+ * Class EmailPagePartAdminTypeTest
+ */
+class EmailPagePartAdminTypeTest extends TypeTestCase
+{
+    public function testFormType()
+    {
+        $formData = [
+            'required' => false,
+            'errorMessageRequired' => 'required',
+            'errorMessageInvalid' => 'invalid',
+            'label' => 'label',
+        ];
+
+        $form = $this->factory->create(EmailPagePartAdminType::class);
+
+        $object = new EmailPagePart();
+        $object->setRequired(false);
+        $object->setErrorMessageRequired('required');
+        $object->setLabel('label');
+        $object->setErrorMessageInvalid('invalid');
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertTrue($form->isValid());
+
+        $this->assertEquals($object, $form->getData());
+    }
+}
