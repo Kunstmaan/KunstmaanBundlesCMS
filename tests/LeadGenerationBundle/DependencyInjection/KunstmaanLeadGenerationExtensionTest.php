32b181b6d347f9051efe98cd356d9c33266ad8db
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-08 13:08:41 +0100

    [LeadGenerationBundle] KunstmaanLeadGenerationExtensionTest

---
+++ tests/LeadGenerationBundle/DependencyInjection/KunstmaanLeadGenerationExtensionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Kunstmaan\LeadGenerationBundle\DependencyInjection;
+
+use Kunstmaan\LeadGenerationBundle\DependencyInjection\KunstmaanLeadGenerationExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Class KunstmaanLeadGenerationExtensionTest
+ */
+class KunstmaanLeadGenerationExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return [new KunstmaanLeadGenerationExtension()];
+    }
+
+
+    public function testCorrectParametersHaveBeenSet()
+    {
+
+        $this->load([
+            'popup_types' => ['abc'],
+            'debug' => true,
+         ]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_lead_generation.popup_types');
+        $this->assertContainerBuilderHasParameter('kunstmaan_lead_generation.debug', true);
+        $this->assertContainerBuilderHasParameter('kunstmaan_lead_generation.popup.twig.extension.class', 'Kunstmaan\LeadGenerationBundle\Twig\PopupTwigExtension');
+        $this->assertContainerBuilderHasParameter('kunstmaan_lead_generation.popup.manager.class', 'Kunstmaan\LeadGenerationBundle\Service\PopupManager');
+        $this->assertContainerBuilderHasParameter('kunstmaan_lead_generation.menu.adaptor.class', 'Kunstmaan\LeadGenerationBundle\Service\MenuAdaptor');
+    }
+}
