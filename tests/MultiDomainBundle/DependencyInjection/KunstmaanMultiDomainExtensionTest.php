54508c4862bf393097c08c6564561b0b4b828ed9
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-08 13:51:21 +0100

    [MultiDomainBundle] extension test

---
+++ tests/MultiDomainBundle/DependencyInjection/KunstmaanMultiDomainExtensionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Kunstmaan\MultiDomainBundle\DependencyInjection;
+
+use Kunstmaan\MultiDomainBundle\DependencyInjection\KunstmaanMultiDomainExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Class KunstmaanMultiDomainExtensionTest
+ */
+class KunstmaanMultiDomainExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return [new KunstmaanMultiDomainExtension()];
+    }
+
+
+    public function testCorrectParametersHaveBeenSet()
+    {
+
+        $this->load([
+            'hosts' => [
+                [
+                    'host' => 'cia.gov',
+                    'protocol' => 'https',
+                    'aliases' => ['cia.com'],
+                    'type' => 'single_lang',
+                    'root' => 'homepage',
+                    'default_locale' => 'nl',
+                    'locales' => [
+                        [
+                            'uri_locale' => '/nl',
+                            'locale' => 'nl',
+                            'extra' => 'huh?',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_multi_domain.hosts');
+        $this->assertContainerBuilderHasParameter('kunstmaan_multi_domain.router.class', 'Kunstmaan\MultiDomainBundle\Router\DomainBasedLocaleRouter');
+        $this->assertContainerBuilderHasParameter('kunstmaan_multi_domain.domain_configuration.class', 'Kunstmaan\MultiDomainBundle\Helper\DomainConfiguration');
+        $this->assertContainerBuilderHasParameter('kunstmaan_node.slugrouter.class', 'Kunstmaan\MultiDomainBundle\Router\DomainBasedLocaleRouter');
+        $this->assertContainerBuilderHasParameter('kunstmaan_admin.domain_configuration.class', 'Kunstmaan\MultiDomainBundle\Helper\DomainConfiguration');
+    }
+}
