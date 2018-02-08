9a56d51d1b1c39693c1fda1bc5aaa9186cd43fe6
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-08 12:45:31 +0100

    [DashboardBundle] DashboardExtension Test

---
+++ tests/DashboardBundle/DependencyInjection/KunstmaanDashboardExtensionTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Kunstmaan\DashboardBundle\DependencyInjection;
+
+use Kunstmaan\DashboardBundle\DependencyInjection\KunstmaanDashboardExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Class KunstmaanDashboardExtensionTest
+ */
+class KunstmaanDashboardExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return [new KunstmaanDashboardExtension()];
+    }
+
+
+    public function testCorrectParametersHaveBeenSet()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_dashboard.widget.googleanalytics.command');
+        $this->assertContainerBuilderHasParameter('kunstmaan_dashboard.widget.googleanalytics.controller');
+        $this->assertContainerBuilderHasParameter('kunstmaan_dashboard.googleclient.class');
+        $this->assertContainerBuilderHasParameter('kunstmaan_dashboard.googleclienthelper.class');
+        $this->assertContainerBuilderHasParameter('google.api.app_name');
+    }
+}
