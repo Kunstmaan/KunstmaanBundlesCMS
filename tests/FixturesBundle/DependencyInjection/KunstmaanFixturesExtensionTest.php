74b8074093ab0cef41bebf4f52a61fcd040b2b27
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-08 12:52:05 +0100

    [FixturesBundle] Fixtures Extension Test

---
+++ tests/FixturesBundle/DependencyInjection/KunstmaanFixturesExtensionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Kunstmaan\FixturesBundle\DependencyInjection;
+
+use Kunstmaan\FixturesBundle\DependencyInjection\KunstmaanFixturesExtension;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * Class KunstmaanFixturesExtensionTest
+ */
+class KunstmaanFixturesExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @return ExtensionInterface[]
+     */
+    protected function getContainerExtensions()
+    {
+        return [new KunstmaanFixturesExtension()];
+    }
+
+
+    public function testCorrectParametersHaveBeenSet()
+    {
+        $this->container->setParameter('empty_extension', true);
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('empty_extension', true);
+    }
+}
