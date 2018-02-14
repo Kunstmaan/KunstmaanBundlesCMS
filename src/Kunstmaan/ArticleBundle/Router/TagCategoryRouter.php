179d3ca9586b9e179450f4b9795e570f0dacc352
Sander <sander@kodeus.be>
2018-02-14 11:21:05 +0100

    [ArticleBundle]: deprecate container access
    [ArticleBundle]: deprecate direct container access

--- src/Kunstmaan/ArticleBundle/Router/TagCategoryRouter.php
+++ src/Kunstmaan/ArticleBundle/Router/TagCategoryRouter.php
@@ -2,73 +2,106 @@
 
 namespace Kunstmaan\ArticleBundle\Router;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Kunstmaan\NodeBundle\Router\SlugRouter;
-use Symfony\Component\Routing\Route;
+use Kunstmaan\TranslatorBundle\Service\Translator\Translator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Translation\TranslatorInterface;
-use Symfony\Component\VarDumper\VarDumper;
 
+/**
+ * Class TagCategoryRouter
+ *
+ * @package Kunstmaan\ArticleBundle\Router
+ */
 class TagCategoryRouter extends SlugRouter
 {
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(
+        /* DomainConfigurationInterface */ $domainConfiguration,
+        RequestStack $requestStack,
+        EntityManagerInterface $em,
+        $adminKey,
+        TranslatorInterface $translator
+    ) {
+
+        if ($domainConfiguration instanceof ContainerInterface) {
+            @trigger_error(
+                'Container injection is deprecated in KunstmaanNodeBundle 5.1 and will be removed in KunstmaanNodeBundle 6.0.',
+                E_USER_DEPRECATED
+            );
+
+            parent::__construct($domainConfiguration);
+            $this->translator = $domainConfiguration->get(Translator::class);
+
+            return;
+        }
+
+        parent::__construct($domainConfiguration, $requestStack, $em, $adminKey);
+        $this->translator = $translator;
+    }
+
     /**
-     * @return \Symfony\Component\Routing\RouteCollection
+     * @return RouteCollection
      */
     public function getRouteCollection()
     {
-        if (! is_null($this->routeCollection)) {
+        if (null !== $this->routeCollection) {
             return $this->routeCollection;
         }
+
+        // Create empty route collection.
         $this->routeCollection = new RouteCollection();
 
-        $extendParameters = array('category' => null, 'tag' => null);
+        $extendParameters = ['category' => null, 'tag' => null];
         $baseSlugParameters = array_merge($this->getSlugRouteParameters(), $extendParameters);
         $baseSlugPreviewParameters = array_merge($this->getPreviewRouteParameters(), $extendParameters);
 
-        /** @var TranslatorInterface $translator */
-        $translator = $this->container->get('translator');
-
         if ($this->isMultiLanguage()) {
             foreach ($this->getFrontendLocales() as $locale) {
-                $translator->setLocale($locale);
-                $categoryTrans = $translator->trans('article_overview_page.route.category');
-                $tagTrans = $translator->trans('article_overview_page.route.tag');
+                $this->translator->setLocale($locale);
+                $categoryTrans = $this->translator->trans('article_overview_page.route.category');
+                $tagTrans = $this->translator->trans('article_overview_page.route.tag');
 
-                $routePathParts = array(
+                $routePathParts = [
                     '_slug_category_tag' => sprintf('/%s/{category}/%s/{tag}', $categoryTrans, $tagTrans),
                     '_slug_tag' => sprintf('/%s/{tag}', $tagTrans),
                     '_slug_category' => sprintf('/%s/{category}', $categoryTrans),
-                );
+                ];
 
                 foreach ($routePathParts as $routeName => $routePart) {
-                    $slugParameters  = $baseSlugParameters;
-                    $slugParameters['path'] = '/{_locale}/{url}' . $routePart;
+                    $slugParameters = $baseSlugParameters;
+                    $slugParameters['path'] = '/{_locale}/{url}'.$routePart;
 
                     $slugPreviewParameters = $baseSlugPreviewParameters;
-                    $slugPreviewParameters['path'] = '/{_locale}/admin/preview/{url}' . $routePart;;
+                    $slugPreviewParameters['path'] = '/{_locale}/admin/preview/{url}'.$routePart;;
 
-                    $routeName .= '_' . $locale;
-                    $this->addRoute($routeName . '_preview', $slugPreviewParameters);
+                    $routeName .= '_'.$locale;
+                    $this->addRoute($routeName.'_preview', $slugPreviewParameters);
                     $this->addRoute($routeName, $slugParameters);
                 }
             }
         } else {
-            $categoryTrans = $translator->trans('article_overview_page.route.category');
-            $tagTrans = $translator->trans('article_overview_page.route.tag');
+            $categoryTrans = $this->translator->trans('article_overview_page.route.category');
+            $tagTrans = $this->translator->trans('article_overview_page.route.tag');
 
-            $slugParameters  = $baseSlugParameters;
+            $slugParameters = $baseSlugParameters;
             $slugPreviewParameters = $baseSlugPreviewParameters;
 
-            $routePathParts = array(
+            $routePathParts = [
                 '_slug_category_tag' => sprintf('/%s/{category}/%s/{tag}', $categoryTrans, $tagTrans),
                 '_slug_tag' => sprintf('/%s/{tag}', $tagTrans),
                 '_slug_category' => sprintf('/%s/{category}', $categoryTrans),
-            );
+            ];
 
             foreach ($routePathParts as $routeName => $routePart) {
-                $slugParameters['path'] = '/{url}' . $routePart;
-                $slugPreviewParameters['path'] = '/admin/preview/{url}' . $routePart;
+                $slugParameters['path'] = '/{url}'.$routePart;
+                $slugPreviewParameters['path'] = '/admin/preview/{url}'.$routePart;
 
-                $this->addRoute($routeName . '_preview', $slugPreviewParameters);
+                $this->addRoute($routeName.'_preview', $slugPreviewParameters);
                 $this->addRoute($routeName, $slugParameters);
             }
         }
