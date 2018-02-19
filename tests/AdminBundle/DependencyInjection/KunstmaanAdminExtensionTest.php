bbf0fa1ea907b7621d9f5121a1279f1a5b3b1e0f
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-19 12:38:56 +0100

    [AdminBundle] Compiler pass tests

--- tests/AdminBundle/DependencyInjection/KunstmaanAdminExtensionTest.php
+++ tests/AdminBundle/DependencyInjection/KunstmaanAdminExtensionTest.php
@@ -24,7 +24,16 @@ class KunstmaanAdminExtensionTest extends AbstractPrependableExtensionTestCase
     {
         $this->container->setParameter('kernel.logs_dir', '/somewhere/over/the/rainbow');
         $this->container->setParameter('kernel.environment', 'staging');
-        $this->load();
+        $this->load([
+            'dashboard_route' => true,
+            'admin_password' => 'omgchangethis',
+            'menu_items' => [
+                [
+                    'route' => 'route66',
+                    'label' => 'Route 66',
+                ],
+            ],
+        ]);
 
         $this->assertContainerBuilderHasParameter('version_checker.url', 'https://bundles.kunstmaan.be/version-check');
         $this->assertContainerBuilderHasParameter('version_checker.timeframe', (60 * 60 * 24));
