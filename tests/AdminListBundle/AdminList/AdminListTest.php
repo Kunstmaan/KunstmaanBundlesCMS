66ef211568f01b8b269805841e50bb80e2bdac4c
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-02-27 18:06:35 +0100

    [AdminListBundle] AdminList and configurator tests

--- tests/AdminListBundle/AdminList/AdminListTest.php
+++ tests/AdminListBundle/AdminList/AdminListTest.php
@@ -2,12 +2,20 @@
 namespace Tests\Kunstmaan\AdminListBundle\AdminList;
 
 use ArrayIterator;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\PersistentCollection;
 use Kunstmaan\AdminListBundle\AdminList\AdminList;
 use Kunstmaan\AdminListBundle\AdminList\Configurator\AbstractAdminListConfigurator;
 use Kunstmaan\AdminListBundle\AdminList\FilterBuilder;
 use Kunstmaan\MenuBundle\Entity\MenuItem;
 use Pagerfanta\Pagerfanta;
 use PHPUnit_Framework_TestCase;
+use stdClass;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ConcreteConfigurator extends AbstractAdminListConfigurator
 {
@@ -86,7 +94,7 @@ class ConcreteConfigurator extends AbstractAdminListConfigurator
      */
     public function getPagerfanta()
     {
-//        return new Pagerfanta();
+        return 'fake pagerfanta';
     }
 
     /**
@@ -95,6 +103,22 @@ class ConcreteConfigurator extends AbstractAdminListConfigurator
     public function getIterator()
     {
         return new ArrayIterator();
+    }
+}
+
+class PrivateObject
+{
+    protected $name = 'delboy1978uk';
+}
+
+class PublicObject extends PrivateObject
+{
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
     }
 }
 
@@ -118,6 +142,7 @@ class AdminListTest extends PHPUnit_Framework_TestCase
         $this->assertCount(0, $adminList->getColumns());
         $this->assertCount(0, $adminList->getExportColumns());
         $this->assertEquals(0, $adminList->getCount());
+        $this->assertEquals('fake pagerfanta', $adminList->getPagerfanta());
         $this->assertInstanceOf(ArrayIterator::class, $adminList->getIterator());
         $this->assertTrue($adminList->canAdd());
         $this->assertTrue($adminList->canEdit($item));
@@ -125,6 +150,7 @@ class AdminListTest extends PHPUnit_Framework_TestCase
         $this->assertFalse($adminList->canExport());
         $this->assertFalse($adminList->canView($item));
         $this->assertFalse($adminList->hasSort());
+        $this->assertFalse($adminList->hasSort('non-exitant-var-name'));
 
         $url = $adminList->getIndexUrl();
         $this->assertArrayHasKey('path', $url);
@@ -184,5 +210,85 @@ class AdminListTest extends PHPUnit_Framework_TestCase
         $itemActions = $adminList->getItemActions();
         $this->assertFalse($hasItemActions);
         $this->assertEmpty($itemActions);
+
+
+    }
+
+    public function testGetValue()
+    {
+        $config = new ConcreteConfigurator();
+        $adminList = new AdminList($config);
+
+        $em = $this->createMock(EntityManager::class);
+        $meta = $this->createMock(ClassMetadata::class);
+
+
+        $collection = new PersistentCollection($em, $meta, new ArrayCollection([
+            new PublicObject(),
+        ]));
+
+
+        $object = new stdClass();
+        $object->name = 'delboy1978uk';
+        $value = $adminList->getValue($object, 'name');
+        $this->assertEquals('delboy1978uk', $value);
+
+        $array = [
+            'name' => 'delboy1978uk',
+        ];
+        $value = $adminList->getValue($array, 'name');
+        $this->assertEquals('delboy1978uk', $value);
+        $this->assertEquals('', $adminList->getValue($array, 'missing'));
+
+        $private = new PrivateObject();
+        $this->assertEquals('undefined function [get/is/has]name()', $adminList->getValue($private, 'name'));
+
+        $date = new DateTime('2014-09-18 22:00:00');
+        $array = [
+            'key' => $date,
+            'array' => [
+                'random',
+                'strings'
+            ],
+            'bool' => true,
+            'string' => 'strings!',
+            'persistent' => $collection,
+        ];
+        $value = $adminList->getStringValue($array, 'key');
+        $this->assertEquals('2014-09-18 22:00:00', $value);
+        $this->assertEquals('random, strings', $adminList->getStringValue($array, 'array'));
+        $this->assertEquals('true', $adminList->getStringValue($array, 'bool'));
+        $this->assertEquals('strings!', $adminList->getStringValue($array, 'string'));
+        $this->assertEquals('delboy1978uk', $adminList->getStringValue($array, 'persistent'));
+
+        $collection = new PersistentCollection($em, $meta, new ArrayCollection([]));
+
+        $array = [
+            'persistent' => $collection,
+        ];
+        $this->assertEquals('', $adminList->getStringValue($array, 'persistent'));
+    }
+
+
+    public function testBindRequest()
+    {
+        $session = $this->createMock(Session::class);
+        $session->expects($this->any())->method('has')->willReturn(true);
+        $session->expects($this->any())->method('get')->willReturn([
+            'page' => 1,
+            'orderBy' => 'id',
+            'orderDirection' => 'ASC',
+        ]);
+        $request = new Request();
+        $request->setSession($session);
+        $request->query->add([
+            '_route' => 'some-route',
+        ]);
+        $config = new ConcreteConfigurator();
+        $adminList = new AdminList($config);
+        $adminList->bindRequest($request);
+
+        $this->assertEquals('id', $adminList->getOrderBy());
+        $this->assertEquals('ASC', $adminList->getOrderDirection());
     }
 }
