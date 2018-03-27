4ab3e4de5c4049634c652e02db8f971b26c62b8f
Derek Stephen McLean <delboy1978uk@gmail.com>
2018-03-27 15:30:30 +0200

    [AdminBundle] Testing helpers

--- tests/AdminBundle/Helper/Menu/MenuBuilderTest.php
+++ tests/AdminBundle/Helper/Menu/MenuBuilderTest.php
@@ -1,12 +1,15 @@
 <?php
 namespace Tests\Kunstmaan\AdminBundle\Helper\Menu;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuAdaptorInterface;
 use Kunstmaan\AdminBundle\Helper\Menu\MenuBuilder;
+use Kunstmaan\AdminBundle\Helper\Menu\ModulesMenuAdaptor;
 use Kunstmaan\AdminBundle\Helper\Menu\SettingsMenuAdaptor;
 use Kunstmaan\AdminBundle\Helper\Menu\SimpleMenuAdaptor;
 use Kunstmaan\AdminBundle\Helper\Menu\TopMenuItem;
 use Kunstmaan\MenuBundle\Entity\MenuItem;
+use Kunstmaan\AdminBundle\Helper\Menu\MenuItem as MenuItemHelper;
 use ReflectionClass;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -125,5 +128,91 @@ class MenuBuilderTest extends \PHPUnit_Framework_TestCase
 
         $lowest = $this->object->getLowestTopChild();
         $this->assertNull($lowest);
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function testAdaptChildren()
+    {
+        $this->authCheck->expects($this->any())->method('isGranted')->willReturn(true);
+
+        $parent = $this->createMock(MenuItemHelper::class);
+        $parent->expects($this->once())->method('getRoute')->willReturn('KunstmaanAdminBundle_settings');
+
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->any())->method('getParameter')->willReturn(true);
+        $container->expects($this->any())->method('get')->willReturn($this->authCheck);
+
+        $this->container = $container;
+        $this->object = new MenuBuilder($container);
+
+        $adapter = new SettingsMenuAdaptor($this->container);
+        $array = [];
+        $adapter->adaptChildren($this->object, $array, $parent, new Request([],[],['_route' => 'KunstmaanAdminBundle_settings_bundle_version']));
+    }
+
+
+    public function testModulesMenuAdaptor()
+    {
+        $this->stack->expects($this->atLeastOnce())
+            ->method('getCurrentRequest')
+            ->willReturn(new Request([],[],['_route' => 'KunstmaanAdminBundle_modules']));
+
+        $this->authCheck->expects($this->any())->method('isGranted')->willReturn(true);
+        $adapter = new ModulesMenuAdaptor();
+        $this->object->addAdaptMenu($adapter);
+        $current = $this->object->getCurrent();
+        $this->assertInstanceOf(TopMenuItem::class, $current);
+    }
+
+    /**
+     * @throws \ReflectionException
+     */
+    public function testSimpleMenuAdaptor()
+    {
+        $this->stack->expects($this->atLeastOnce())
+            ->method('getCurrentRequest')
+            ->willReturn(new Request([],[],['_route' => 'KunstmaanAdminBundle_modules']));
+
+        $this->authCheck->expects($this->any())->method('isGranted')->willReturn(true);
+        $adapter = new SimpleMenuAdaptor($this->authCheck, [[
+            'label' => 'label',
+            'parent' => null,
+            'role' => 'ADMIN',
+            'route' => 'KunstmaanAdminBundle_modules',
+            'params' => ['x' => 'y'],
+        ]]);
+        $this->object->addAdaptMenu($adapter);
+
+        $mirror = new ReflectionClass(MenuBuilder::class);
+        $property = $mirror->getProperty('topMenuItems');
+        $property->setAccessible(true);
+
+        $helper = new MenuItemHelper($this->object);
+        $helper->setActive(true);
+        $property->setValue($this->object, [$helper]);
+
+        $current = $this->object->getCurrent();
+        $this->assertInstanceOf(TopMenuItem::class, $current);
+
+        $mirror = new ReflectionClass(SimpleMenuAdaptor::class);
+        $method = $mirror->getMethod('parentMatches');
+        $method->setAccessible(true);
+        $bool = $method->invoke($adapter, null, ['parent' => null]);
+        $this->assertTrue($bool);
+
+        $authCheck = $this->createMock(AuthorizationCheckerInterface::class);
+        $authCheck->expects($this->any())->method('isGranted')->willReturn(false);
+        $adapter = new SimpleMenuAdaptor($authCheck, [[
+            'label' => 'label',
+            'parent' => null,
+            'role' => 'ADMIN',
+            'route' => 'KunstmaanAdminBundle_modules',
+            'params' => ['x' => 'y'],
+        ]]);
+        $array = [];
+        $adapter->adaptChildren($this->object, $array, null, new Request());
+        $this->assertEmpty($array);
     }
 }
