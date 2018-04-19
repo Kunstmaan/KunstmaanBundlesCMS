478cba95562c63c614d518167a990638f2364fa8
Danny van Wijk <danny@insiders.nl>
2018-04-19 13:56:38 +0000

    KUN-1082 Change menu item required

--- src/Kunstmaan/MenuBundle/Form/MenuItemAdminType.php
+++ src/Kunstmaan/MenuBundle/Form/MenuItemAdminType.php
@@ -135,7 +135,8 @@ class MenuItemAdminType extends AbstractType
             'newWindow',
             CheckboxType::class,
             array(
-                'label' => 'kuma_menu.form.new_window'
+                'label' => 'kuma_menu.form.new_window',
+                'required' => false,
             )
         );
     }
