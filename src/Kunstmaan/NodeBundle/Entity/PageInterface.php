e65008837d4d8d3b8fc6ad3e72d72dd3b7b794b4
Jeroen Thora <jeroenthora@gmail.com>
2018-05-21 22:06:54 +0200

    [NodeBundle] Remove incorrect deprecation

--- src/Kunstmaan/NodeBundle/Entity/PageInterface.php
+++ src/Kunstmaan/NodeBundle/Entity/PageInterface.php
@@ -19,8 +19,6 @@ interface PageInterface extends HasNodeInterface
      * @param Request            $request   The Request
      * @param RenderContext      $context   The Render context
      *
-     * @deprecated Using the service action is deprecated in KunstmaanNodeBundle 5.1 and will be removed in KunstmaanNodeBundle 6.0. Use the SlugActionInterface instead.
-     *
      * @return void|RedirectResponse
      */
     public function service(ContainerInterface $container, Request $request, RenderContext $context);
