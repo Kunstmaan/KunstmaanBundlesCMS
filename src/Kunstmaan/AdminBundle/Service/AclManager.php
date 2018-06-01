b6aba1a3ece0f9cb718e7d5181145a246c749435
Peter Zentjens <peter.zentjens@kunstmaan.be>
2018-06-01 11:02:18 +0200

    [AdminBundle] updated aclUpdateCommand to use AclManager

--- src/Kunstmaan/AdminBundle/Service/AclManager.php
+++ src/Kunstmaan/AdminBundle/Service/AclManager.php
@@ -5,6 +5,7 @@ namespace Kunstmaan\AdminBundle\Service;
 use Kunstmaan\NodeBundle\Entity\Node;
 use Symfony\Component\Security\Acl\Model\MutableAclProviderInterface;
 use Symfony\Component\Security\Acl\Model\ObjectIdentityRetrievalStrategyInterface;
+use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
 
 /**
  * Class AclManager
@@ -16,12 +17,12 @@ class AclManager
     private $aclProvider;
 
     /** @var ObjectIdentityRetrievalStrategyInterface */
-    private $strategy;
+    private $objectIdentityRetrievalStrategy;
 
-    public function __construct(MutableAclProviderInterface $aclProvider, ObjectIdentityRetrievalStrategyInterface $strategy)
+    public function __construct(MutableAclProviderInterface $aclProvider, ObjectIdentityRetrievalStrategyInterface $objectIdentityRetrievalStrategy)
     {
         $this->aclProvider = $aclProvider;
-        $this->strategy = $strategy;
+        $this->objectIdentityRetrievalStrategy = $objectIdentityRetrievalStrategy;
     }
 
     /**
@@ -30,10 +31,10 @@ class AclManager
      */
     public function updateNodeAcl(Node $originalNode, Node $nodeNewPage)
     {
-        $originalIdentity = $this->strategy->getObjectIdentity($originalNode);
+        $originalIdentity = $this->objectIdentityRetrievalStrategy->getObjectIdentity($originalNode);
         $originalAcl = $this->aclProvider->findAcl($originalIdentity);
 
-        $newIdentity = $this->strategy->getObjectIdentity($nodeNewPage);
+        $newIdentity = $this->objectIdentityRetrievalStrategy->getObjectIdentity($nodeNewPage);
         $newAcl = $this->aclProvider->createAcl($newIdentity);
 
         $aces = $originalAcl->getObjectAces();
@@ -45,5 +46,32 @@ class AclManager
             }
         }
         $this->aclProvider->updateAcl($newAcl);
+
+    }
+
+    /**
+     * @param array     $nodes
+     * @param string    $role
+     * @param int       $mask
+     */
+    public function updateNodesAclToRole(array $nodes, $role, $mask)
+    {
+        foreach ($nodes as $node) {
+            $objectIdentity = $this->objectIdentityRetrievalStrategy->getObjectIdentity($node);
+
+            /** @var Acl $acl */
+            $acl = $this->aclProvider->findAcl($objectIdentity);
+            $securityIdentity = new RoleSecurityIdentity($role);
+
+            /** @var Entry $ace */
+            foreach ($acl->getObjectAces() as $index => $ace) {
+                if (!$ace->getSecurityIdentity()->equals($securityIdentity)) {
+                    continue;
+                }
+                $acl->updateObjectAce($index, $mask);
+                break;
+            }
+            $this->aclProvider->updateAcl($acl);
+        }
     }
 }
