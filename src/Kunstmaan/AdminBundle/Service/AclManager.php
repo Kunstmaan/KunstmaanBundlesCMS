6989bbc32564b5c3720bbab2d5393033c4c0ce39
Jeroen Thora <jeroenthora@gmail.com>
2018-06-08 15:09:37 +0200

    Add missing use statements to fix broken acl setup

--- src/Kunstmaan/AdminBundle/Service/AclManager.php
+++ src/Kunstmaan/AdminBundle/Service/AclManager.php
@@ -3,6 +3,8 @@
 namespace Kunstmaan\AdminBundle\Service;
 
 use Kunstmaan\NodeBundle\Entity\Node;
+use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
+use Symfony\Component\Security\Acl\Model\EntryInterface;
 use Symfony\Component\Security\Acl\Model\MutableAclProviderInterface;
 use Symfony\Component\Security\Acl\Model\ObjectIdentityRetrievalStrategyInterface;
 use Symfony\Component\Security\Acl\Domain\RoleSecurityIdentity;
