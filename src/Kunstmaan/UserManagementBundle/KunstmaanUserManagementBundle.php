077cd2274890634f095bca83ac67d67b4da1e724
Peter Zentjens <26066867+deZinc@users.noreply.github.com>
2018-06-08 13:37:31 +0200

    [AdminBundle][UserManagementBundle] marked service as public using compilerpass (#1997)

--- src/Kunstmaan/UserManagementBundle/KunstmaanUserManagementBundle.php
+++ src/Kunstmaan/UserManagementBundle/KunstmaanUserManagementBundle.php
@@ -2,8 +2,17 @@
 
 namespace Kunstmaan\UserManagementBundle;
 
+use Kunstmaan\UserManagementBundle\DependencyInjection\Compiler\FixUserManagerPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KunstmaanUserManagementBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new FixUserManagerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -1);
+    }
 }
