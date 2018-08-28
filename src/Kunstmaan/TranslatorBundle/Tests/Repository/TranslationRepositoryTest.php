269fb48955a18858c9d8ae25b0187e5c57f33400
Numkil <kevin.jossart@hotmail.com>
2018-08-28 16:31:47 +0200

    [TranslatorBundle] add status field on translations for later use in rest api (#2026)
    * [TranslatorBundle] add status field on translations for later use in rest api
    
    * Remove unused variable

--- src/Kunstmaan/TranslatorBundle/Tests/Repository/TranslationRepositoryTest.php
+++ src/Kunstmaan/TranslatorBundle/Tests/Repository/TranslationRepositoryTest.php
@@ -1,11 +1,12 @@
 <?php
 namespace Kunstmaan\TranslatorBundle\Tests\Service\Importer;
 
+use Kunstmaan\TranslatorBundle\Repository\TranslationRepository;
 use Kunstmaan\TranslatorBundle\Tests\BaseTestCase;
 
 class TranslationRepositoryTest extends BaseTestCase
 {
-
+    /** @var TranslationRepository */
     private $translationRepository;
 
     public function setUp()
@@ -40,6 +41,26 @@ class TranslationRepositoryTest extends BaseTestCase
     public function testGetTranslationsByLocalesAndDomains()
     {
         $result = $this->translationRepository->getTranslationsByLocalesAndDomains(array('nl'), array('messages'));
+        $this->assertInstanceOf('Kunstmaan\TranslatorBundle\Entity\Translation', $result[0]);
+        $this->assertGreaterThan(0, count($result));
+    }
+
+    /**
+     * @group translation-repository
+     */
+    public function testFindAllNotDisabled()
+    {
+        $result = $this->translationRepository->findAllNotDisabled('nl');
+        $this->assertInstanceOf('Kunstmaan\TranslatorBundle\Entity\Translation', $result[0]);
+        $this->assertGreaterThan(0, count($result));
+    }
+
+    /**
+     * @group translation-repository
+     */
+    public function testFindAllDeprecated()
+    {
+        $result = $this->translationRepository->findDeprecatedTranslationsBeforeDate(new \DateTime(), 'messages');
         $this->assertInstanceOf('Kunstmaan\TranslatorBundle\Entity\Translation', $result[0]);
         $this->assertGreaterThan(0, count($result));
     }
