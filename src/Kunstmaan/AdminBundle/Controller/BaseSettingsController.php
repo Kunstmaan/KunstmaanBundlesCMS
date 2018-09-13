90652a05065c9c89dbd6deeb02be576ecf0109b6
Jeroen Thora <jeroenthora@gmail.com>
2018-09-13 15:11:32 +0200

    Revert commit c49d8a3 from PR #2097 (#2123)

--- src/Kunstmaan/AdminBundle/Controller/BaseSettingsController.php
+++ src/Kunstmaan/AdminBundle/Controller/BaseSettingsController.php
@@ -3,16 +3,30 @@
 namespace Kunstmaan\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpKernel\Kernel;
 
-if (Kernel::VERSION_ID < 40000) {
-    class_alias('Kunstmaan\AdminBundle\Controller\Symfony3BaseSettingsController', 'Kunstmaan\AdminBundle\Controller\BaseSettingsController');
-} else {
-    class_alias('Kunstmaan\AdminBundle\Controller\Symfony4BaseSettingsController', 'Kunstmaan\AdminBundle\Controller\BaseSettingsController');
-}
-
-if (false) {
-    class BaseSettingsController extends Controller
+class BaseSettingsController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated
+     */
+    protected function get($id)
     {
+        @trigger_error('Getting services directly from the container is deprecated in KunstmaanAdminBundle 5.1 and will be removed in KunstmaanAdminBundle 6.0. Register your controllers as services and inject the necessary dependencies.', E_USER_DEPRECATED);
+
+        return parent::get($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated
+     */
+    protected function getParameter($name)
+    {
+        @trigger_error('Getting parameters directly from the container is deprecated in KunstmaanAdminBundle 5.1 and will be removed in KunstmaanAdminBundle 6.0. Register your controllers as services and inject the necessary parameters.', E_USER_DEPRECATED);
+
+        return parent::getParameter($name);
     }
 }
--- src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
+++ src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
@@ -26,8 +26,33 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 /**
  * AdminListController
  */
-abstract class AdminListController extends ContainerDeprecationController
+abstract class AdminListController extends Controller
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated
+     */
+    protected function get($id)
+    {
+        @trigger_error('Getting services directly from the container is deprecated in KunstmaanAdminListBundle 5.1 and will be removed in KunstmaanAdminListBundle 6.0. Register your controllers as services and inject the necessary dependencies.', E_USER_DEPRECATED);
+
+        return parent::get($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @deprecated
+     */
+    protected function getParameter($name)
+    {
+        @trigger_error('Getting parameters directly from the container is deprecated in KunstmaanAdminListBundle 5.1 and will be removed in KunstmaanAdminListBundle 6.0. Register your controllers as services and inject the necessary parameters.', E_USER_DEPRECATED);
+
+        return parent::getParameter($name);
+    }
+
+
     /**
      * You can override this method to return the correct entity manager when using multiple databases ...
      *
