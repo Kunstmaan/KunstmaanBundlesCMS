04c7f770ac8b82ba7423333c979e733acb5813d7
Kevin Jossart <kevin.jossart@kunstmaan.be>
2018-09-24 14:40:47 +0200

    [VotingBundle] [Test] fix namespace tests

--- src/Kunstmaan/VotingBundle/Tests/_support/Helper/Unit.php
+++ src/Kunstmaan/VotingBundle/Tests/_support/Helper/Unit.php
@@ -1,5 +1,5 @@
 <?php
-namespace VotingBundle\Helper;
+namespace Kunstmaan\VotingBundle\Tests\Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
