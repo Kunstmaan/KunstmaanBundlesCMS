6b0f1be82ac5115e408d5c722992f022ff8d9f6f
Jeroen Thora <jeroenthora@gmail.com>
2018-10-31 11:38:22 +0100

    Revert code changes for container access deprecation to make symfony 4 upgrade easier (#2167)

--- src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
+++ src/Kunstmaan/AdminListBundle/Controller/AdminListController.php
@@ -29,31 +29,6 @@ use Symfony\Component\PropertyAccess\PropertyAccess;
 abstract class AdminListController extends Controller
 {
     /**
-     * {@inheritdoc}
-     *
-     * @deprecated
-     */
-    protected function get($id)
-    {
-        @trigger_error('Getting services directly from the container is deprecated in KunstmaanAdminListBundle 5.1 and will be removed in KunstmaanAdminListBundle 6.0. Register your controllers as services and inject the necessary dependencies.', E_USER_DEPRECATED);
-
-        return parent::get($id);
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @deprecated
-     */
-    protected function getParameter($name)
-    {
-        @trigger_error('Getting parameters directly from the container is deprecated in KunstmaanAdminListBundle 5.1 and will be removed in KunstmaanAdminListBundle 6.0. Register your controllers as services and inject the necessary parameters.', E_USER_DEPRECATED);
-
-        return parent::getParameter($name);
-    }
-
-
-    /**
      * You can override this method to return the correct entity manager when using multiple databases ...
      *
      * @return \Doctrine\Common\Persistence\ObjectManager|object
