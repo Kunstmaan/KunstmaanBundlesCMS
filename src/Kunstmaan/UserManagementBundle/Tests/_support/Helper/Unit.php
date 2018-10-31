7270ed2e7ae2c98dc548fbd1375df73475a08a3d
Numkil <kevin.jossart@hotmail.com>
2018-10-31 12:01:23 +0100

    [UsermanagementBundle] [Tests] fix namespace tests (#2145)

--- src/Kunstmaan/UserManagementBundle/Tests/_support/Helper/Unit.php
+++ src/Kunstmaan/UserManagementBundle/Tests/_support/Helper/Unit.php
@@ -1,5 +1,5 @@
 <?php
-namespace UserManagementBundle\Helper;
+namespace Kunstmaan\UserManagementBundle\Tests\Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
