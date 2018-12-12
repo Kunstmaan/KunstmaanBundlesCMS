1e9322278426d97611cb8bcfbe2fd30ef3b3dc4f
Jeroen Thora <jeroenthora@gmail.com>
2018-12-12 13:22:18 +0100

    [UtilitiesBundle] Deprecate direct set of parameter and use kernel.secret by default

--- src/Kunstmaan/UtilitiesBundle/DependencyInjection/KunstmaanUtilitiesExtension.php
+++ src/Kunstmaan/UtilitiesBundle/DependencyInjection/KunstmaanUtilitiesExtension.php
@@ -22,10 +22,12 @@ class KunstmaanUtilitiesExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        if (isset($config['cipher'], $config['cipher']['secret'])) {
-            $container->setParameter('kunstmaan_utilities.cipher.secret', $config['cipher']['secret']);
-        } elseif (!$container->hasParameter('kunstmaan_utilities.cipher.secret')) {
+        if ($container->hasParameter('kunstmaan_utilities.cipher.secret')) {
+            @trigger_error('Setting the "kunstmaan_utilities.cipher.secret" parameter is deprecated since KunstmaanUtilitiesBundle 5.2, this value will be ignored/overwritten in KunstmaanUtilitiesBundle 6.0. Use the "kunstmaan_utilities.cipher.secret" config instead if you want to set a different value than the default "%kernel.secret%".', E_USER_DEPRECATED);
+        } elseif ($container->hasParameter('secret')) {
             $container->setParameter('kunstmaan_utilities.cipher.secret', $container->getParameter('secret'));
+        } else {
+            $container->setParameter('kunstmaan_utilities.cipher.secret', $config['cipher']['secret']);
         }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
