e0f6a4afd385186b8d631c004accc657b7d174e6
JZuidema <37873948+JZuidema@users.noreply.github.com>
2018-12-17 08:00:10 +0100

    [NodeBundle] Add tabs (#2044)
    * Fixed active menu item when having more than 1 configuration entity
    
    * Added tab bundle to node bundle
    
    * Added test
    
    * Renamed test
    
    * Didnt need all the classes
    
    * Removed unused classes
    
    * Text files should end with a newline character
    
    * Added test form type for tabs
    Removed setters in value object
    Added entity tab listener
    
    * Added description of tab functionality
    
    * Entity tabs example
    
    * Documentation
    
    * Fixed display in readme
    
    * Removed test from NodeBundle
    
    * Added tests again

---
+++ src/Kunstmaan/NodeBundle/Entity/PageTabInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kunstmaan\NodeBundle\Entity;
+
+use Kunstmaan\NodeBundle\ValueObject\PageTab;
+
+interface PageTabInterface {
+	/**
+	 * @return PageTab[]
+	 */
+	public function getTabs();
+}
