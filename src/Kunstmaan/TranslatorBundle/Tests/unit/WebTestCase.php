c96fe311c6803819c4e76af533973ab7697598ca
Peter Zentjens <peter.zentjens@kunstmaan.be>
2019-01-29 12:54:03 +0100

    [TranslatorBundle] setup the WebTestCase for new tests with app

---
+++ src/Kunstmaan/TranslatorBundle/Tests/unit/WebTestCase.php
@@ -0,0 +1,63 @@
+<?php
+namespace Kunstmaan\TranslatorBundle\Tests\unit;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+class WebTestCase extends BaseWebTestCase
+{
+    public static function assertRedirect($response, $location)
+    {
+        self::assertTrue($response->isRedirect(), 'Response is not a redirect, got status code: '.$response->getStatusCode());
+        self::assertEquals('http://localhost'.$location, $response->headers->get('Location'));
+    }
+
+    public static function setUpBeforeClass()
+    {
+        static::deleteTmpDir();
+    }
+
+    public static function tearDownAfterClass()
+    {
+        static::deleteTmpDir();
+    }
+
+    protected static function deleteTmpDir()
+    {
+        if (!file_exists($dir = sys_get_temp_dir().'/'.static::getVarDir())) {
+            return;
+        }
+
+        $fs = new Filesystem();
+        $fs->remove($dir);
+    }
+
+    protected static function getKernelClass()
+    {
+        require_once __DIR__.'/app/AppKernel.php';
+
+        return 'Symfony\Bundle\FrameworkBundle\Tests\Functional\app\AppKernel';
+    }
+
+    protected static function createKernel(array $options = [])
+    {
+        $class = self::getKernelClass();
+
+        if (!isset($options['test_case'])) {
+            throw new \InvalidArgumentException('The option "test_case" must be set.');
+        }
+
+        return new $class(
+            static::getVarDir(),
+            $options['test_case'],
+            isset($options['root_config']) ? $options['root_config'] : 'config.yml',
+            isset($options['environment']) ? $options['environment'] : strtolower(static::getVarDir().$options['test_case']),
+            isset($options['debug']) ? $options['debug'] : true
+        );
+    }
+
+    protected static function getVarDir()
+    {
+        return 'FB'.substr(strrchr(\get_called_class(), '\\'), 1);
+    }
+}
