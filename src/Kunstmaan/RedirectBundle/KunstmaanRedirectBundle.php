df4e30d69bf596488be262e5d9eb7bc4bd6c7f68
Jeroen Thora <jeroenthora@gmail.com>
2019-01-30 16:26:55 +0100

    [RedirectBundle] Deprecate service class parameters

--- src/Kunstmaan/RedirectBundle/KunstmaanRedirectBundle.php
+++ src/Kunstmaan/RedirectBundle/KunstmaanRedirectBundle.php
@@ -2,8 +2,14 @@
 
 namespace Kunstmaan\RedirectBundle;
 
+use Kunstmaan\RedirectBundle\DependencyInjection\Compiler\DeprecateClassParametersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KunstmaanRedirectBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DeprecateClassParametersPass());
+    }
 }
