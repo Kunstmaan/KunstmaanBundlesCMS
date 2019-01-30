506efe934cf3ae9fb7fc9287ee0283b627d5bfd8
Peter Zentjens <26066867+deZinc@users.noreply.github.com>
2019-01-30 12:35:23 +0100

    [AdminBundle] update composer file of the admin-bundle (#2271)
    * [AdminBundle] updated some more dependencies
    
    * [AdminBundle] updated the deve dependencies
    
    * [AdminBundle] increased version suggested user management bundle
    
    * [AdminBundle] fixed ConsoleExceptionListener test
    
    * [AdminBundlstyleci fix
    
    * [AdminBundle] corrected version id for sf4
    
    Co-Authored-By: deZinc <26066867+deZinc@users.noreply.github.com>

--- src/Kunstmaan/AdminBundle/Tests/unit/EventListener/ConsoleExceptionListenerTest.php
+++ src/Kunstmaan/AdminBundle/Tests/unit/EventListener/ConsoleExceptionListenerTest.php
@@ -7,29 +7,32 @@ use Kunstmaan\AdminBundle\Command\ApplyAclCommand;
 use Kunstmaan\AdminBundle\EventListener\ConsoleExceptionListener;
 use PHPUnit_Framework_TestCase;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Console\Event\ConsoleErrorEvent;
 use Symfony\Component\Console\Event\ConsoleExceptionEvent;
+use Symfony\Component\HttpKernel\Kernel;
 
 class ConsoleExceptionListenerTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @group legacy
+     */
     public function testListener()
     {
+        if (Kernel::VERSION_ID >= 40000) {
+            static::markTestSkipped('Skipping legacy test');
+        }
+
         $logger = $this->createMock(LoggerInterface::class);
         $listener = new ConsoleExceptionListener($logger);
-
         $event = $this->createMock(ConsoleExceptionEvent::class);
-        if (class_exists(ConsoleErrorEvent::class)) {
-            $this->assertNull($listener->onConsoleException($event));
-        } else {
-            $command = new ApplyAclCommand();
-            $exception = new Exception();
 
-            $logger->expects($this->once())->method('error')->willReturn(true);
+        $command = new ApplyAclCommand();
+        $exception = new Exception();
 
-            $event->expects($this->once())->method('getCommand')->willReturn($command);
-            $event->expects($this->once())->method('getException')->willReturn($exception);
+        $logger->expects($this->once())->method('error')->willReturn(true);
 
-            $listener->onConsoleException($event);
-        }
+        $event->expects($this->once())->method('getCommand')->willReturn($command);
+        $event->expects($this->once())->method('getException')->willReturn($exception);
+
+        $listener->onConsoleException($event);
     }
 }
