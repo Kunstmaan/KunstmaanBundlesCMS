0a6da5ca67a3b31288eb78a96d59358aa16f4f63
Peter Zentjens <peter.zentjens@kunstmaan.be>
2019-01-30 09:09:57 +0100

    [TranslatorBundle] improved ExportCommandHandler test

--- src/Kunstmaan/TranslatorBundle/Tests/unit/Service/Exporter/ExportCommandHandlerTest.php
+++ src/Kunstmaan/TranslatorBundle/Tests/unit/Service/Exporter/ExportCommandHandlerTest.php
@@ -2,30 +2,50 @@
 
 namespace Kunstmaan\TranslatorBundle\Tests\Service\Exporter;
 
+use Codeception\Test\Unit;
+use Doctrine\Common\Collections\ArrayCollection;
+use Kunstmaan\TranslatorBundle\Entity\Translation;
 use Kunstmaan\TranslatorBundle\Model\Export\ExportCommand;
-use Kunstmaan\TranslatorBundle\Tests\unit\WebTestCase;
+use Kunstmaan\TranslatorBundle\Model\Export\ExportFile;
+use Kunstmaan\TranslatorBundle\Repository\TranslationRepository;
+use Kunstmaan\TranslatorBundle\Service\Command\Exporter\ExportCommandHandler;
+//use Kunstmaan\TranslatorBundle\Tests\unit\WebTestCase;
 
-class ExportCommandHandlerTest extends WebTestCase
+class ExportCommandHandlerTest extends Unit
 {
+    const TEST_DATA_DOMAIN = 'messages';
+    const TEST_DATA_LOCALE = 'en';
+    const TEST_DATA_EXT = 'yml';
+
     private $exportCommandHandler;
 
     public function setUp()
     {
-        static::bootKernel(['test_case' => 'TranslatorBundleTest', 'root_config' => 'config.yaml']);
-        $container = static::$kernel->getContainer();
-        static::loadFixtures($container);
+        $translation = new Translation();
+        $translation
+            ->setDomain(self::TEST_DATA_DOMAIN)
+            ->setLocale(self::TEST_DATA_LOCALE)
+        ;
 
-        $this->exportCommandHandler = $container->get('kunstmaan_translator.service.exporter.command_handler');
+        $translationRepository = $this->makeEmpty(TranslationRepository::class, [
+            'getTranslationsByLocalesAndDomains' => [$translation],
+        ]);
+
+        $this->exportCommandHandler = new ExportCommandHandler();
+        $this->exportCommandHandler->setTranslationRepository($translationRepository);
+
     }
 
     public function testGetExportFiles()
     {
         $exportCommand = new ExportCommand();
         $exportCommand
-                ->setDomains(false)
-                ->setLocales(false)
-                ->setFormat('yml');
+                ->setDomains(self::TEST_DATA_DOMAIN)
+                ->setLocales(self::TEST_DATA_LOCALE)
+                ->setFormat(self::TEST_DATA_EXT);
 
-        $this->exportCommandHandler->getExportFiles($exportCommand);
+        $files = $this->exportCommandHandler->getExportFiles($exportCommand);
+        $this->assertInstanceOf(ArrayCollection::class, $files);
+        $this->assertInstanceOf(ExportFile::class, $files->first());
     }
 }
