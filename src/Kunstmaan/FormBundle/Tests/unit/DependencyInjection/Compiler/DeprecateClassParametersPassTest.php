d1e24066fbd52adb70b82ee2544969a1c966c0a2
Jeroen Thora <jeroenthora@gmail.com>
2019-01-31 12:07:34 +0100

    [FormBundle] Deprecate service class parameters

---
+++ src/Kunstmaan/FormBundle/Tests/unit/DependencyInjection/Compiler/DeprecateClassParametersPassTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kunstmaan\FormBundle\Tests\DependencyInjection\Compiler;
+
+use Kunstmaan\FormBundle\DependencyInjection\Compiler\DeprecateClassParametersPass;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class DeprecateClassParametersPassTest extends AbstractCompilerPassTestCase
+{
+    protected function registerCompilerPass(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DeprecateClassParametersPass());
+    }
+
+    /**
+     * @group legacy
+     * @expectedDeprecation Using the "%s" parameter to change the class of the service definition is deprecated in KunstmaanFormBundle 5.2 and will be removed in KunstmaanFormBundle 6.0. Use service decoration or a service alias instead.
+     */
+    public function testServiceClassParameterOverride()
+    {
+        $this->setParameter('kunstmaan_form.form_mailer.class', 'Custom\Class');
+
+        $this->compile();
+    }
+}
