5414d5b7858d8122788f1af08f85dec968c39ea8
Peter Zentjens <peter.zentjens@kunstmaan.be>
2019-01-31 11:32:08 +0100

    [DashboardBundle] updated phpunit namespaces

--- src/Kunstmaan/DashboardBundle/Tests/unit/DependencyInjection/ConfigurationTest.php
+++ src/Kunstmaan/DashboardBundle/Tests/unit/DependencyInjection/ConfigurationTest.php
@@ -4,12 +4,12 @@ namespace Kunstmaan\DashboardBundle\Tests\DependencyInjection;
 
 use Kunstmaan\DashboardBundle\DependencyInjection\Configuration;
 use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class ConfigurationTest
  */
-class ConfigurationTest extends PHPUnit_Framework_TestCase
+class ConfigurationTest extends TestCase
 {
     use ConfigurationTestCaseTrait;
 
