d7b70f2a93817fe00a053408ec51039742ce856c
Jeroen Thora <jeroenthora@gmail.com>
2019-01-31 13:07:38 +0100

    [CacheBundle] Deprecate service class parameters

--- src/Kunstmaan/CacheBundle/KunstmaanCacheBundle.php
+++ src/Kunstmaan/CacheBundle/KunstmaanCacheBundle.php
@@ -2,8 +2,14 @@
 
 namespace Kunstmaan\CacheBundle;
 
+use Kunstmaan\CacheBundle\DependencyInjection\Compiler\DeprecateClassParametersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KunstmaanCacheBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DeprecateClassParametersPass());
+    }
 }
