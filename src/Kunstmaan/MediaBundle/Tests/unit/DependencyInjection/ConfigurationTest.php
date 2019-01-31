8e408de0c3d5e96cda0917f6137fb05970fb5ea8
Peter Zentjens <peter.zentjens@kunstmaan.be>
2019-01-31 16:28:26 +0100

    [MediaBundle] updated phpunit namespaces

--- src/Kunstmaan/MediaBundle/Tests/unit/DependencyInjection/ConfigurationTest.php
+++ src/Kunstmaan/MediaBundle/Tests/unit/DependencyInjection/ConfigurationTest.php
@@ -4,13 +4,13 @@ namespace Kunstmaan\MediaBundle\Tests\DependencyInjection;
 
 use Kunstmaan\MediaBundle\DependencyInjection\Configuration;
 use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Class ConfigurationTest
  */
-class ConfigurationTest extends PHPUnit_Framework_TestCase
+class ConfigurationTest extends TestCase
 {
     use ConfigurationTestCaseTrait;
 
