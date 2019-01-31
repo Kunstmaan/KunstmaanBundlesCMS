d49ae6708f44d55c3d9a4954521f35ab8a452cd7
Jeroen Thora <jeroenthora@gmail.com>
2019-01-31 10:08:08 +0100

    [MenuBundle] Deprecate service class parameters

--- src/Kunstmaan/MenuBundle/KunstmaanMenuBundle.php
+++ src/Kunstmaan/MenuBundle/KunstmaanMenuBundle.php
@@ -2,8 +2,14 @@
 
 namespace Kunstmaan\MenuBundle;
 
+use Kunstmaan\MenuBundle\DependencyInjection\Compiler\DeprecateClassParametersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class KunstmaanMenuBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DeprecateClassParametersPass());
+    }
 }
