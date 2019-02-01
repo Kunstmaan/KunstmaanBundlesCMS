acaf0578e78f517969d046f9a98621e617ad3a98
Jeroen Thora <jeroenthora@gmail.com>
2019-02-01 12:01:39 +0100

    [FormBundle] Deprecate service class parameters (#2303)

--- src/Kunstmaan/FormBundle/KunstmaanFormBundle.php
+++ src/Kunstmaan/FormBundle/KunstmaanFormBundle.php
@@ -2,6 +2,8 @@
 
 namespace Kunstmaan\FormBundle;
 
+use Kunstmaan\FormBundle\DependencyInjection\Compiler\DeprecateClassParametersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,4 +11,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class KunstmaanFormBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new DeprecateClassParametersPass());
+    }
 }
