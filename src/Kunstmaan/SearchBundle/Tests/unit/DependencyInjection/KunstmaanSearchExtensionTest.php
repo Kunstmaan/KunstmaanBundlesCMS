e6e3944c9d9768c0dabd899fbec16ed0e9d73349
Jeroen Thora <jeroenthora@gmail.com>
2019-02-04 09:44:36 +0100

    [SearchBundle] Move search parameters to search_bundle config instead of parameters (#2276)

--- src/Kunstmaan/SearchBundle/Tests/unit/DependencyInjection/KunstmaanSearchExtensionTest.php
+++ src/Kunstmaan/SearchBundle/Tests/unit/DependencyInjection/KunstmaanSearchExtensionTest.php
@@ -3,48 +3,145 @@
 namespace Kunstmaan\SearchBundle\Tests\DependencyInjection;
 
 use Kunstmaan\SearchBundle\DependencyInjection\KunstmaanSearchExtension;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
 
-class KunstmaanSearchExtensionTest extends TestCase
+class KunstmaanSearchExtensionTest extends AbstractExtensionTestCase
 {
-    /**
-     * @var KunstmaanSearchExtension
-     */
-    private $extension;
-
-    public function setUp()
+    protected function getContainerExtensions()
     {
-        parent::setUp();
-
-        $this->extension = $this->getExtension();
+        return [new KunstmaanSearchExtension()];
     }
 
-    public function testGetConfig()
+    public function testCorrectParametersHaveBeenSet()
     {
-        $container = $this->getContainer();
-        $this->extension->load(array(array()), $container);
-        $this->assertTrue($container->hasParameter('analyzer_languages'));
-        $this->assertTrue(is_array($container->getParameter('analyzer_languages')));
+        $this->load([
+            'connection' => [
+                'driver' => 'elastic_search',
+            ],
+        ]);
 
-        $analyzers = $container->getParameter('analyzer_languages');
+        $this->assertContainerBuilderHasParameter('analyzer_languages');
+
+        $analyzers = $this->container->getParameter('analyzer_languages');
+
+        $this->assertTrue(is_array($analyzers));
         $this->assertTrue(array_key_exists('ar', $analyzers));
         $this->assertEquals('arabic', $analyzers['ar']['analyzer']);
     }
 
-    /**
-     * @return KunstmaanSearchExtension
-     */
-    protected function getExtension()
+    public function testConnectionHostWithnoConfig()
     {
-        return new KunstmaanSearchExtension();
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.hostname', 'localhost');
     }
 
     /**
-     * @return ContainerBuilder
+     * @group legacy
+     * @expectedDeprecation Not providing a value for the "kunstmaan_search.connection.host" config while setting the "kunstmaan_search.hostname" parameter is deprecated since KunstmaanDashboardBundle 5.2, this config value will replace the "kunstmaan_search.hostname" parameter in KunstmaanDashboardBundle 6.0.
      */
-    private function getContainer()
+    public function testConnectionHostWithParameterSet()
     {
-        return new ContainerBuilder();
+        $this->setParameter('kunstmaan_search.hostname', '127.0.0.1');
+
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.hostname', '127.0.0.1');
+    }
+
+    public function testConnectionHostWithParameterAndConfigSet()
+    {
+        $this->setParameter('kunstmaan_search.hostname', '127.0.0.1');
+
+        $this->load(['connection' => ['driver' => 'elastic_search', 'host' => 'localhost']]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.hostname', 'localhost');
+    }
+
+    public function testConnectionPortWithnoConfig()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.port', 9200);
+    }
+
+    /**
+     * @group legacy
+     * @expectedDeprecation Not providing a value for the "kunstmaan_search.connection.port" config while setting the "kunstmaan_search.port" parameter is deprecated since KunstmaanDashboardBundle 5.2, this config value will replace the "kunstmaan_search.port" parameter in KunstmaanDashboardBundle 6.0.
+     */
+    public function testConnectionPortWithParameterSet()
+    {
+        $this->setParameter('kunstmaan_search.port', 9300);
+
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.port', 9300);
+    }
+
+    public function testConnectionPortWithParameterAndConfigSet()
+    {
+        $this->setParameter('kunstmaan_search.port', 9300);
+
+        $this->load(['connection' => ['driver' => 'elastic_search', 'port' => 9205]]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.port', 9205);
+    }
+
+    public function testConnectionUsernameWithnoConfig()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.username', null);
+    }
+
+    /**
+     * @group legacy
+     * @expectedDeprecation Not providing a value for the "kunstmaan_search.connection.username" config while setting the "kunstmaan_search.username" parameter is deprecated since KunstmaanDashboardBundle 5.2, this config value will replace the "kunstmaan_search.username" parameter in KunstmaanDashboardBundle 6.0.
+     */
+    public function testConnectionUsernameWithParameterSet()
+    {
+        $this->setParameter('kunstmaan_search.username', 'user');
+
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.username', 'user');
+    }
+
+    public function testConnectionUsernameWithParameterAndConfigSet()
+    {
+        $this->setParameter('kunstmaan_search.username', 'user');
+
+        $this->load(['connection' => ['driver' => 'elastic_search', 'username' => 'config_user']]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.username', 'config_user');
+    }
+
+    public function testConnectionPasswordWithnoConfig()
+    {
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.password', null);
+    }
+
+    /**
+     * @group legacy
+     * @expectedDeprecation Not providing a value for the "kunstmaan_search.connection.password" config while setting the "kunstmaan_search.password" parameter is deprecated since KunstmaanDashboardBundle 5.2, this config value will replace the "kunstmaan_search.password" parameter in KunstmaanDashboardBundle 6.0.
+     */
+    public function testConnectionPasswordWithParameterSet()
+    {
+        $this->setParameter('kunstmaan_search.password', 'password');
+
+        $this->load();
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.password', 'password');
+    }
+
+    public function testConnectionPasswordWithParameterAndConfigSet()
+    {
+        $this->setParameter('kunstmaan_search.password', 'password');
+
+        $this->load(['connection' => ['driver' => 'elastic_search', 'password' => 'other_password']]);
+
+        $this->assertContainerBuilderHasParameter('kunstmaan_search.password', 'other_password');
     }
 }
