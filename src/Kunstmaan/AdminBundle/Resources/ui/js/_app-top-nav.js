33a64f5cda76bf996794788c128ba8ff20d956a1
Dries Beerten <dries.beerten@kunstmaan.be>
2019-02-19 11:46:28 +0100

    [AdminBundle] dynamic breakpoint for top navbar in admin area

---
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_app-top-nav.js
@@ -0,0 +1,41 @@
+var kunstmaanbundles = kunstmaanbundles || {};
+
+kunstmaanbundles.topNav = (function($, window, undefined) {
+
+    var $navBar, collapseClass,
+        init, initAutoCollapse, doCheck;
+
+    collapseClass = 'collapsed';
+
+    init = function() {
+        initAutoCollapse();
+    };
+
+    initAutoCollapse = function() {
+        $navBar = $('.js-app-top-nav');
+
+        doCheck();
+
+        $(window).on('resize', function() {
+            doCheck();
+        });
+    };
+
+    doCheck = function() {
+        $navBar.removeClass(collapseClass);
+
+        var navBarHeight = $navBar.height();
+        var singleNavBarItemHeight = $navBar.find('li:first-child').innerHeight();
+
+        if (navBarHeight > singleNavBarItemHeight) {
+            $navBar.addClass(collapseClass);
+        } else {
+            $navBar.removeClass(collapseClass);
+        }
+    };
+
+    return {
+        init: init
+    };
+
+})(jQuery, window);
