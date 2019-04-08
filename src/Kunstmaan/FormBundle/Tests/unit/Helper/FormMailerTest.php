5d2fc4affb19c343889255001d40cb5617d8b8f1
Kevin Jossart <kevin.jossart@kunstmaan.be>
2019-04-08 14:44:31 +0200

    [FormBundle] In newer versions of symfony the @templating service is changed to the delegatingengine instead of the twigengine

--- src/Kunstmaan/FormBundle/Tests/unit/Helper/FormMailerTest.php
+++ src/Kunstmaan/FormBundle/Tests/unit/Helper/FormMailerTest.php
@@ -5,29 +5,33 @@ namespace Kunstmaan\FormBundle\Tests\Helper;
 use Kunstmaan\FormBundle\Entity\FormSubmission;
 use Kunstmaan\FormBundle\Helper\FormMailer;
 use PHPUnit\Framework\TestCase;
-use Symfony\Bundle\TwigBundle\TwigEngine;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Templating\DelegatingEngine;
 
 class FormMailerTest extends TestCase
 {
     public function testSendContactMail()
     {
+        /** @var \Swift_Mailer $mailer */
         $mailer = $this->createMock(\Swift_Mailer::class);
-        $twigEngine = $this->createMock(TwigEngine::class);
+        /** @var DelegatingEngine $twigEngine */
+        $twigEngine = $this->createMock(DelegatingEngine::class);
+        /** @var ContainerInterface $container */
         $container = $this->createMock(ContainerInterface::class);
         $request = $this->createMock(Request::class);
         $requestStack = $this->createMock(RequestStack::class);
 
         $mailer->expects($this->once())->method('send');
-        $request->expects($this->once())->method('getScheme')->will($this->returnValue('http'));
-        $request->expects($this->once())->method('getHttpHost')->will($this->returnValue('example.com'));
-        $requestStack->expects($this->once())->method('getCurrentRequest')->will($this->returnValue($request));
-        $container->expects($this->once())->method('get')->will($this->returnValue($requestStack));
+        $request->expects($this->once())->method('getScheme')->willReturn('http');
+        $request->expects($this->once())->method('getHttpHost')->willReturn('example.com');
+        $requestStack->expects($this->once())->method('getCurrentRequest')->willReturn($request);
+        $container->expects($this->once())->method('get')->willReturn($requestStack);
 
         $formMailer = new FormMailer($mailer, $twigEngine, $container);
 
+        /** @var FormSubmission $formSubmission */
         $formSubmission = $this->createMock(FormSubmission::class);
 
         $formMailer->sendContactMail($formSubmission, 'from@example.com', 'to@example.com', 'subject');
