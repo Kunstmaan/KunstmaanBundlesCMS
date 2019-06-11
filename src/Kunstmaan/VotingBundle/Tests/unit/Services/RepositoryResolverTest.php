40d998617f920988d8dcf731813e9625a5562365
Jeroen Thora <jeroenthora@gmail.com>
2019-06-11 14:27:18 +0200

    [VotingBundle] Code improvement found by static code analysis (#2484)

--- src/Kunstmaan/VotingBundle/Tests/unit/Services/RepositoryResolverTest.php
+++ src/Kunstmaan/VotingBundle/Tests/unit/Services/RepositoryResolverTest.php
@@ -15,7 +15,7 @@ class RepositoryResolverTest extends TestCase
      */
     public function testGetRepositoryForEvent($event, $repositoryname)
     {
-        $mockEm = $this->createMock('Doctrine\ORM\EntityManager', array('getRepository'), array(), 'MockedEm', false);
+        $mockEm = $this->createMock('Doctrine\ORM\EntityManager');
 
         $mockEm->expects($this->once())
                  ->method('getRepository')
@@ -29,11 +29,11 @@ class RepositoryResolverTest extends TestCase
     public function dataRepositoryEvent()
     {
         return array(
-            array($this->createMock('\Kunstmaan\VotingBundle\Event\UpDown\DownVoteEvent', array(), array(), 'MockDownVoteEvent', false), 'Kunstmaan\VotingBundle\Entity\UpDown\DownVote'),
-            array($this->createMock('\Kunstmaan\VotingBundle\Event\UpDown\UpVoteEvent', array(), array(), 'MockUpVoteEvent', false), 'Kunstmaan\VotingBundle\Entity\UpDown\UpVote'),
-            array($this->createMock('\Kunstmaan\VotingBundle\Event\Facebook\FacebookLikeEvent', array(), array(), 'MockFacebookLikeEvent', false), 'Kunstmaan\VotingBundle\Entity\Facebook\FacebookLike'),
-            array($this->createMock('\Kunstmaan\VotingBundle\Event\Facebook\FacebookSendEvent', array(), array(), 'MockFacebookSendEvent', false), 'Kunstmaan\VotingBundle\Entity\Facebook\FacebookSend'),
-            array($this->createMock('\Kunstmaan\VotingBundle\Event\LinkedIn\LinkedInShareEvent', array(), array(), 'MockLinkedInShareEvent', false), 'Kunstmaan\VotingBundle\Entity\LinkedIn\LinkedInShare'),
+            array($this->createMock('\Kunstmaan\VotingBundle\Event\UpDown\DownVoteEvent'), 'Kunstmaan\VotingBundle\Entity\UpDown\DownVote'),
+            array($this->createMock('\Kunstmaan\VotingBundle\Event\UpDown\UpVoteEvent'), 'Kunstmaan\VotingBundle\Entity\UpDown\UpVote'),
+            array($this->createMock('\Kunstmaan\VotingBundle\Event\Facebook\FacebookLikeEvent'), 'Kunstmaan\VotingBundle\Entity\Facebook\FacebookLike'),
+            array($this->createMock('\Kunstmaan\VotingBundle\Event\Facebook\FacebookSendEvent'), 'Kunstmaan\VotingBundle\Entity\Facebook\FacebookSend'),
+            array($this->createMock('\Kunstmaan\VotingBundle\Event\LinkedIn\LinkedInShareEvent'), 'Kunstmaan\VotingBundle\Entity\LinkedIn\LinkedInShare'),
         );
     }
 }
