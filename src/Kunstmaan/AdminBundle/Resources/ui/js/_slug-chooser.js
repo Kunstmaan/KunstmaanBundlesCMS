b93e0971114b20bc27a27e4f2f58d9028c3a4ad6
Joshwin Zuidema <jzuidema@e-sites.nl>
2019-07-08 14:03:43 +0200

    Moved prefix

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
@@ -1,9 +1,8 @@
 var kunstmaanbundles = kunstmaanbundles || {};
 
-kunstmaanbundles.slugChooser = (function(window, undefined) {
+kunstmaanbundles.slugChooser = (function() {
 
-    var init, slugChooser,
-        updateSlugPreview, resetSlug;
+    var init, slugChooser, resetSlug;
 
 
     init = function() {
@@ -16,53 +15,21 @@ kunstmaanbundles.slugChooser = (function(window, undefined) {
 
     // Slug-Chooser
     slugChooser = function($widget) {
-        var resetValue = $widget.data('reset'),
-            urlprefix = $widget.data('url-prefix');
-
-        // Setup url prefix
-        if(urlprefix.length == 0 || urlprefix.indexOf('/', urlprefix.length - 1) == -1) { //endwidth
-            urlprefix += '/';
-        }
+        var resetValue = $widget.data('reset');
 
         // Elements
         var $input = $widget.find('.js-slug-chooser__input'),
-            $preview = $widget.find('.js-slug-chooser__preview'),
             $resetBtn = $widget.find('.js-slug-chooser__reset-btn');
-
-        // Update
-        $input.on('change', function() {
-            updateSlugPreview($input, $preview, urlprefix, resetValue);
-        });
-        $input.on('keyup', function() {
-            updateSlugPreview($input, $preview, urlprefix, resetValue);
-        });
 
         // Reset Btn
         $resetBtn.on('click', function() {
             resetSlug($input, resetValue);
-            updateSlugPreview($input, $preview, urlprefix, resetValue);
         });
-
-        // Set initial value
-        updateSlugPreview($input, $preview, urlprefix, resetValue);
     };
 
 
     resetSlug = function($input, resetValue) {
         $input.val(resetValue);
-    };
-
-
-    updateSlugPreview = function($input, $preview, urlprefix, resetValue) {
-        var inputValue = $input.val();
-
-        if(inputValue === resetValue) {
-            $preview.hide();
-
-            return;
-        }
-
-        $preview.html('url: ' + urlprefix + inputValue).show();
     };
 
 
