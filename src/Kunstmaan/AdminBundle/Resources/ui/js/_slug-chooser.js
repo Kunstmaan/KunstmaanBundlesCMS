ccaaa46962893b841bb93cf1b05ff8a6e283ad2e
Joshwin Zuidema <jzuidema@e-sites.nl>
2019-07-19 14:12:26 +0200

    feature/current-url-menu-tab

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
@@ -16,49 +16,21 @@ kunstmaanbundles.slugChooser = (function(window, undefined) {
 
     // Slug-Chooser
     slugChooser = function($widget) {
-        var resetValue = $widget.data('reset'),
-            urlprefix = $widget.data('url-prefix');
-
-        // Setup url prefix
-        if(urlprefix.length == 0 || urlprefix.indexOf('/', urlprefix.length - 1) == -1) { //endwidth
-            urlprefix += '/';
-        }
+        var resetValue = $widget.data('reset');
 
         // Elements
         var $input = $widget.find('.js-slug-chooser__input'),
-            $preview = $widget.find('.js-slug-chooser__preview'),
             $resetBtn = $widget.find('.js-slug-chooser__reset-btn');
-
-        // Update
-        $input.on('change', function() {
-            updateSlugPreview($input, $preview, urlprefix);
-        });
-        $input.on('keyup', function() {
-            updateSlugPreview($input, $preview, urlprefix);
-        });
 
         // Reset Btn
         $resetBtn.on('click', function() {
             resetSlug($input, resetValue);
-            updateSlugPreview($input, $preview, urlprefix);
         });
-
-        // Set initial value
-        updateSlugPreview($input, $preview, urlprefix);
     };
-
 
     resetSlug = function($input, resetValue) {
         $input.val(resetValue);
     };
-
-
-    updateSlugPreview = function($input, $preview, urlprefix) {
-        var inputValue = $input.val();
-
-        $preview.html('url: ' + urlprefix + inputValue);
-    };
-
 
     return {
         init: init
