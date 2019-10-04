6c3eef54d12416279f7aa6003334c63d8dffd8d3
Joshwin Zuidema <jzuidema@e-sites.nl>
2019-10-04 15:56:30 +0200

    display updated url

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_slug-chooser.js
@@ -20,12 +20,37 @@ kunstmaanbundles.slugChooser = (function(window, undefined) {
 
         // Elements
         var $input = $widget.find('.js-slug-chooser__input'),
+            $preview = $widget.find('.js-slug-chooser__preview'),
+            $prefix = $widget.find('.js-slug-chooser__prefix'),
             $resetBtn = $widget.find('.js-slug-chooser__reset-btn');
+
+        // Update
+        $input.on('change', function() {
+            updateSlugPreview($input, $preview, $prefix);
+        });
+        $input.on('keyup', function() {
+            updateSlugPreview($input, $preview, $prefix);
+        });
 
         // Reset Btn
         $resetBtn.on('click', function() {
             resetSlug($input, resetValue);
         });
+    };
+
+    updateSlugPreview = function($input, $preview, $prefix) {
+        var updatedUrl = $prefix.html() + $input.val();
+
+        if($input.attr('data-slug') === $input.val()) {
+            $preview.hide();
+
+            return;
+        }
+
+        $preview.find('a').attr('href', updatedUrl);
+        $preview.find('a').html(updatedUrl);
+
+        $preview.show();
     };
 
     resetSlug = function($input, resetValue) {
