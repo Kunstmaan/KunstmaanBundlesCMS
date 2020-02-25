315b3c89439622d7c5f4faff0f42df9b09d0c328
Kevin Jossart <kevin.jossart@kunstmaan.be>
2020-02-25 12:01:28 +0100

    restore some of the js

--- src/Kunstmaan/AdminBundle/Resources/ui/js/_media-chooser.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/js/_media-chooser.js
@@ -1,26 +1,31 @@
 var kunstmaanbundles = kunstmaanbundles || {};
 
-kunstmaanbundles.mediaChooser = (function(window, undefined) {
+kunstmaanbundles.mediaChooser = (function (window, undefined) {
 
-    var init, initDelBtn;
+    var init, initDelBtn, initCropBtn;
 
     var $body = $('body');
 
-    init = function() {
+    init = function () {
         // Save and update preview can be found in url-chooser.js
         initDelBtn();
+        initCropBtn();
     };
 
 
     // Del btn
-    initDelBtn = function() {
-        $body.on('click', '.js-media-chooser-del-preview-btn', function(e) {
+    initDelBtn = function () {
+        $body.on('click', '.js-media-chooser-del-preview-btn', function (e) {
             var $this = $(this),
                 linkedID = $this.data('linked-id'),
                 $widget = $('#' + linkedID + '-widget'),
                 $input = $('#' + linkedID);
 
-            $this.parent('.media-chooser__preview').find('.media-chooser__preview__img').attr({'src': '', 'srcset': '', 'alt': ''});
+            $this.parent('.media-chooser__preview').find('.media-chooser__preview__img').attr({
+                'src': '',
+                'srcset': '',
+                'alt': ''
+            });
 
             $(".media-thumbnail__icon").remove();
 
@@ -29,6 +34,17 @@ kunstmaanbundles.mediaChooser = (function(window, undefined) {
         });
     };
 
+    // Crop btn
+    initCropBtn = function () {
+        $body.on('click', '.js-media-chooser-crop-preview-btn', function (element) {
+            var $this = $(this),
+                linkedID = $this.data('linked-id'),
+                $mediaCropperModal = $('#' + linkedID + '-mediaCropperModal');
+
+            $mediaCropperModal.modal('show');
+
+        });
+    };
 
     return {
         init: init
