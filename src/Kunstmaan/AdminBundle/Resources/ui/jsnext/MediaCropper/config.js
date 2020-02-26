52d9d9868f2665f378112934f34243897b32a622
kevin.five <kevin.five@:accenture.com>
2020-02-26 14:13:03 +0100

    new crop vendor | move crop handles outside on smaller crop sizes

---
+++ src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/config.js
@@ -0,0 +1,31 @@
+const SELECTORS = {
+    HOOK: '.js-media-chooser-crop-preview-btn',
+    CONTAINER: '.js-media-cropper',
+    IMAGE: '.js-media-cropper-image',
+    META_CONTAINER: '.js-media-cropper-meta',
+    META_ITEM: '.js-media-cropper-meta-value',
+};
+
+const MODIFIERS = {
+    CROP_BOX_SMALL_CROPPED_AREA: 'cropper-crop-box--expanded',
+};
+
+const CROP_BOX_THRESHOLD = 250;
+
+const META_KEYS = ['x', 'y', 'width', 'height'];
+
+const CROPPER_CONFIG = {
+    viewMode: 2,
+    rotatable: false,
+    zoomable: false,
+    zoomOnTouch: false,
+    zoomOnWheel: false,
+};
+
+export {
+    SELECTORS,
+    MODIFIERS,
+    CROP_BOX_THRESHOLD,
+    META_KEYS,
+    CROPPER_CONFIG,
+};
