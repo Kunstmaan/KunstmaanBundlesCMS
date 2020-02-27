c25947b5ce6dec81cc4e6b5a562872c32fa6d5e7
kevin.five <kevin.five@:accenture.com>
2020-02-27 13:46:46 +0100

    start different views

--- src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/MediaCropper.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/MediaCropper.js
@@ -1,19 +1,19 @@
 import Cropper from 'cropperjs';
 import { SELECTORS, MODIFIERS, META_KEYS, CROP_BOX_THRESHOLD } from './config';
+import { renderViewSelectOptions } from './renderViewSelectOptions';
 class MediaCropper {
     constructor(node, CROPPER_CONFIG) {
         this.node = node;
         this.image = this.node.querySelector(SELECTORS.IMAGE);
         this.metaContainer = this.node.querySelector(SELECTORS.META_CONTAINER);
+        this.viewSelect = this.metaContainer.querySelector(SELECTORS.VIEW_SELECT);
         this.metaValueNodes = {};
         this.cropper = new Cropper(this.image, CROPPER_CONFIG);
+        this.views = this.node.hasAttribute('data-cropping-views');
+        this.viewData = {};
 
-        this.getValueNodes();
 
-        this.image.addEventListener('crop', () => {
-            const data = this.getData();
-            this.updateValue(data);
-        });
+        this.init();
     }
 
     getValueNodes() {
@@ -35,12 +35,45 @@ class MediaCropper {
         this.metaValueNodes.height.textContent = height;
 
         if ((width || height) <= CROP_BOX_THRESHOLD && !small_crop_box_area) {
-            this.cropper.cropBox.classList.add(MODIFIERS.CROP_BOX_SMALL_CROPPED_AREA);
+            this.node.classList.add(MODIFIERS.CROP_BOX_SMALL_CROPPED_AREA);
             small_crop_box_area = true;
         } else {
-            this.cropper.cropBox.classList.remove(MODIFIERS.CROP_BOX_SMALL_CROPPED_AREA);
+            this.node.classList.remove(MODIFIERS.CROP_BOX_SMALL_CROPPED_AREA);
             small_crop_box_area = false;
         }
+
+        if (this.viewData && this.currentView) {
+            this.cropData[this.currentView].x = x;
+            this.cropData[this.currentView].y = y;
+            this.cropData[this.currentView].width = width;
+            this.cropData[this.currentView].height = height;
+        }
+    }
+
+    addEventListeners() {
+        this.image.addEventListener('crop', () => {
+            const data = this.getData();
+            this.updateValue(data);
+        });
+    }
+
+    init() {
+        this.getValueNodes();
+
+        if (this.views) {
+            const viewData = JSON.parse(this.node.dataset.croppingViews);
+            viewData.forEach((view) => {
+                this.viewData[view.name] = {};
+                this.viewData[view.name].aspectRatio = view.height / view.width;
+                this.viewData[view.name].width = view.width;
+                this.viewData[view.name].height = view.height;
+            });
+            renderViewSelectOptions(this.viewSelect, this.viewData);
+        }
+
+        this.addEventListeners();
+
+        console.log(this);
     }
 }
 
--- src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/config.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/config.js
@@ -4,10 +4,11 @@ const SELECTORS = {
     IMAGE: '.js-media-cropper-image',
     META_CONTAINER: '.js-media-cropper-meta',
     META_ITEM: '.js-media-cropper-meta-value',
+    VIEW_SELECT: '.js-media-cropper-view-select',
 };
 
 const MODIFIERS = {
-    CROP_BOX_SMALL_CROPPED_AREA: 'cropper-crop-box--expanded',
+    CROP_BOX_SMALL_CROPPED_AREA: 'media-cropper--crop-box-expanded',
 };
 
 const CROP_BOX_THRESHOLD = 250;
@@ -17,6 +18,7 @@ const META_KEYS = ['x', 'y', 'width', 'height'];
 const CROPPER_CONFIG = {
     viewMode: 2,
     rotatable: false,
+    scalable: false,
     zoomable: false,
     zoomOnTouch: false,
     zoomOnWheel: false,
