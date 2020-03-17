f5fae8273f084f6664b276500f7d93033a542be2
kevin.five <kevin.five@:accenture.com>
2020-03-17 17:30:17 +0100

    setup focus point chooser

--- src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/config.js
+++ src/Kunstmaan/AdminBundle/Resources/ui/jsnext/MediaCropper/config.js
@@ -6,10 +6,15 @@ const SELECTORS = {
     META_ITEM: '.js-media-cropper-meta-value',
     VIEW_SELECT: '.js-media-cropper-view-select',
     SAVE: '.js-media-cropper-save',
+    SELECT_FOCUS_POINT: '.js-media-cropper-choose-focus-point',
+    CROPPER_PREVIEW: '.js-media-cropper-preview',
+    FOCUS_POINT_WRAPPER: '.js-media-cropper-focus-wrapper',
 };
 
 const MODIFIERS = {
     CROP_BOX_SMALL_CROPPED_AREA: 'media-cropper--crop-box-expanded',
+    FOCUS_POINT_WRAPPER_VISIBLE: 'media-cropper__focus-wrapper--visible',
+    PREVIEW_HIDDEN: 'media-cropper__preview--hidden',
 };
 
 const CROP_BOX_THRESHOLD = 250;
