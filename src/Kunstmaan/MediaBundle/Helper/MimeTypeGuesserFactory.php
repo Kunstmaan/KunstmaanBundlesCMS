2d3cd36a7f9ef2fb8bd1ea1fe6f8ebebe8b43e6f
Jeroen Thora <jeroenthora@gmail.com>
2020-09-14 14:50:46 +0200

    Improve deprecation messages

--- src/Kunstmaan/MediaBundle/Helper/MimeTypeGuesserFactory.php
+++ src/Kunstmaan/MediaBundle/Helper/MimeTypeGuesserFactory.php
@@ -6,10 +6,8 @@ use Kunstmaan\MediaBundle\Helper\File\SVGMimeTypeGuesser;
 use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser;
 use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesserInterface;
 
-@trigger_error(sprintf('The "%s" class is deprecated since KunstmaanMediaBundle 5.7 and will be removed in KunstmaanMediaBundle 6.0. Use the symfony/mime component instead.', __CLASS__), E_USER_DEPRECATED);
-
 /**
- * @deprecated This class is deprecated since KunstmaanMediaBundle 5.7 and will be removed in KunstmaanMediaBundle 6.0. Use the symfony/mime component instead.
+ * @deprecated since KunstmaanMediaBundle 5.7 and will be removed in KunstmaanMediaBundle 6.0. Use the symfony/mime component instead.
  */
 class MimeTypeGuesserFactory implements MimeTypeGuesserFactoryInterface
 {
