182a1600c4322fa7d3faf6f7f2de46623c321c35
Kevin Jossart <kevin.jossart@kunstmaan.be>
2020-09-16 11:34:46 +0200

    add remember me functionality

--- src/Kunstmaan/AdminBundle/Form/UserLoginType.php
+++ src/Kunstmaan/AdminBundle/Form/UserLoginType.php
@@ -3,6 +3,7 @@
 namespace Kunstmaan\AdminBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,6 +22,12 @@ class UserLoginType extends AbstractType
             ->add('_password', PasswordType::class, [
                 'attr' => [
                     'class' => 'form-control form-group--icon-in-control__form-control'
+                ]
+            ])
+            ->add('_remember_me', CheckboxType::class, [
+                'required' => false,
+                'attr' => [
+                    'checked' => true,
                 ]
             ]);
     }
