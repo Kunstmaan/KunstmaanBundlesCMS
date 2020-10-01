83a6c339a9628612e1894469ecf5c57c22757585
Jeroen Thora <jeroenthora@gmail.com>
2020-10-01 15:54:35 +0200

    [AllBundles] Allow next majors of doctrine-bundle and doctrine-migrations-bundle to prepare symfony 5 support

--- src/Kunstmaan/TranslatorBundle/Service/Command/DiffCommand.php
+++ src/Kunstmaan/TranslatorBundle/Service/Command/DiffCommand.php
@@ -7,53 +7,102 @@ use Doctrine\DBAL\Migrations\Tools\Console\Command\GenerateCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class DiffCommand.
- *
- * @deprecated This class is deprecated since KunstmaanTranslatorBundle 5.2 and will be removed in 6.0.
- */
-class DiffCommand extends GenerateCommand
-{
-    protected function configure()
+if (class_exists(\Doctrine\Migrations\Tools\Console\Command\GenerateCommand::class)) {
+    /**
+     * @deprecated This class is deprecated since KunstmaanTranslatorBundle 5.2 and will be removed in 6.0.
+     */
+    class DiffCommand extends \Doctrine\Migrations\Tools\Console\Command\GenerateCommand
     {
-        parent::configure();
-    }
+        protected function configure(): void
+        {
+            parent::configure();
+        }
 
-    public function execute(InputInterface $input, OutputInterface $output)
-    {
-        $configuration = $this->getMigrationConfiguration($input, $output);
-        $sql = $this->getApplication()->getKernel()->getContainer()->get('kunstmaan_translator.service.migrations.migrations')->getDiffSqlArray();
+        public function execute(InputInterface $input, OutputInterface $output): ?int
+        {
+            $configuration = $this->getMigrationConfiguration($input, $output);
+            $sql = $this->getApplication()->getKernel()->getContainer()->get('kunstmaan_translator.service.migrations.migrations')->getDiffSqlArray();
 
-        $up = $this->buildCodeFromSql($configuration, $sql);
-        $down = '';
+            $up = $this->buildCodeFromSql($configuration, $sql);
+            $down = '';
 
-        if (!$up && !$down) {
-            $output->writeln('No changes detected in your mapping information.', 'ERROR');
+            if (!$up && !$down) {
+                $output->writeln('No changes detected in your mapping information.', 'ERROR');
+
+                return 0;
+            }
+
+            $version = date('YmdHis');
+            $path = $this->generateMigration($configuration, $input, $version, $up, $down);
+
+            $output->writeln(sprintf('Generated new migration class to "<info>%s</info>" from schema differences.', $path));
 
             return 0;
         }
 
-        $version = date('YmdHis');
-        $path = $this->generateMigration($configuration, $input, $version, $up, $down);
-
-        $output->writeln(sprintf('Generated new migration class to "<info>%s</info>" from schema differences.', $path));
-
-        return 0;
-    }
-
-    private function buildCodeFromSql(Configuration $configuration, array $sql)
-    {
-        $currentPlatform = $configuration->getConnection()->getDatabasePlatform()->getName();
-        $code = array(
-            "\$this->abortIf(\$this->connection->getDatabasePlatform()->getName() != \"$currentPlatform\", \"Migration can only be executed safely on '$currentPlatform'.\");", '',
-        );
-        foreach ($sql as $query) {
-            if (strpos($query, $configuration->getMigrationsTableName()) !== false) {
-                continue;
+        private function buildCodeFromSql(\Doctrine\Migrations\Configuration\Configuration $configuration, array $sql)
+        {
+            $currentPlatform = $configuration->getConnection()->getDatabasePlatform()->getName();
+            $code = array(
+                "\$this->abortIf(\$this->connection->getDatabasePlatform()->getName() != \"$currentPlatform\", \"Migration can only be executed safely on '$currentPlatform'.\");", '',
+            );
+            foreach ($sql as $query) {
+                if (strpos($query, $configuration->getMigrationsTableName()) !== false) {
+                    continue;
+                }
+                $code[] = "\$this->addSql(\"$query\");";
             }
-            $code[] = "\$this->addSql(\"$query\");";
+
+            return implode("\n", $code);
+        }
+    }
+} else {
+    /**
+     * @deprecated This class is deprecated since KunstmaanTranslatorBundle 5.2 and will be removed in 6.0.
+     */
+    class DiffCommand extends GenerateCommand
+    {
+        protected function configure()
+        {
+            parent::configure();
         }
 
-        return implode("\n", $code);
+        public function execute(InputInterface $input, OutputInterface $output)
+        {
+            $configuration = $this->getMigrationConfiguration($input, $output);
+            $sql = $this->getApplication()->getKernel()->getContainer()->get('kunstmaan_translator.service.migrations.migrations')->getDiffSqlArray();
+
+            $up = $this->buildCodeFromSql($configuration, $sql);
+            $down = '';
+
+            if (!$up && !$down) {
+                $output->writeln('No changes detected in your mapping information.', 'ERROR');
+
+                return 0;
+            }
+
+            $version = date('YmdHis');
+            $path = $this->generateMigration($configuration, $input, $version, $up, $down);
+
+            $output->writeln(sprintf('Generated new migration class to "<info>%s</info>" from schema differences.', $path));
+
+            return 0;
+        }
+
+        private function buildCodeFromSql(Configuration $configuration, array $sql)
+        {
+            $currentPlatform = $configuration->getConnection()->getDatabasePlatform()->getName();
+            $code = array(
+                "\$this->abortIf(\$this->connection->getDatabasePlatform()->getName() != \"$currentPlatform\", \"Migration can only be executed safely on '$currentPlatform'.\");", '',
+            );
+            foreach ($sql as $query) {
+                if (strpos($query, $configuration->getMigrationsTableName()) !== false) {
+                    continue;
+                }
+                $code[] = "\$this->addSql(\"$query\");";
+            }
+
+            return implode("\n", $code);
+        }
     }
 }
