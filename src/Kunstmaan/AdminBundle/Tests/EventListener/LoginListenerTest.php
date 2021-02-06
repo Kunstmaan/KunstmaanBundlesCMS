84bd9d64d2d3eee431889369b52d0c91db509402
Jeroen Thora <jeroenthora@gmail.com>
2021-02-06 21:46:41 +0100

    fixes

--- src/Kunstmaan/AdminBundle/Tests/EventListener/LoginListenerTest.php
+++ src/Kunstmaan/AdminBundle/Tests/EventListener/LoginListenerTest.php
@@ -2,7 +2,6 @@
 
 namespace Kunstmaan\AdminBundle\Tests\EventListener;
 
-use Doctrine\ORM\EntityManager;
 use Kunstmaan\AdminBundle\Entity\User;
 use Kunstmaan\AdminBundle\EventListener\LoginListener;
 use Kunstmaan\AdminBundle\Helper\VersionCheck\VersionChecker;
@@ -20,14 +19,13 @@ class LoginListenerTest extends TestCase
         $token = $this->createMock(TokenInterface::class);
         $user = $this->createMock(User::class);
         $event = $this->createMock(InteractiveLoginEvent::class);
-        $em = $this->createMock(EntityManager::class);
 
         $logger->expects($this->once())->method('info')->willReturn(true);
         $version->expects($this->once())->method('periodicallyCheck')->willReturn(true);
         $event->expects($this->once())->method('getAuthenticationToken')->willReturn($token);
         $token->expects($this->once())->method('getUser')->willReturn($user);
 
-        $listener = new LoginListener($logger, $version, $em);
+        $listener = new LoginListener($logger, $version);
         $listener->onSecurityInteractiveLogin($event);
     }
 }
