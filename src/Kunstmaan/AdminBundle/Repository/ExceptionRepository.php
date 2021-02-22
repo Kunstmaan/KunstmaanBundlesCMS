3b0ffb088b923feb3f7ee84848f8384965717b7a
adlangenkamp <Integra.01>
2021-02-22 13:50:16 +0100

    removed unneccesary dtatabase platform check fro exception repo

--- src/Kunstmaan/AdminBundle/Repository/ExceptionRepository.php
+++ src/Kunstmaan/AdminBundle/Repository/ExceptionRepository.php
@@ -9,10 +9,10 @@ class ExceptionRepository extends EntityRepository
     public function findAllHigherThanDays(\DateTimeInterface $date)
     {
         return $this->createQueryBuilder('e')
-            ->where('e.isResolved = :isResolvedTrue')
+            ->where('e.isResolved = :isResolved')
             ->andWhere('e.createdAt <= :date')
             ->setParameter('date', $date)
-            ->setParameter('isResolvedTrue', true)
+            ->setParameter('isResolved', true)
             ->getQuery()
             ->getResult();
     }
@@ -25,32 +25,22 @@ class ExceptionRepository extends EntityRepository
      */
     public function markAllAsResolved()
     {
-        if ($this->_em->getConnection()->getDatabasePlatform()->getName() == 'postgresql') {
-            return $this->createQueryBuilder('e')
-                ->update()
-                ->set('e.isResolved', ':isResolvedTrue')
-                ->where('e.isResolved = :isResolvedFalse')
-                ->setParameter('isResolvedTrue', true, \PDO::PARAM_BOOL)
-                ->setParameter('isResolvedFalse', false, \PDO::PARAM_BOOL)
-                ->getQuery()
-                ->getSingleScalarResult();
-        } else {
-            return $this->createQueryBuilder('e')
-                ->update()
-                ->set('e.isResolved', 1)
-                ->where('e.isResolved = :isResolvedFalse')
-                ->setParameter('isResolvedFalse', false)
-                ->getQuery()
-                ->getSingleScalarResult();
-        }
+        return $this->createQueryBuilder('e')
+            ->update()
+            ->set('e.isResolved', ':isResolvedTrue')
+            ->where('e.isResolved = :isResolvedFalse')
+            ->setParameter('isResolvedTrue', true, \PDO::PARAM_BOOL)
+            ->setParameter('isResolvedFalse', false, \PDO::PARAM_BOOL)
+            ->getQuery()
+            ->getSingleScalarResult();
     }
 
     public function findExceptionStatistics()
     {
         return $this->createQueryBuilder('e')
             ->select('COUNT(e.id) as cp_all, SUM(e.events) as cp_sum')
-            ->where('e.isResolved = :isResolvedFalse')
-            ->setParameter('isResolvedFalse', false)
+            ->where('e.isResolved = :isResolved')
+            ->setParameter('isResolved', false)
             ->getQuery()
             ->getOneOrNullResult();
     }
