b2ecee8fda9b3e31d8ab7247b9b4096ee565c6e2
Jeroen Thora <jeroenthora@gmail.com>
2021-02-28 12:29:18 +0100

    Deprecate password reset listener as the logic is already implemented in the user manager

--- src/Kunstmaan/AdminBundle/EventListener/PasswordResettingListener.php
+++ src/Kunstmaan/AdminBundle/EventListener/PasswordResettingListener.php
@@ -3,24 +3,18 @@
 namespace Kunstmaan\AdminBundle\EventListener;
 
 use FOS\UserBundle\Event\FilterUserResponseEvent;
-use FOS\UserBundle\Model\UserManager as FOSUserManager;
-use Kunstmaan\AdminBundle\Event\ChangePasswordSuccessEvent;
-use Kunstmaan\AdminBundle\Service\UserManager;
+use FOS\UserBundle\Model\UserManager;
 
+/**
+ * @deprecated since KunstmaanAdminBundle 5.8 and will be removed in KunstmaanAdminBundle 6.0. The listener logic is already executed in the `\Kunstmaan\AdminBundle\Service\UserManager::updatePassword` method.
+ */
 class PasswordResettingListener
 {
     /** @var UserManager */
     private $userManager;
 
-    public function __construct(/* UserManager */ $userManager)
+    public function __construct(UserManager $userManager)
     {
-        if (!$userManager instanceof UserManager && !$userManager instanceof FOSUserManager) {
-            throw new \InvalidArgumentException(sprintf('The "$userManager" argument must be of type "%s" or type "%s"', UserManager::class, FOSUserManager::class));
-        }
-        if ($userManager instanceof FOSUserManager) {
-            // NEXT_MAJOR set the $userManager typehint to the kunstmaan usermanager.
-            @trigger_error(sprintf('Passing the FOSUserBundle UserManager service as the "$userManager" argument of "%s" is deprecated since KunstmaanAdminBundle 5.8 and will be removed in KunstmaanAdminBundle 6.0. Use the "%s" service instead.', __METHOD__, UserManager::class), E_USER_DEPRECATED);
-        }
         $this->userManager = $userManager;
     }
 
@@ -28,13 +22,6 @@ class PasswordResettingListener
      * @deprecated Using the FosUser FilterUserResponseEvent is deprecated in KunstmaanNodeBundle 5.8 and will be removed in KunstmaanNodeBundle 6.0. Use the "Kunstmaan\AdminBundle\Event\ChangePasswordSuccessEvent" instead.
      */
     public function onPasswordResettingSuccess(FilterUserResponseEvent $event)
-    {
-        $user = $event->getUser();
-        $user->setPasswordChanged(true);
-        $this->userManager->updateUser($user);
-    }
-
-    public function onPasswordResettingSuccessCMS(ChangePasswordSuccessEvent $event)
     {
         $user = $event->getUser();
         $user->setPasswordChanged(true);
