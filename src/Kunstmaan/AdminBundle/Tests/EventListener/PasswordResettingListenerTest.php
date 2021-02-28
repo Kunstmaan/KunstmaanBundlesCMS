55bc21c0158291ca40b15c48f852c6e1793e97bf
Jeroen Thora <jeroenthora@gmail.com>
2021-02-28 13:07:31 +0100

    Fixed password reset listener test

--- src/Kunstmaan/AdminBundle/Tests/EventListener/PasswordResettingListenerTest.php
+++ src/Kunstmaan/AdminBundle/Tests/EventListener/PasswordResettingListenerTest.php
@@ -3,13 +3,10 @@
 namespace Kunstmaan\AdminBundle\Tests\EventListener;
 
 use FOS\UserBundle\Event\FilterUserResponseEvent;
-use FOS\UserBundle\Model\UserManager as FosUserManager;
-use Kunstmaan\AdminBundle\Service\UserManager;
+use FOS\UserBundle\Model\UserManager;
 use Kunstmaan\AdminBundle\Entity\User;
-use Kunstmaan\AdminBundle\Event\ChangePasswordSuccessEvent;
 use Kunstmaan\AdminBundle\EventListener\PasswordResettingListener;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\HttpFoundation\Response;
 
 class PasswordResettingListenerTest extends TestCase
 {
@@ -18,7 +15,7 @@ class PasswordResettingListenerTest extends TestCase
      */
     public function testListener()
     {
-        $manager = $this->createMock(FosUserManager::class);
+        $manager = $this->createMock(UserManager::class);
         $user = $this->createMock(User::class);
 
         $manager->expects($this->once())->method('updateUser')->willReturn(true);
@@ -29,19 +26,5 @@ class PasswordResettingListenerTest extends TestCase
 
         $listener = new PasswordResettingListener($manager);
         $listener->onPasswordResettingSuccess($event);
-    }
-
-    public function testListenerNewMethod()
-    {
-        $manager = $this->createMock(UserManager::class);
-        $user = $this->createMock(User::class);
-
-        $manager->expects($this->once())->method('updateUser');
-        $user->expects($this->once())->method('setPasswordChanged')->willReturn(true);
-
-        $event = new ChangePasswordSuccessEvent($user, new Response());
-
-        $listener = new PasswordResettingListener($manager);
-        $listener->onPasswordResettingSuccessCMS($event);
     }
 }
