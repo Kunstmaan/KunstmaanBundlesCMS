544a02a098bb734bc1e86138e2db6f0bf3ebde2d
Jeroen Thora <jeroenthora@gmail.com>
2021-04-08 23:51:06 +0200

    [NodeSearchBundle] Deprecate direct instantiation of the abstractSearchPage

--- UPGRADE-5.9.md
+++ UPGRADE-5.9.md
@@ -4,4 +4,9 @@ UPGRADE FROM 5.8 to 5.9
 AdminlistBundle
 ------------
 
-* Using the `setObjectManager`, `setThreshold` and `setLockEnabled` methods of `Kunstmaan\AdminListBundle\Service\EntityVersionLockService` is deprecated, use the constructor to inject the required values instead. 
+* Using the `setObjectManager`, `setThreshold` and `setLockEnabled` methods of `Kunstmaan\AdminListBundle\Service\EntityVersionLockService` is deprecated, use the constructor to inject the required values instead.
+
+NodeSearchBundle
+------------
+
+* Instantiating the `Kunstmaan\NodeSearchBundle\Entity\AbstractSearchPage` class is deprecated and will be made abstract. Extend your implementation from this class instead.
--- src/Kunstmaan/NodeSearchBundle/Tests/Entity/AbstractSearchPageTest.php
+++ src/Kunstmaan/NodeSearchBundle/Tests/Entity/AbstractSearchPageTest.php
@@ -4,16 +4,29 @@ namespace Kunstmaan\NodeSearchBundle\Tests\Entity;
 
 use Kunstmaan\NodeSearchBundle\Entity\AbstractSearchPage;
 use PHPUnit\Framework\TestCase;
+use Symfony\Bridge\PhpUnit\ExpectDeprecationTrait;
 
 class AbstractSearchPageTest extends TestCase
 {
+    use ExpectDeprecationTrait;
+
     public function testGetters()
     {
-        $page = new AbstractSearchPage();
+        $page = new class() extends AbstractSearchPage {};
         $this->assertEquals('KunstmaanNodeSearchBundle:AbstractSearchPage:service', $page->getControllerAction());
         $this->assertEquals('@KunstmaanNodeSearch/AbstractSearchPage/view.html.twig', $page->getDefaultView());
         $this->assertEquals('kunstmaan_node_search.search.node', $page->getSearcher());
         $this->assertFalse($page->isIndexable());
         $this->assertIsArray($page->getPossibleChildTypes());
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testInstantiationDeprecation()
+    {
+        $this->expectDeprecation('Instantiating the "Kunstmaan\NodeSearchBundle\Entity\AbstractSearchPage" class is deprecated in KunstmaanNodeSearchBundle 5.9 and will be made abstract in KunstmaanNodeSearchBundle 6.0. Extend your implementation from this class instead.');
+
+        new AbstractSearchPage();
     }
 }
