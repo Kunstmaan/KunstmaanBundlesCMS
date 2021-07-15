d5ea520dc83ede080c96ce6824363a2a302f7fd3
dannyvw <dannyvanwijk@gmail.com>
2021-07-15 14:29:54 +0000

    Add pre render event for adding sitemap indexes

---
+++ src/Kunstmaan/SitemapBundle/Model/SitemapIndex.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kunstmaan\SitemapBundle\Model;
+
+class SitemapIndex
+{
+    private $url;
+
+    public function __construct(string $url)
+    {
+        $this->url = $url;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+}
