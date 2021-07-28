e6148fcf769c85b1211856629c9ab404138159d1
Jeroen Thora <jeroenthora@gmail.com>
2021-07-28 23:47:30 +0200

    [AdminBundle] Remove admin bundle deprecations

--- src/Kunstmaan/AdminBundle/FlashMessages/FlashTypes.php
+++ src/Kunstmaan/AdminBundle/FlashMessages/FlashTypes.php
@@ -8,10 +8,6 @@ namespace Kunstmaan\AdminBundle\FlashMessages;
 class FlashTypes
 {
     const SUCCESS = 'success';
-    /**
-     * @deprecated The `FlashTypes::ERROR` constant is deprecated in KunstmaanAdminBundle 5.4 and will be removed in KunstmaanAdminBundle 6.0. Use `FlashTypes::DANGER` instead.
-     */
-    const ERROR = 'error';
     const WARNING = 'warning';
 
     const INFO = 'info';
--- src/Kunstmaan/AdminBundle/KunstmaanAdminBundle.php
+++ src/Kunstmaan/AdminBundle/KunstmaanAdminBundle.php
@@ -4,17 +4,12 @@ namespace Kunstmaan\AdminBundle;
 
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\AddLogProcessorsCompilerPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\AdminPanelCompilerPass;
-use Kunstmaan\AdminBundle\DependencyInjection\Compiler\ConsoleCompilerPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\ConvertFosUserParametersCompilerPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\DataCollectorPass;
-use Kunstmaan\AdminBundle\DependencyInjection\Compiler\DomainConfigurationPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\EnablePermissionsPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\InjectUntrackedTokenStorageCompilerPass;
 use Kunstmaan\AdminBundle\DependencyInjection\Compiler\MenuCompilerPass;
-use Kunstmaan\AdminBundle\DependencyInjection\Compiler\PagerfantaBridgePass;
 use Kunstmaan\AdminBundle\DependencyInjection\KunstmaanAdminExtension;
-use Kunstmaan\AdminBundle\DependencyInjection\PagerfantaExtension;
-use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -34,15 +29,10 @@ class KunstmaanAdminBundle extends Bundle
         $container->addCompilerPass(new AdminPanelCompilerPass());
         $container->addCompilerPass(new AddLogProcessorsCompilerPass());
         $container->addCompilerPass(new DataCollectorPass());
-        $container->addCompilerPass(new DomainConfigurationPass());
-        $container->addCompilerPass(new ConsoleCompilerPass());
         $container->addCompilerPass(new InjectUntrackedTokenStorageCompilerPass());
         $container->addCompilerPass(new EnablePermissionsPass());
         $container->addCompilerPass(new ConvertFosUserParametersCompilerPass());
 
         $container->registerExtension(new KunstmaanAdminExtension());
-
-        $container->registerExtension(new PagerfantaExtension());
-        $container->addCompilerPass(new PagerfantaBridgePass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -1); // Should run after all passes from BabDevPagerfantaBundle
     }
 }
