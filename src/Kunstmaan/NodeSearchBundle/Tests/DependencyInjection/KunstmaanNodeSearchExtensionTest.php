1adb275b867c8b31b4dd8038db5f3bdad62c48ee
Jeroen Thora <jeroenthora@gmail.com>
2021-07-28 18:52:53 +0200

    [NodeSearchBundle] Remove deprecated container parameters

--- src/Kunstmaan/NodeSearchBundle/Tests/DependencyInjection/KunstmaanNodeSearchExtensionTest.php
+++ src/Kunstmaan/NodeSearchBundle/Tests/DependencyInjection/KunstmaanNodeSearchExtensionTest.php
@@ -20,11 +20,7 @@ class KunstmaanNodeSearchExtensionTest extends AbstractExtensionTestCase
     {
         $this->load();
 
-        $this->assertContainerBuilderHasParameter('kunstmaan_node_search.search_configuration.node.class', 'Kunstmaan\NodeSearchBundle\Configuration\NodePagesConfiguration');
         $this->assertContainerBuilderHasParameter('kunstmaan_node_search.indexname', 'nodeindex');
         $this->assertContainerBuilderHasParameter('kunstmaan_node_search.indextype', 'page');
-        $this->assertContainerBuilderHasParameter('kunstmaan_node_search.search.node.class', 'Kunstmaan\NodeSearchBundle\Search\NodeSearcher');
-        $this->assertContainerBuilderHasParameter('kunstmaan_node_search.search_service.class', 'Kunstmaan\NodeSearchBundle\Services\SearchService');
-        $this->assertContainerBuilderHasParameter('kunstmaan_node_search.node_index_update.listener.class', 'Kunstmaan\NodeSearchBundle\EventListener\NodeIndexUpdateEventListener');
     }
 }
