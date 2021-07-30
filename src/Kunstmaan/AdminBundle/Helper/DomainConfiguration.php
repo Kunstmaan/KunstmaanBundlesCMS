0c7c7af6e9cf27997b10c1a5e8dd492acf5464c3
Jeroen Thora <jeroenthora@gmail.com>
2021-07-30 16:36:46 +0200

    [MultidomainBundle] Remove deprecated code

--- src/Kunstmaan/AdminBundle/Helper/DomainConfiguration.php
+++ src/Kunstmaan/AdminBundle/Helper/DomainConfiguration.php
@@ -25,23 +25,8 @@ class DomainConfiguration implements DomainConfigurationInterface
     /** @var string */
     protected $defaultLocale;
 
-    /**
-     * @param ContainerInterface|string $multilanguage
-     */
-    public function __construct(/*ContainerInterface|RequestStack*/ $requestStack, $multilanguage = null, $defaultLocale = null, $requiredLocales = null)
+    public function __construct(RequestStack $requestStack, bool $multilanguage, string $defaultLocale, string $requiredLocales)
     {
-        if ($requestStack instanceof ContainerInterface) {
-            @trigger_error('Container injection and the usage of the container is deprecated in KunstmaanNodeBundle 5.1 and will be removed in KunstmaanNodeBundle 6.0.', E_USER_DEPRECATED);
-
-            $this->container = $requestStack;
-            $this->multiLanguage = $this->container->getParameter('kunstmaan_admin.multi_language');
-            $this->defaultLocale = $this->container->getParameter('kunstmaan_admin.default_locale');
-            $this->requiredLocales = explode('|', $this->container->getParameter('kunstmaan_admin.required_locales'));
-            $this->requestStack = $this->container->get('request_stack');
-
-            return;
-        }
-
         $this->requestStack = $requestStack;
         $this->multiLanguage = $multilanguage;
         $this->defaultLocale = $defaultLocale;
