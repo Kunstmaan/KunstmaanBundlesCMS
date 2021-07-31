d5c99011c36ff47dab94c380b2bf8b3bb6246a1c
Jeroen Thora <jeroenthora@gmail.com>
2021-07-31 11:34:19 +0200

    [UserManagementBundle] Remove deprecated code

--- src/Kunstmaan/UserManagementBundle/Tests/DependencyInjection/KunstmaanUserManagementExtensionTest.php
+++ src/Kunstmaan/UserManagementBundle/Tests/DependencyInjection/KunstmaanUserManagementExtensionTest.php
@@ -20,19 +20,6 @@ class KunstmaanUserManagementExtensionTest extends AbstractExtensionTestCase
         return [new KunstmaanUserManagementExtension()];
     }
 
-    /**
-     * @group legacy
-     */
-    public function testAdminlistConfiguratorClassParameterOverride()
-    {
-        $this->expectDeprecation('Overriding the user adminlist configurator class with the "kunstmaan_user_management.user_admin_list_configurator.class" parameter is deprecated since KunstmaanUserManagementBundle 5.9 and will not be allowed in KunstmaanUserManagementBundle 6.0. Use the "kunstmaan_user_management.user.adminlist_configurator" config option instead.');
-
-        $this->setParameter('kunstmaan_user_management.user_admin_list_configurator.class', 'CustomUserAdminListConfigurator');
-        $this->load();
-
-        $this->assertContainerBuilderHasParameter('kunstmaan_user_management.user_admin_list_configurator.class', 'CustomUserAdminListConfigurator');
-    }
-
     public function testAdminlistConfiguratorClassParameterWithConfigValue()
     {
         $this->load(['user' => ['adminlist_configurator' => 'CustomUserAdminListConfigurator']]);
