a8e1de9e849366409032bf78aa513219e70a8db4
Jeroen Thora <jeroenthora@gmail.com>
2021-10-13 14:51:36 +0200

    [AllBundles] Fix getMasterRequest deprecation

--- src/Kunstmaan/NodeSearchBundle/ViewDataProvider/SearchPageViewDataProvider.php
+++ src/Kunstmaan/NodeSearchBundle/ViewDataProvider/SearchPageViewDataProvider.php
@@ -29,7 +29,7 @@ final class SearchPageViewDataProvider implements PageViewDataProviderInterface
 
     public function provideViewData(NodeTranslation $nodeTranslation, RenderContext $renderContext): void
     {
-        $request = $this->requestStack->getMasterRequest();
+        $request = method_exists($this->requestStack, 'getMainRequest') ? $this->requestStack->getMainRequest() : $this->requestStack->getMasterRequest();
         if (null === $request) {
             return;
         }
