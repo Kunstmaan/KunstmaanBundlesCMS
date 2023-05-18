4ca83a67c2f581a74b614ffce11946c16f22368d
Jeroen Thora <jeroenthora@gmail.com>
2023-05-18 13:20:37 +0200

    [MultidomainBundle] Deprecation logout handler

--- src/Kunstmaan/MultiDomainBundle/Helper/HostOverrideCleanupHandler.php
+++ src/Kunstmaan/MultiDomainBundle/Helper/HostOverrideCleanupHandler.php
@@ -2,11 +2,15 @@
 
 namespace Kunstmaan\MultiDomainBundle\Helper;
 
+use Kunstmaan\MultiDomainBundle\EventSubscriber\LogoutHostOverrideCleanupEventSubscriber;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Logout\LogoutHandlerInterface;
 
+/**
+ * @deprecated since 6.3. Will be removed in 7.0, the "Kunstmaan\MultiDomainBundle\EventSubscriber\LogoutHostOverrideCleanupEventSubscriber" will be used instead.
+ */
 class HostOverrideCleanupHandler implements LogoutHandlerInterface
 {
     /**
@@ -16,6 +20,7 @@ class HostOverrideCleanupHandler implements LogoutHandlerInterface
      */
     public function logout(Request $request, Response $response, TokenInterface $token)
     {
+        trigger_deprecation('kunstmaan/multidomain-bundle', '6.3', 'The "%s" class is deprecated and is replaced by the "%s" subscriber with the new authentication system.', self::class, LogoutHostOverrideCleanupEventSubscriber::class);
         // Remove host override
         if ($request->hasPreviousSession() && $request->getSession()->has(DomainConfiguration::OVERRIDE_HOST)) {
             $request->getSession()->remove(DomainConfiguration::OVERRIDE_HOST);
