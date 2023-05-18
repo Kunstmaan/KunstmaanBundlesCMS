d7f519402e8141f786ad5fc14325ce43bd25e494
Jeroen Thora <jeroenthora@gmail.com>
2023-05-18 13:52:02 +0200

    [FormBundle] Replace container access with dependency injection in FormHandler

--- src/Kunstmaan/FormBundle/Helper/FormHandler.php
+++ src/Kunstmaan/FormBundle/Helper/FormHandler.php
@@ -2,8 +2,7 @@
 
 namespace Kunstmaan\FormBundle\Helper;
 
-use ArrayObject;
-use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Kunstmaan\FormBundle\Entity\FormAdaptorInterface;
 use Kunstmaan\FormBundle\Entity\FormSubmission;
 use Kunstmaan\FormBundle\Entity\FormSubmissionField;
@@ -14,37 +13,54 @@ use Kunstmaan\NodeBundle\Helper\RenderContext;
 use Kunstmaan\PagePartBundle\Entity\PagePartRef;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * The form handler handles everything from creating the form to handling the submitted form
  */
 class FormHandler implements FormHandlerInterface
 {
-    /**
-     * @var ContainerInterface
-     */
-    private $container;
+    private ContainerInterface $container;
+    private EntityManagerInterface $em;
+    private FormFactoryInterface $formFactory;
+    private RouterInterface $router;
+    private EventDispatcherInterface $eventDispatcher;
 
-    public function __construct(ContainerInterface $container)
-    {
+    public function __construct(
+        ContainerInterface $container,
+        EntityManagerInterface $em = null,
+        FormFactoryInterface $formFactory = null,
+        RouterInterface $router = null,
+        EventDispatcherInterface $eventDispatcher = null
+    ) {
         $this->container = $container;
+
+        if (null === $em) {
+            trigger_deprecation('kunstmaan/form-bundle', '6.3', 'Not passing the required services to "%s" is deprecated and those parameters will be required in 7.0. Injected the required services in the constructor instead.', __METHOD__);
+
+            $this->em = $this->container->get('doctrine.orm.entity_manager');
+            $this->formFactory = $this->container->get('form.factory');
+            $this->router = $this->container->get('router');
+            $this->eventDispatcher = $this->container->get('event_dispatcher');
+
+            return;
+        }
+
+        $this->em = $em;
+        $this->formFactory = $formFactory;
+        $this->router = $router;
+        $this->eventDispatcher = $eventDispatcher;
     }
 
     public function handleForm(FormPageInterface $page, Request $request, RenderContext $context)
     {
-        /* @var EntityManager $em */
-        $em = $this->container->get('doctrine.orm.entity_manager');
-        /* @var FormBuilderInterface $formBuilder */
-        $formBuilder = $this->container->get('form.factory')->createBuilder(FormType::class);
-        /* @var RouterInterface $router */
-        $router = $this->container->get('router');
-        /* @var ArrayObject $fields */
+        $formBuilder = $this->formFactory->createBuilder(FormType::class);
         $fields = new \ArrayObject();
-        $pageParts = $em->getRepository(PagePartRef::class)->getPageParts($page, $page->getFormElementsContext());
+        $pageParts = $this->em->getRepository(PagePartRef::class)->getPageParts($page, $page->getFormElementsContext());
         foreach ($pageParts as $sequence => $pagePart) {
             if ($pagePart instanceof FormAdaptorInterface) {
                 $pagePart->adaptForm($formBuilder, $fields, $sequence);
@@ -57,24 +73,24 @@ class FormHandler implements FormHandlerInterface
             if ($form->isSubmitted() && $form->isValid()) {
                 $formSubmission = new FormSubmission();
                 $formSubmission->setIpAddress($request->getClientIp());
-                $formSubmission->setNode($em->getRepository(Node::class)->getNodeFor($page));
+                $formSubmission->setNode($this->em->getRepository(Node::class)->getNodeFor($page));
                 $formSubmission->setLang($request->getLocale());
-                $em->persist($formSubmission);
+                $this->em->persist($formSubmission);
 
                 /* @var FormSubmissionField $field */
                 foreach ($fields as $field) {
                     $field->setSubmission($formSubmission);
                     $field->onValidPost($form, $formBuilder, $request, $this->container);
-                    $em->persist($field);
+                    $this->em->persist($field);
                 }
 
-                $em->flush();
-                $em->refresh($formSubmission);
+                $this->em->flush();
+                $this->em->refresh($formSubmission);
 
                 $event = new SubmissionEvent($formSubmission, $page);
-                $this->container->get('event_dispatcher')->dispatch($event, FormEvents::ADD_SUBMISSION);
+                $this->eventDispatcher->dispatch($event, FormEvents::ADD_SUBMISSION);
 
-                return new RedirectResponse($page->generateThankYouUrl($router, $context));
+                return new RedirectResponse($page->generateThankYouUrl($this->router, $context));
             }
         }
         $context['frontendform'] = $form->createView();
